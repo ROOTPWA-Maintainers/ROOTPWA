51c0f3c3cefd5cfbcf7cbca98dc2f4d671ce70d5
Sebastian Neubert <sneubert@users.sourceforge.net>
2009-09-28 16:00:33 +0000

    added script for constraint fit

---
+++ scripts/4fit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# start 4 fitting jobs simultaneusly!
+
+IDS=( 1 2 3 4 )
+#IDS=( 5 6 7 8 )
+
+for i in 0 1 2 3 ; do
+export SGE_TASK_ID=${IDS[$i]}
+echo "Starting Job $SGE_TASK_ID"
+./rootfitBatch.sge > batch$SGE_TASK_ID.log &
+done
---
+++ scripts/rootfitCBatch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+test -z $FITDIR && echo "FITDIR not set!" && exit;
+test -z $OUTFILE && echo "OUTFILE not set!" && exit;
+test -z $1 && echo "No bin dir given!" && exit;
+test -z $RANK && export RANK=2;
+
+echo "FITDIR=$FITDIR"
+
+test -s $FITDIR/$OUTFILE && mv $FITDIR/$OUTFILE $FITDIR/$OUTFILE.previous
+
+for i in $1/*; 
+do 
+  export BIN=`echo $i | gawk -F"/" '{ print $(NF) }' `;
+  echo "---- Massbin: $BIN";
+  LOWER=`echo $BIN | gawk -F"." '{ print $1 }' `;
+  UPPER=`echo $BIN | gawk -F"." '{ print $2 }' `;
+  echo "$LOWER...$UPPER"
+  cd $i/AMPS;
+  time pwaConstraintFit -q -w $FITDIR/$WLIST -c $FITDIR/$CONSTRFILE -o $FITDIR/$OUTFILE -S $FITDIR/$OUTFILE -r $RANK -l $LOWER -u $UPPER $RENORM -a $ACC -n $NORM
+
+  #cd -;
+done ;
+
