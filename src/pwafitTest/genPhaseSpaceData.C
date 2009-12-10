db0b7da5083a11446c4b32d3d792300fc33c7011
Prometeusz Jasinski <promme@users.sourceforge.net>
2009-12-10 12:38:50 +0000

    beam spot is not a point anymore. Writing out formated output for Comgeant simulation.

--- src/pwafitTest/genPhaseSpaceData.C
+++ src/pwafitTest/genPhaseSpaceData.C
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <sstream>
 
 #include "TVector3.h"
 #include "TLorentzVector.h"
@@ -25,7 +26,8 @@ const double gPionMass   = 0.13957018;
 const double gPionMass2  = gPionMass * gPionMass;
 
 // target position
-const double gTargetZPos = -300.0; // [cm]
+const double gTargetZPos = -30.0; // [cm] (target cell end)
+const double gTargetlength = -40.0; // [cm] (twards upstream)
 
 // beam parameters:
 const double gBeamMomSigma = 1.2;  // [GeV/c]
@@ -40,6 +42,12 @@ const double gBeamDxDz      = 0.0;
 const double gBeamDxDzSigma = 0.0;
 const double gBeamDyDz      = 0.0;
 const double gBeamDyDzSigma = 0.0;
+
+// beamspot parameters [cm]
+const double gBeamOffsetX	= 0.0;
+const double gBeamOffsetY	= 0.0;
+const double gBeamSpotSizeX = 1.5; // in terms of 3 sigma
+const double gBeamSpotSizeY = 1.5;
 
 // cut on t-distribution
 const double tMin = 0.001;  // [(GeV/c)^2]
@@ -65,6 +73,17 @@ makeBeam()
   return TLorentzVector(px, py, pz, EBeam);
 }
 
+// constructs Interaction point in the target cell
+TVector3
+makeVertex()
+{
+	TVector3 result;
+	result.SetX(gRandom->Gaus(gBeamOffsetX, gBeamSpotSizeX/3.));
+	result.SetY(gRandom->Gaus(gBeamOffsetY, gBeamSpotSizeY/3.));
+	result.SetZ(gRandom->Uniform(gTargetZPos+gTargetlength, gTargetZPos));
+	return result;
+}
+
 
 // writes event to ascii file read by gamp
 bool
@@ -76,7 +95,7 @@ writePwa2000Ascii(ostream&              out,
     cerr << "Output stream is not writable." << endl;
     return false;
   }
-    
+
   const int geantIds[3] = { 8,  9,  9};
   const int charges[3]  = {+1, -1, -1};
   // total number of particles
@@ -93,6 +112,40 @@ writePwa2000Ascii(ostream&              out,
     // hadron: geant ID, charge, p_x, p_y, p_z, E
     out << setprecision(numeric_limits<double>::digits10 + 1)
 	<< geantIds[i] << " " << charges[i] << " " << hadron->Px() << " " << hadron->Py() << " " << hadron->Pz() << " " << hadron->E() << endl;
+    }
+  return true;
+}
+
+// writes event to ascii file read by ComGeant fort.26 interface
+bool
+writeComGeantAscii(ostream&         out,
+		  const TLorentzVector& 	beam,
+				TGenPhaseSpace&     event,
+		  const TVector3&			vertexpos)
+{
+  if (!out) {
+    cerr << "Output stream is not writable." << endl;
+    return false;
+  }
+
+  const int geantIds[3] = { 8,  9,  9};
+  const int charges[3]  = {+1, -1, -1};
+  // total number of particles
+  out << 4 << endl;
+  // vertex position in cm
+  out << vertexpos.X() << " " << vertexpos.Y() << " " << vertexpos.Z() << endl;
+  // beam particle: geant ID , -p_x, -p_y, -p_z must go the opposite direction upstream!
+  out << setprecision(numeric_limits<double>::digits10 + 1)
+      << "9 " << -beam.Px() << " " << -beam.Py() << " " << -beam.Pz() << endl;// << " " << beam.E() << endl;
+  for (unsigned int i = 0; i < 3; ++i) {
+    TLorentzVector* hadron = event.GetDecay(i);
+    if (!hadron) {
+      cerr << "genbod returns NULL pointer to Lorentz vector for daughter " << i << "." << endl;
+      continue;
+    }
+    // hadron: geant ID, p_x, p_y, p_z
+    out << setprecision(numeric_limits<double>::digits10 + 1)
+	<< geantIds[i] /*<< " " << charges[i]*/ << " " << hadron->Px() << " " << hadron->Py() << " " << hadron->Pz() << endl;// << " " << hadron->E() << endl;
     }
   return true;
 }
@@ -120,7 +173,9 @@ genPhaseSpaceData(const double   xMassMin          = 2.100,  // lower bound of m
 		  const TString& outFileName       = "2100.2140.genbod.evt",
 		  const TString& thetaHistFileName = "./hTheta.root",  // histogram with experimental distribution of scattering angle
 		  const int      nmbEvent          = 2000,
-		  const bool     plot              = false)
+		  const bool     plot              = false,
+		  const TString& outFileNameComGeant = "" // outputfilename for Comgeant output, if empty filename will be generated according to mass limits
+		  )
 {
   Double_t daughterMasses[3] = {gPionMass, gPionMass, gPionMass};
 
@@ -139,15 +194,27 @@ genPhaseSpaceData(const double   xMassMin          = 2.100,  // lower bound of m
     ht        = new TH1D("ht", "t", 10000, -0.1, 1);
     hm        = new TH1D("hm", "3pi mass", 1000, 0.5, 2.5);
     hTheta    = new TH1D("hThetaGen", "cos theta", 100, 0.99985, 1);
-    hVertex3D = new TH3D("hVertex3D", "Vertex xyz", 100, -2, 2, 100, -2, 2, 200, gTargetZPos - 5, gTargetZPos + 40);
+    hVertex3D = new TH3D("hVertex3D", "Vertex xyz", 100, -2, 2, 100, -2, 2, 200, gTargetZPos + gTargetlength -10 , gTargetZPos + 10);
     hVertex   = new TH2D("hVertex", "Vertex xy", 100, -2, 2, 100, -2, 2);
-    hVz       = new TH1D("hVz","Vertex z", 1000, gTargetZPos - 40, gTargetZPos + 40);
+    hVz       = new TH1D("hVz","Vertex z", 1000, gTargetZPos + gTargetlength -10 , gTargetZPos +10);
     hE        = new TH1D("hE", "E", 100, 180, 200);
   }
 
-  // open output file
+  // open output files
   ofstream outFile(outFileName);
-  cout << "Writing " << nmbEvent << " events to file '" << outFileName << "'." << endl;
+
+  // generate filename if needed
+  TString _outFileNameComGeant = outFileNameComGeant;
+  if (_outFileNameComGeant == ""){
+	  stringstream _filename;
+	  _filename << (int) (xMassMin*1e3) << "." << (int) (xMassMax*1e3) << ".genbod.fort.26";
+	  cout << " created ComGeantevents filename: " << _filename.str() << endl;
+	  _outFileNameComGeant = _filename.str();
+  }
+
+  ofstream outFileComGeant(_outFileNameComGeant);
+
+  cout << "Writing " << nmbEvent << " events to file '" << outFileName << " and " << _outFileNameComGeant << "'." << endl;
 
   // get theta histogram
   TH1* thetaDist = NULL;
@@ -171,7 +238,7 @@ genPhaseSpaceData(const double   xMassMin          = 2.100,  // lower bound of m
     ++attempts;
 
     // construct primary vertex and beam
-    const TVector3       vertexPos(0, 0, gTargetZPos);
+    const TVector3       vertexPos = makeVertex();// (0, 0, gTargetZPos);
     const TLorentzVector beam = makeBeam();
 
     // sample theta directly:
@@ -189,7 +256,7 @@ genPhaseSpaceData(const double   xMassMin          = 2.100,  // lower bound of m
     const double e = Ea - 1 / (2 * gProtonMass * epso) * (eat * eat + mpeps * mpeps);
     const double pw = sqrt(e * e - xMass2); // three momentum magnitude
     const double phi= gRandom->Uniform(0., TMath::TwoPi());
-     
+
     TVector3 p3(1, 0, 0);
     p3.SetMagThetaPhi(pw, theta, phi);
     // rotate to beamdirection:
@@ -227,8 +294,9 @@ genPhaseSpaceData(const double   xMassMin          = 2.100,  // lower bound of m
       hVz->Fill(vertexPos.Z());
       hE->Fill(e);
     }
-     
+
     writePwa2000Ascii(outFile, beam, phaseSpace);
+    writeComGeantAscii(outFileComGeant,beam, phaseSpace, vertexPos);
   }
 
   cout << endl << "Needed " << attempts << " attempts to generate " << countEvent << " events." << endl;
@@ -244,7 +312,7 @@ genPhaseSpaceData(const double   xMassMin          = 2.100,  // lower bound of m
     c->cd(3);
     hVz->Draw();
     c->cd(4);
-    hVertex->Draw();
+    hVertex->Draw("COLZ");
     c->cd(5);
     hE->Draw();
     c->cd(6);
