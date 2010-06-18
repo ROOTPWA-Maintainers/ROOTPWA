31b9962a9de552bd9467848c71e97521eec81627
Prometeusz Jasinski <promme@users.sourceforge.net>
2010-06-18 10:13:26 +0000

    commiting 70 waves set created automatically by genKey_Kpipi.C that goes though all quantumnumbers of all Combinations and moving the waves to an other folder given in genKey_Kpipi.C. Some generated waves were discovered as making problems during the wave check by boris. I will check this.

--- keygen/genKey_Kpipi.C
+++ keygen/genKey_Kpipi.C
@@ -35,6 +35,12 @@
 
 
 #include <string>
+#include <vector>
+#include <utility>
+#include <iostream>
+#include <stdio.h>
+#include <sstream>
+
 
 #include "particleKey.h"
 #include "waveKey.h"
@@ -49,6 +55,7 @@ void genKey_Kpipi(const bool testKey = true, const string& dataFileName =
 		const string& pdgTableFileName = "./pdgTable.txt") {
 
 	const string thisFilePath = __FILE__;
+	const string movetoFilePath = "${ROOTPWA}/keyfiles/keyKpipi/SETX";
 	// define final state particles
 	particleKey pi_minus("pi-");
 	particleKey pi_plus("pi+");
@@ -58,8 +65,8 @@ void genKey_Kpipi(const bool testKey = true, const string& dataFileName =
 
 	// K pi decay modes
 	// Daum et al. used a very controversial resonance, the kappa or K*(800)
-	//particleKey Kstar800("Kstar(800)",  &K_minus, &pi_plus, 0, 1); // not in the PDG table yet
-	particleKey Kstar892("Kstar(892)0", &K_minus, &pi_plus, 0, 1); // 1-
+	//particleKey Kstar800("Kstar(800)",  &K_minus, &pi_plus, 1, 0); // not in the PDG table yet
+	particleKey Kstar892("Kstar(892)0", &K_minus, &pi_plus, 1, 0); // 1-
 	// probably I will have to treat both Kstar(1430) as a broad
 	// Kpi-P wave since they are overlapping, but it is also very
 	// likely that I'm talking bullshit
@@ -72,6 +79,80 @@ void genKey_Kpipi(const bool testKey = true, const string& dataFileName =
 	particleKey rho770("rho(770)", &pi_plus, &pi_minus, 1, 0); // 1--
 	particleKey f21270("f2(1270)", &pi_plus, &pi_minus, 2, 0); // 2++
 
+	// let's create all possible combinations of decays according
+	// to the rules of L,S coupling and parity
+
+	//		pair<parity,   isobar>
+	vector< pair<int,particleKey*> > isobar1[2]; // {K*, pi*}
+	pair<int,particleKey*> isobar2[2]; 			 // {pi, K  }
+
+	// fill all isobars
+	isobar2[0] = make_pair(-1,&pi_minus);
+	isobar2[1] = make_pair(-1,&K_minus);
+
+	isobar1[0].push_back(make_pair(+1,&Kstar01430));
+	isobar1[0].push_back(make_pair(-1,&Kstar892));
+	isobar1[0].push_back(make_pair(+1,&Kstar21430));
+
+	isobar1[1].push_back(make_pair(+1,&f0980));
+	isobar1[1].push_back(make_pair(-1,&rho770));
+	isobar1[1].push_back(make_pair(+1,&f21270));
+
+	unsigned int Mmax = 1;
+	unsigned int lorbmax = 2;
+
+	int wavecounter(0);
+
+	for (unsigned int comb = 0; comb < 2; comb++){ // go through the to Combinations K* pi and pi* K
+		for (unsigned int i = 0; i < isobar1[comb].size(); i++){ // loop over all K* isobars
+			cout << endl;
+			int parity_isobar1 = isobar1[comb][i].first;
+			int parity_isobar2 = isobar2[comb].first;
+			int J_isobar1 = isobar1[comb][i].second->L();
+			int J_isobar2 = isobar2[comb].second->L();
+			for (unsigned int spin = abs(J_isobar1-J_isobar2); spin <= (unsigned) abs(J_isobar1+J_isobar2); spin++){ // couple the spins
+				for (unsigned int lorb = 0; lorb <= lorbmax; lorb++){ // allow only orbital angular momenta up to lorbmax
+					// construct the parity
+					int parity = parity_isobar1 * parity_isobar2 * pow(-1,lorb);
+					// create the particle key
+					particleKey X("X", isobar1[comb][i].second, isobar2[comb].second, lorb, spin);
+					for (unsigned int J = abs(lorb-spin); J <= (unsigned) abs(lorb+spin); J++){ // couple now spin and orbital angular momentum
+						for (unsigned int M = 0; (M <= J && M <= Mmax); M++){ // go through the J projections up to either the allowed value or Mmax
+							cout << "JPM LS: " << J << " " << parity << " " << M << " " << lorb << " " << spin << endl;
+							// create a wave of positive reflectivity if (1)
+							if (1){
+								//      wave(&X, J, P, M, refl);
+								waveKey wave(&X, J,parity,M,+1);
+								generateKeyFile(wave, thisFilePath, testKey, dataFileName, pdgTableFileName);
+								stringstream command;
+								command << "mv " << wave.waveName(true) << " " << movetoFilePath << "/";
+								cout << " executing " << command.str();
+								system(command.str().c_str());
+								wavecounter++;
+							}
+							// create a wave of negative reflectivity if (1)
+							if (0){
+								//      wave(&X, J, P, M, refl);
+								waveKey wave(&X, J,parity,M,-1);
+								generateKeyFile(wave, thisFilePath, testKey, dataFileName, pdgTableFileName);
+								stringstream command;
+								command << "mv " << wave.waveName(true) << " " << movetoFilePath << "/";
+								cout << " executing " << command.str();
+								system(command.str().c_str());
+								wavecounter++;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	cout << " created " << wavecounter << " keys " << endl;
+
+	// ..JPLMpartiyexchange(isobar1 isobar2)
+
+	if (0){
 	// PWA wave set according to Daum et al.
 	// ..JPLMpartiyexchange(isobar1 isobar2)
 	{ // 0-P0+(K*pi)
@@ -209,4 +290,5 @@ void genKey_Kpipi(const bool testKey = true, const string& dataFileName =
 		waveKey wave(&X, 1,+1, 0,+1);
 		generateKeyFile(wave, thisFilePath, testKey, dataFileName, pdgTableFileName);
 	}*/
+	}
 }
