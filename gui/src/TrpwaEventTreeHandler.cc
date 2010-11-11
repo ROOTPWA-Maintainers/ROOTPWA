313a80763860ddbb1dcdf1cf9e8462b608084efb
Prometeusz Jasinski <promme@users.sourceforge.net>
2010-11-11 14:43:50 +0000

    Declaration of event tree handler started

---
+++ gui/src/TrpwaEventTreeHandler.cc
@@ -0,0 +1,55 @@
+/*
+ * TrpwaEventTreeHandler.cc
+ *
+ *  Created on: Nov 11, 2010
+ *      Author: Promme
+ */
+
+#include "TrpwaEventTreeHandler.h"
+
+TrpwaEventTreeHandler::TrpwaEventTreeHandler(){
+	;
+}
+
+bool TrpwaEventTreeHandler::Add_eventtreefile(string filename, string treename){
+	bool result = true;
+
+	return result;
+}
+
+
+
+bool TrpwaEventTreeHandler::Add_eventtreefiles(vector<string> filenames, string treename){
+	bool result = true;
+
+	return result;
+}
+
+
+
+bool TrpwaEventTreeHandler::Write_Trees_to_BNL_events(bool overwrite){
+	bool result = true;
+
+	return result;
+}
+
+
+
+bool TrpwaEventTreeHandler::Set_bin_paths(string dirnames){
+	bool result = true;
+
+	return result;
+}
+
+
+
+TrpwaEventTreeHandler::~TrpwaEventTreeHandler(){
+}
+
+
+
+void Path_to_bin(string path, int & binlow, int & binhigh){
+}
+
+
+
