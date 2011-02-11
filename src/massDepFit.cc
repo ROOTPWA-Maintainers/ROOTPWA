d51009724523430e65672e2d47006a01029b59f3
Sebastian Neubert <sneubert@users.sourceforge.net>
2011-02-11 10:09:38 +0000

    small fix

--- src/massDepFit.cc
+++ src/massDepFit.cc
@@ -418,7 +418,7 @@ extern char* optarg;
 
 
   // find minimum of likelihood function
-  if(plottingMode) printInfo << "Plotting mode, skipping minimzation" << endl;
+  if(onlyPlotting) printInfo << "Plotting mode, skipping minimzation" << endl;
   else {
     printInfo << "performing minimization." << endl;
     
@@ -845,7 +845,8 @@ extern char* optarg;
        phasefitgraphs[c]->SetLineColor(kRed);
        phasefitgraphs[c]->SetMarkerColor(kRed);
        phasefitgraphs[c]->SetDrawOption("AP");
-       phasefitgraphs[c]->SetMarkerStyle(22);
+       phasefitgraphs[c]->SetMarkerStyle(24);
+       phasefitgraphs[c]->SetMarkerSize(0.2);
        phasegraphs[c]->Add(phasefitgraphs[c],"cp");
 
        realfitgraphs.push_back(new TGraph(nbins));
@@ -954,6 +955,14 @@ extern char* optarg;
 	 phasedatagraphs[c]->SetPointError(i,binwidth,
 					   rho->phaseErr(wl[iw].c_str(),
 							 wl[iw2].c_str()));
+	 phasedatagraphs[c]->SetPointError(i+ndatabins,binwidth,
+					   rho->phaseErr(wl[iw].c_str(),
+							 wl[iw2].c_str()));
+	 phasedatagraphs[c]->SetPointError(i+2*ndatabins,binwidth,
+					   rho->phaseErr(wl[iw].c_str(),
+							 wl[iw2].c_str()));
+
+
 	 phasefitgraphs[c]->SetPoint(i,m,compset.phase(wl[iw],ps,
 						       wl[iw2],ps2,m)*TMath::RadToDeg());
 
@@ -1019,32 +1028,32 @@ extern char* optarg;
 
 /// rectivfy phase graphs
    
-   // unsigned int refbin=4;
-//    double m;
-//    double predph;
-//    phasedatagraphs[iw]->GetPoint(refbin,m,predph);
-//    double prefph;
-//    phasefitgraphs[iw]->GetPoint(refbin,m,prefph);
-//    for(unsigned int ib=refbin+1;ib<nbins;++ib){
-//      double dph; phasedatagraphs[iw]->GetPoint(ib,m,dph);
-//      double fph; phasefitgraphs[iw]->GetPoint(ib,m,fph);
-//      double dp,dm;dp=dph+360;dm=dph-360;
-//      double fp,fm;fp=fph+360;fm=fph-360;
-//      if(0){
-//        if(fabs(dp-predph)<fabs(dph-predph) && fabs(dp-predph)<fabs(dm-predph))
-// 	 phasedatagraphs[iw]->SetPoint(ib,m,dp);
-//        else if(fabs(dm-predph)<fabs(dph-predph) && fabs(dm-predph)<fabs(dp-predph))
-// 	 phasedatagraphs[iw]->SetPoint(ib,m,dm);
+   // unsigned int refbin=6;
+   // double m;
+   // double predph;
+   // phasedatagraphs[iw]->GetPoint(refbin,m,predph);
+   // double prefph;
+   // phasefitgraphs[iw]->GetPoint(refbin,m,prefph);
+   // for(unsigned int ib=refbin+1;ib<nbins;++ib){
+   //   double dph; phasedatagraphs[iw]->GetPoint(ib,m,dph);
+   //   double fph; phasefitgraphs[iw]->GetPoint(ib,m,fph);
+   //   double dp,dm;dp=dph+360;dm=dph-360;
+   //   double fp,fm;fp=fph+360;fm=fph-360;
+   //   if(0){
+   //     if(fabs(dp-predph)<fabs(dph-predph) && fabs(dp-predph)<fabs(dm-predph))
+   // 	 phasedatagraphs[iw]->SetPoint(ib,m,dp);
+   //     else if(fabs(dm-predph)<fabs(dph-predph) && fabs(dm-predph)<fabs(dp-predph))
+   // 	 phasedatagraphs[iw]->SetPoint(ib,m,dm);
        
-//        if(fabs(fp-prefph)<fabs(fph-prefph) && fabs(fp-prefph)<fabs(fm-prefph))
-// 	 phasefitgraphs[iw]->SetPoint(ib,m,fp);
-//        else if(fabs(fm-prefph)<fabs(fph-prefph) && fabs(fm-prefph)<fabs(fp-prefph))
-// 	 phasefitgraphs[iw]->SetPoint(ib,m,fm);
+   //     if(fabs(fp-prefph)<fabs(fph-prefph) && fabs(fp-prefph)<fabs(fm-prefph))
+   // 	 phasefitgraphs[iw]->SetPoint(ib,m,fp);
+   //     else if(fabs(fm-prefph)<fabs(fph-prefph) && fabs(fm-prefph)<fabs(fp-prefph))
+   // 	 phasefitgraphs[iw]->SetPoint(ib,m,fm);
        
-//        phasedatagraphs[iw]->GetPoint(ib,m,predph);
-//        phasefitgraphs[iw]->GetPoint(ib,m,prefph);
-//      }
-//    }
+   //     phasedatagraphs[iw]->GetPoint(ib,m,predph);
+   //     phasefitgraphs[iw]->GetPoint(ib,m,prefph);
+   //   }
+   // }
    
      phasegraphs[iw]->Write();
      overlapRegraphs[iw]->Write();
