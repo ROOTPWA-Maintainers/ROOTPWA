e42177b389a3371332dd50300a2fc860467da75b
Sebastian Neubert <sneubert@users.sourceforge.net>
2011-04-06 16:16:46 +0000

    nice plotting

--- src/rootscripts/plotMassDepFitResult.C
+++ src/rootscripts/plotMassDepFitResult.C
@@ -1,5 +1,6 @@
 #include "TFile.h"
 #include "TList.h"
+#include "TStyle.h"
 #include "TKey.h"
 #include "TString.h"
 #include "TCanvas.h"
@@ -8,12 +9,218 @@
 #include "TH1F.h"
 #include "TMultiGraph.h"
 #include "TAxis.h"
+#include "TGaxis.h"
+#include "TLatex.h"
+#include "TFrame.h"
+#include "TObjArray.h"
+#include "TROOT.h"
+//#include "TQSender.h"
 
 #include <iostream>
+#include <sstream>
+#include <map>
 using namespace std;
+
+
+TString parseTitle(TString l){
+ // setup isobar dictionary key->tex
+  map<TString, TString> isobars;
+  isobars["pi+"     ] = "\\pi^{+}";
+  isobars["pi-"     ] = "\\pi^{-}";
+  isobars["pi+-"    ] = "\\pi^{\\pm}";
+  isobars["pi-+"    ] = "\\pi^{\\mp}";
+  isobars["sigma"   ] = "\\sigma";
+  isobars["rho770"  ] = "\\rho(770)";
+  isobars["a11269"  ] = "a_{1}(1269)";
+  isobars["a21320"  ] = "a_{2}(1320)";
+  isobars["rho1450" ] = "\\rho(1450)";
+  isobars["rho1700" ] = "\\rho(1700)";
+  isobars["pi1300"  ] = "\\pi(1300)";
+  isobars["pi1800"  ] = "\\pi(1800)";
+  isobars["pi21670" ] = "\\pi_2(1670)";
+  isobars["f01370"  ] = "f_{0}(1370)";
+  isobars["f01500"  ] = "f_{0}(1500)";
+  isobars["f01700"  ] = "f_{0}(1700)";
+  isobars["f11285"  ] = "f_{1}(1285)";
+  isobars["f11420"  ] = "f_{1}(1420)";
+  isobars["b11235"  ] = "b_{1}(1235)";
+  isobars["b11800"  ] = "b_{1}(1800)";
+  isobars["b11500"  ] = "b_{1}(1500)";
+  isobars["f21270"  ] = "f_{2}(1270)";
+  isobars["f21950"  ] = "f_{2}(1950)";
+  isobars["f21565"  ] = "f_{2}(1565)";
+  isobars["f21270"  ] = "f_{2}(1270)";
+  isobars["f22010"  ] = "f_{2}(2010)";
+  isobars["f11420"  ] = "f_{1}(1420)";
+  isobars["eta1440" ] = "\\eta(1420)";
+  isobars["eta21645"] = "\\eta_{2}(1645)";
+  isobars["rho31690"] = "\\rho_{3(1690)";
+  isobars["a21320"  ] = "a_{2}(1320)}";
+
+   // remove file extension
+    l.Remove(l.Length() - 4);
+    // extract X quantum numbers
+    const TString head = l(0, 7);
+    const TString I    = head(0, 1); 
+    const TString G    = head(1, 1);
+    const TString J    = head(2, 1);
+    const TString P    = head(3, 1);
+    const TString C    = head(4, 1);
+    const TString M    = head(5, 1);
+    const TString refl = head(6, 1);
+    l.Remove(0, 7);
+    // print X quantum numbers
+    
+    stringstream res;
+    res << I << "^{" << G <<"}(" << J << "^{" << P << C << "}" << M << "^{" << refl << "})";
+
+     // tokenize input
+    TObjArray* tokens = l.Tokenize("_=");
+    int        mode   = 0;
+    for (int i = 0; i < tokens->GetEntries(); ++i) {
+      const TString token = ((TObjString*)tokens->At(i))->GetString();
+      cerr << "    " << mode << ": '" << token << "'" << endl;
+      if (mode == 0) {  // isobar mode
+	if (isobars.find(token) != isobars.end())
+	  res << isobars[token];
+	else
+	  res << token;
+	res << " ";
+	// check which mode to switch to depending whether we get _ or =
+	l.Remove(0, token.Length());
+	if (l(0, 1) == "_")
+	  mode = 1;
+	else
+	  mode = 2;
+      } else if (mode == 1) {  // ls mode
+	if (token.Length() == 1)  // only l
+	  res << "[" << token << "] ";
+	else
+	  res << "\\left[#splitline{" << token(0, 1) << "}{"
+	       << token(1, 1) << "}\\right]";
+	l.Remove(0, token.Length());
+	mode = 0;
+      } else if (mode == 2) {
+	res << "\\rightarrow ";
+	if (isobars.find(token) != isobars.end())
+	  res << isobars[token];
+	else
+	  res << token;
+	res << " ";
+	l.Remove(0, token.Length());
+	if (l(0, 1) == "_" )
+	  mode = 1;
+	else
+	  mode = 2;
+      }
+      l.Remove(0, 1); // remove delimiter
+    }
+    res;    
+
+    TString result(res.str().c_str());
+    return result;
+}
+
+void exec3event(Int_t event, Int_t x, Int_t y, TObject *selected)
+{
+  TCanvas *c = (TCanvas *) gTQSender;
+  //if(selected->IsA()->GetName())
+  //printf("Canvas %s: event=%d, x=%d, y=%d, selected=%s\n", c->GetName(),event, x, y, selected->IsA()->GetName());
+  
+  if(event==1){ // clicked
+
+    // figure out which pad we clicked onto
+    TVirtualPad* pad=c->GetClickSelectedPad();
+    
+    TCanvas* cpopup=(TCanvas*)gROOT->FindObjectAny("cpopup");
+    cpopup->Clear("");
+    cpopup->cd();
+    TVirtualPad* clone=(TVirtualPad*)pad->DrawClone();
+    clone->SetPad("pu","PopUp",0,0,1,1,0);
+    cpopup->cd(1);
+    cpopup->Update();
+    clone->cd();
+    
+   
+    double xcenter=0.5;
+    double ycenter=0.5;
+    
+    double x=xcenter-0.25;
+    double y=xcenter-0.2;
+    
+    // preliminary
+    TLatex* prelim=new TLatex(x,y,"preliminary");
+    prelim->SetNDC();
+    prelim->SetTextColor(kGray);
+    prelim->SetTextSize(0.1);
+    prelim->SetTextAngle(20);
+    prelim->Draw();
+    
+    // compass 2004
+    double xc=xcenter+0.05;
+    //if(right)xc=xcenter+0.1;
+    double yc=ycenter+0.35;
+    TLatex* com04=new TLatex(xc,yc,"COMPASS 2004");
+    com04->SetNDC();
+    com04->SetTextSize(0.05);
+    com04->Draw();
+    
+    // 5 pi on pb
+    xc=xcenter+0.05;
+    //xc=xcenter+0.1;
+    yc=ycenter+0.32;
+    TLatex* react=new TLatex(xc,yc,"#pi^{-} Pb #rightarrow #pi^{-}#pi^{+}#pi^{-}#pi^{+}#pi^{-} Pb");
+    react->SetNDC();
+    react->SetTextSize(0.0395);
+    react->Draw();
+    
+    // add waves 
+    cout << "####### Title:" << endl;
+    TMultiGraph* gr=(TMultiGraph*)clone->GetListOfPrimitives()->At(1);
+    TString title=gr->GetName();
+    cout << title << endl;
+    // check if this is intensity or off diagonal
+    TString wave;
+    if(!title.Contains("---")){
+      cout << "Processing Intensity:" << endl;
+      wave=parseTitle(title);
+      cout << wave << endl;
+
+    }
+
+     xc=xcenter+-0.1;
+    //xc=xcenter+0.1;
+    yc=ycenter+0.28;
+    TLatex* waveL=new TLatex(xc,yc,wave);
+    waveL->SetNDC();
+    waveL->SetTextSize(0.0295);
+    waveL->Draw();
+    cpopup->UseCurrentStyle();
+    cpopup->Update();
+    //cpopup->Flush();
+  }
+}
+
+//------------------------------------------------------
+//------------------------------------------------------
+//------------------------------------------------------
+//------------------------------------------------------
+//------------------------------------------------------
+
 
 void plotMassDepFitResult(TString infilename, TString plotdir="plots/"){
   TFile* infile=TFile::Open(infilename);
+  Int_t font=132;
+  gStyle->SetTextFont(font);
+  gStyle->SetLabelFont(font,"xy");
+  gStyle->SetTitleFont(font,"xy");
+    gStyle->SetOptStat(0);
+    gStyle->SetOptTitle(0);
+    gStyle->SetStripDecimals(1);
+    TGaxis::SetMaxDigits(4);
+   
+    gROOT->ForceStyle();
+
 
   TList* keylist=infile->GetListOfKeys();
   unsigned int num=keylist->GetEntries();
@@ -31,17 +238,28 @@ void plotMassDepFitResult(TString infilename, TString plotdir="plots/"){
   unsigned int nwaves=wavenames.size();
   std::cout << nwaves << " waves used in fit" << endl;
 
-  TCanvas* c=new TCanvas("c","Spin Density Matrix",10,10,1000,1000);
-  c->Divide(nwaves,nwaves,0,0);
- TCanvas* cRe=new TCanvas("cRe","Spin Density Matrix - RealPart",10,10,1000,1000);
+  TCanvas* cS=new TCanvas("cS","Spin Density Matrix",10,10,1000,1000);
+  cS->Divide(nwaves,nwaves,0,0);
+ TCanvas* cRe=new TCanvas("cRe","Spin Density Matrix - RealImagPart",10,10,1000,1000);
   cRe->Divide(nwaves,nwaves,0,0);
+  cRe->Connect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)", 0, 0,"exec3event(Int_t,Int_t,Int_t,TObject*)");
+  // c->Connect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)", 0, 0,"exec3event(Int_t,Int_t,Int_t,TObject*)");
+
+  TCanvas* cpopup=new TCanvas("cpopup","PopUp",50,50,700,700);
+
+
   // TCanvas* cIm=new TCanvas("cIm","Spin Density Matrix - ImagPart",10,10,1000,1000);
   //cIm->Divide(nwaves,nwaves,0,0);
   
+  // cRe->Connect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)", 0, 0,
+  //		      "exec3event(Int_t,Int_t,Int_t,TObject*)");
+  //c->Connect("ProcessedEvent(Int_t,Int_t,Int_t,TObject*)", 0, 0,
+  //		      "exec3event(Int_t,Int_t,Int_t,TObject*)");
+
   // do plotting
   for(unsigned int ip=0;ip<nwaves;++ip){
     for(unsigned int jp=ip;jp<nwaves;++jp){
-      c->cd(jp+ip*nwaves+1);
+      cS->cd(jp+ip*nwaves+1);
       if(ip==jp){
 	TMultiGraph* g=(TMultiGraph*)infile->Get(wavenames[ip]);
 	g->Draw("APC");
@@ -61,6 +279,8 @@ void plotMassDepFitResult(TString infilename, TString plotdir="plots/"){
 	//g->Draw("APC");
 	cRe->cd(jp+ip*nwaves+1);
 	g->Draw("APC");
+
+
 
 	TCanvas* c2=new TCanvas();
 	g->Draw("APC");
@@ -121,12 +341,15 @@ void plotMassDepFitResult(TString infilename, TString plotdir="plots/"){
 	  c2->SaveAs(TString(plotdir+key+".eps"));
 	  delete c2;
 	  
-	}
-
-      }
+	}// end if g!=NULL
+      } // end else
     } // end inner loop
   } // end plotting loop
-  c->SaveAs(TString(plotdir+"/spindensitymatrix.eps"));
+
+  cS->SaveAs(TString(plotdir+"/spindensitymatrix.eps"));
   cRe->SaveAs(TString(plotdir+"/spindensitymatrixRe.eps"));
   //cIm->SaveAs(TString(plotdir+"/spindensitymatrixIm.eps"));
 }
+
+
+
