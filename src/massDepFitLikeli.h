38a67b90df4dd27861ea58ce1ec70b84415d6af0
Sebastian Neubert <sneubert@users.sourceforge.net>
2012-03-13 16:30:43 +0000

    added possibility to switch off covariances

--- src/massDepFitLikeli.h
+++ src/massDepFitLikeli.h
@@ -23,7 +23,10 @@
 #include <map>
 #include <string>
 #include "pwacomponent.h"
-
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include "TMatrixD.h"
 // Collaborating Class Declarations --
 class TTree;
 class TF1;
@@ -32,6 +35,11 @@ namespace rpwa {
 
   class fitResult;
 
+namespace ublas = boost::numeric::ublas;
+typedef std::complex<double> cnum;
+typedef ublas::matrix<cnum> cmatrix;
+typedef ublas::matrix<double> rmatrix;
+typedef ublas::matrix<rmatrix> ccmatrix;
 
 
   class massDepFitLikeli : public ROOT::Math::IBaseFunctionMultiDim {
@@ -50,7 +58,8 @@ namespace rpwa {
     void init(TTree* fitresulttree,
 	      TF1* finalStatePhaseSpace,
 	      pwacompset* compset,
-	      double mmin=0, double mmax=5000);
+	      double mmin=0, double mmax=5000,
+	      bool doCov=true);
     
 
 
@@ -68,10 +77,15 @@ namespace rpwa {
     TF1* _finalStatePS;
     pwacompset* _compset;
     fitResult* _rhom; // measured spindensity matrix;
+    //std::vector<fitResult> _vrhom; // memory resident data
+    std::vector<cmatrix> _spindens; // mremory resident spindensity
+    std::vector<ccmatrix> _cov;
+    std::vector<double> _mass;
+
     std::vector<std::string> _wlist;
-    std::vector<unsigned int> _index; // wave indices
-    
+    std::vector<unsigned int> _index; // wave indices in fitResult 
     double  _mmin, _mmax; // fitrange
+    bool    _doCov; // take covariance between Real and Imag into account?
     // Private Methods -----------------
     
   };
