3249148bbe0c5555ace5c804e0ca917db308ef04
Boris Grube <bgrube@users.sourceforge.net>
2012-07-24 18:36:58 +0000

    - moved script that compiles Boost libraries from   mpi/compileBoostMpi.sh to compileBoostLibraries.sh; added   Boost.python
    - fixed build error caused by feature list handling in
      cmakeModules/CommonMacros.cmake; feature list handling with the
      2.8.0 interface seems broken -> should be revisited once we bumbed
      up the minimum required Cmake version; added debug output
    
    - converted some switches in CMakeLists.txt into options; reenabled
      MPI section; improved output

--- cmakeModules/CommonMacros.cmake
+++ cmakeModules/CommonMacros.cmake
@@ -39,6 +39,13 @@
 # example:
 #   switch_file_extension("${CC_LIST}" ".cc" ".h" H_LIST)
 function(switch_file_extension IN_FILE_LIST OLD_EXT NEW_EXT OUT_FILE_LIST)
+	if(DEBUG_OUTPUT)
+		message(STATUS "switch_file_extension was called with the following arguments:
+        IN_FILE_LIST  = '${IN_FILE_LIST}'
+        OLD_EXT       = '${OLD_EXT}'
+        NEW_EXT       = '${NEW_EXT}'
+        OUT_FILE_LIST = '${OUT_FILE_LIST}'")
+	endif()
   set(NEW_FILE_LIST)
   foreach(_OLD_FILE ${IN_FILE_LIST})
     string(REGEX REPLACE "^(.*)${OLD_EXT}$" "\\1${NEW_EXT}" _NEW_FILE ${_OLD_FILE})
@@ -51,6 +58,12 @@ endfunction(switch_file_extension)
 # adds standard shared library
 # additional libraries that should be linked to can be given as optional arguments
 function(make_shared_library LIB_NAME SOURCES)
+	if(DEBUG_OUTPUT)
+		message(STATUS "make_shared_library was called with the following arguments:
+        LIB_NAME = '${LIB_NAME}'
+        SOURCES  = '${SOURCES}'
+        ARGN     = '${ARGN}'")
+	endif()
   add_library(${LIB_NAME} SHARED ${SOURCES})
   # proccess link libraries in additional arguments
   foreach(_LIB ${ARGN})
@@ -62,6 +75,12 @@ endfunction(make_shared_library)
 # adds standard executable
 # additional libraries that should be linked to can be given as optional arguments
 function(make_executable EXE_NAME SOURCES)
+	if(DEBUG_OUTPUT)
+		message(STATUS "make_executable was called with the following arguments:
+        EXE_NAME = '${EXE_NAME}'
+        SOURCES  = '${SOURCES}'
+        ARGN     = '${ARGN}'")
+	endif()
   add_executable(${EXE_NAME} ${SOURCES})
   # proccess link libraries in additional arguments
   foreach(_LIB ${ARGN})
@@ -74,18 +93,28 @@ macro(enforce_out_of_source_build)
   if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
     message(FATAL_ERROR "Building this project in the source directory is not allowed. "
 			"Please remove CMakeCache.txt, create a build directory, and run cmake there, for example:
-rm CMakeCache.txt
-mkdir build && cd build
-cmake ..")
+        rm CMakeCache.txt
+        mkdir build && cd build
+        cmake ..")
   endif()
 endmacro(enforce_out_of_source_build)
 
 
 # removes item from property list of enabled features and adds it to the list of disabled ones
 function(disable_feature FEATURE_NAME)
+	if(DEBUG_OUTPUT)
+		message(STATUS "disable_feature was called with the following arguments:
+        FEATURE_NAME = '${FEATURE_NAME}'")
+	endif()
 	set(${FEATURE_NAME}_FOUND FALSE)
 	get_property(_FEATURES GLOBAL PROPERTY ENABLED_FEATURES)
-	list(REMOVE_ITEM _FEATURES ${FEATURE_NAME})
-	set_property(GLOBAL PROPERTY ENABLED_FEATURES ${_FEATURES})
-	set_property(GLOBAL APPEND PROPERTY DISABLED_FEATURES ${FEATURE_NAME})
+	if(DEBUG_OUTPUT)
+		message(STATUS "Removing feature '${FEATURE_NAME}' from ENABLED_FEATURES = '${_FEATURES}'")
+	endif()
+	list(LENGTH _FEATURES _FEATURES_SIZE)
+	if(_FEATURES_SIZE GREATER 0)
+		list(REMOVE_ITEM _FEATURES ${FEATURE_NAME})
+		set_property(GLOBAL PROPERTY ENABLED_FEATURES ${_FEATURES})
+		set_property(GLOBAL APPEND PROPERTY DISABLED_FEATURES ${FEATURE_NAME})
+	endif()
 endfunction(disable_feature)
