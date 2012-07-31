19f2d3d3df91cf7609325e1c835b14003490053d
Sebastian Neubert <sneubert@users.sourceforge.net>
2012-07-31 16:43:25 +0000

    added 2body phasespace target-beam

--- generators/TDiffractivePhaseSpace.cc
+++ generators/TDiffractivePhaseSpace.cc
@@ -34,6 +34,7 @@
 #include "TFile.h"
 
 #include "reportingUtils.hpp"
+#include "physUtils.hpp"
 #include "TDiffractivePhaseSpace.h"
 
 using namespace std;
@@ -399,7 +400,14 @@ TDiffractivePhaseSpace::event()
 
   const TLorentzVector targetLab(0, 0, 0, _targetMass);
   const TLorentzVector overallCm = _beamLab + targetLab;  // beam-target center-of-mass system
-  
+  // check
+  if(_xMassMax + _targetMass  > overallCm.M()){
+    printErr << "Max Mass out of kinematic range." <<  endl
+	     << "Limit = " << overallCm.M()  - _targetMass << "GeV/c2" << endl
+	     << " ABORTING " << flush<< endl;
+    throw;
+  }
+
   bool              done     = false;
   while (!done) {
     
@@ -503,9 +511,18 @@ TDiffractivePhaseSpace::event()
     ++attempts;
     {
       _phaseSpace.pickMasses(xMass);
+
       // correct weight for phase space splitting
-      const double maxPsWeight = _phaseSpace.maxWeight()  * _xMassMax;
-      const double psWeight    = _phaseSpace.calcWeight() * xMass;
+      // and for 2-body phase space beam-target
+      // (1 / 4pi) * q(sqrt(s), m_x, m_recoil) / sqrt(s)
+      const double ps2bodyWMax = breakupMomentum(sqrtS,_xMassMax,_targetMass)/sqrtS;
+      const double ps2bodyW = breakupMomentum(sqrtS,xMass,_targetMass)/sqrtS;
+
+      const double maxPsWeight = _phaseSpace.maxWeight()  * _xMassMax * ps2bodyWMax;
+      const double psWeight    = _phaseSpace.calcWeight() * xMass* ps2bodyW;
+      
+
+
       if ((psWeight / maxPsWeight) < _phaseSpace.random())
     	continue;
       _phaseSpace.pickAngles();
