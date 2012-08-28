9591ff72a91708bcdd647f6c691e0466c15ffce6
Karl Bicker <kbicker@cern.ch>
2012-08-28 13:25:56 +0000

    Redid the particleProperties' python interface.
    Did everything from hand this time, dispensing with the wrapper that the
    PyPlusPlus utility constructed. This wrapper is only needed if a class is
    derived from in python and then used in C++, which will hopefully never be the
    case.

--- pyInterface/particleProperties_py.h
+++ pyInterface/particleProperties_py.h
@@ -5,45 +5,6 @@
 
 #include "particleProperties.h"
 
-namespace bp = boost::python;
-
-struct particleProperties_wrapper : rpwa::particleProperties, bp::wrapper< rpwa::particleProperties > {
-
-	particleProperties_wrapper( )
-		: rpwa::particleProperties( )
-		  , bp::wrapper< rpwa::particleProperties >(){
-			  // null constructor
-
-		  }
-
-	particleProperties_wrapper(::rpwa::particleProperties const & partProp )
-		: rpwa::particleProperties( boost::ref(partProp) )
-		  , bp::wrapper< rpwa::particleProperties >(){
-			  // copy constructor
-
-		  }
-
-	particleProperties_wrapper(::std::string const & partName, int const isospin, int const G, int const J, int const P, int const C )
-		: rpwa::particleProperties( partName, isospin, G, J, P, C )
-		  , bp::wrapper< rpwa::particleProperties >(){
-			  // constructor
-
-		  }
-
-	virtual ::std::string qnSummary(  ) const  {
-		if( bp::override func_qnSummary = this->get_override( "qnSummary" ) )
-			return func_qnSummary(  );
-		else
-			return this->rpwa::particleProperties::qnSummary(  );
-	}
-
-
-	::std::string default_qnSummary(  ) const  {
-		return rpwa::particleProperties::qnSummary( );
-	}
-
-};
-
 void exportParticleProperties();
 
 #endif
