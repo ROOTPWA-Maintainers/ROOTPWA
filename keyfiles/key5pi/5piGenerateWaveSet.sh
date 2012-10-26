8c32dcebcd03f21b19d9542ea544a10ca5f3a0b2
Boris Grube <bgrube@users.sourceforge.net>
2012-10-26 17:08:52 +0000

    - created own particle data table for 5-pion analysis; removed   unnecessary particles; corrected width of b0 and b2(1800)
    - added checking of keyfiles with test data to
      keyfiles/key5pi/5piGenerateWaveSet.sh; added correspnding test data
      file keyfiles/key5pi/5piTestData.root with 100 events

--- keyfiles/key5pi/5piGenerateWaveSet.sh
+++ keyfiles/key5pi/5piGenerateWaveSet.sh
@@ -16,9 +16,10 @@
 #-------------------------------------------------------------------------
 
 
-PARTICLE_DATA_TABLE="../../amplitude/particleDataTable.txt"
+PARTICLE_DATA_TABLE="./5piParticleDataTable.txt"
 PARTICLE_DECAY_LIST="./5piParticleDecays.txt"
 TEMPLATE_KEY_FILES="5pi3pi2pi.template.key 5pi4pi2pi2pi.template.key 5pi4pi3pi.template.key"
+TEST_DATA="./5piTestData.root"
 
 
 for TEMPLATE_KEY_FILE in ${TEMPLATE_KEY_FILES}
@@ -30,8 +31,8 @@ do
 				mkdir --verbose ${DEST_DIR}
 		fi
 		LOG_FILE=${DEST_DIR}/${TEMPLATE_KEY_FILE}.log
-		CMD="generateWaveSet -p \"${PARTICLE_DATA_TABLE}\" -d \"${PARTICLE_DECAY_LIST}\" -f -o \"${DEST_DIR}\" -n -k ${TEMPLATE_KEY_FILE} &> ${LOG_FILE}"
-		echo ${CMD}		
+		CMD="generateWaveSet -p \"${PARTICLE_DATA_TABLE}\" -d \"${PARTICLE_DECAY_LIST}\" -f -o \"${DEST_DIR}\" -n -k \"${TEMPLATE_KEY_FILE}\" &> \"${LOG_FILE}\""
+		echo "${CMD}"
 		eval ${CMD}
 		# postprocess key files
 		for KEY_FILE in ${DEST_DIR}/*.key
@@ -39,10 +40,25 @@ do
 				./5piPostProcessKeyFiles.sed "${KEY_FILE}"
 		done
 		# create backup copy of template key file
-		cp --verbose --force "${TEMPLATE_KEY_FILE}" "${DEST_DIR}"
+		cp --verbose --force "${TEMPLATE_KEY_FILE}" "${DEST_DIR}/${TEMPLATE_KEY_FILE}.bak"
 		
 done
 
+
+# check key files
+if [[ -f "${TEST_DATA}" ]]
+then
+		for TEMPLATE_KEY_FILE in ${TEMPLATE_KEY_FILES}
+		do
+				DEST_DIR=${TEMPLATE_KEY_FILE%.template.key}
+				LOG_FILE=${DEST_DIR}/${TEMPLATE_KEY_FILE}.check.log
+				CMD="checkKeyFile -d \"${TEST_DATA}\" -p \"${PARTICLE_DATA_TABLE}\" \"${DEST_DIR}\"/*.key &> \"${LOG_FILE}\""
+				echo "${CMD}"
+				eval ${CMD}
+		done
+else
+		echo "cannot find test data file '${TEST_DATA}'. skipping amplitude test."
+fi
 
 
 exit 0
