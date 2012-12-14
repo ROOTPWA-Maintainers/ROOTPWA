ef52c416ffa4fa44b1dbac1342bedfee34b742fd
Karl Bicker <kbicker@cern.ch>
2012-12-14 07:35:21 +0000

    plotAngles.py now produces plots.
    Add the code to do the transformation to the relevant reference frames. Also
    save the calculated quantities in histograms. Add a function to randomize the
    order of the input vectors in the TClonesArrays.

--- pyInterface/plotAngles.py
+++ pyInterface/plotAngles.py
@@ -7,6 +7,22 @@ import time
 import pyRootPwa
 import pyRootPwa.utils
 
+def randomizeTClonesArray(array):
+	entries = array.GetEntries()
+	if entries == 0:
+		return array
+	retval = pyRootPwa.ROOT.TClonesArray("TVector3")
+	map = []
+	for i in range(entries):
+		while True:
+			num = int(round(pyRootPwa.ROOT.gRandom.Uniform(-0.5, entries-0.5)))
+			if num not in map:
+				map.append(num)
+				break
+	for i in range(entries):
+		retval[i] = array[map[i]]
+	return retval
+
 if __name__ == "__main__":
 
 	# parse command line arguments
@@ -14,6 +30,7 @@ if __name__ == "__main__":
 	                                 description="No help text available."
 	                                )
 	parser.add_argument("dataFile", metavar="data-file", help="path to data file")
+	parser.add_argument("outputFile", metavar="output-file", help="path to output file")
 	parser.add_argument("templateFile", metavar="template-file", help="path to template file")
 	parser.add_argument("-c", type=str, metavar="config-file", default="rootpwa.config", dest="configFileName", help="path to config file (default: ./rootpwa.config)")
 	arguments = parser.parse_args()
@@ -39,6 +56,14 @@ if __name__ == "__main__":
 		pyRootPwa.utils.printErr("could not construct topology. Aborting...")
 		sys.exit(5)
 
+	outputFile = pyRootPwa.ROOT.TFile.Open(arguments.outputFile, "RECREATE")
+
+	hists = []
+	for i in range(topology.nmbDecayVertices()):
+		hists.append([pyRootPwa.ROOT.TH1D("phi" + str(i), "phi" + str(i), 100, -3.142, 3.142),
+		              pyRootPwa.ROOT.TH1D("theta" + str(i), "theta" + str(i), 100, -1, 1),
+		              pyRootPwa.ROOT.TH1D("m" + str(i), "m" + str(i), 100, 0, 5)])
+
 	dataFile = pyRootPwa.ROOT.TFile.Open(arguments.dataFile)
 	dataTree = dataFile.Get(pyRootPwa.config.inTreeName)
 
@@ -59,7 +84,35 @@ if __name__ == "__main__":
 
 	for i in range(nEvents):
 		dataTree.GetEntry(i)
-		topology.readKinematicsData(prodKinMomenta, decayKinMomenta)
+
+		# Transform all particles
+		topology.readKinematicsData(prodKinMomenta, randomizeTClonesArray(decayKinMomenta))
+		topology.calcIsobarLzVec()
+		beamLv = topology.productionVertex().referenceLzVec()
+		XLv = topology.XParticle().lzVec
+		gjTrans = pyRootPwa.core.isobarAmplitude.gjTransform(beamLv, XLv)
+		for vertex in topology.isobarDecayVertices():
+			vertex.transformOutParticles(gjTrans)
+		for vertex in topology.isobarDecayVertices()[1:]:
+			hfTrans = pyRootPwa.core.isobarHelicityAmplitude.hfTransform(vertex.parent().lzVec)
+			for subVertex in topology.subDecay(vertex).decayVertices():
+				subVertex.transformOutParticles(hfTrans)
+
+		hist_i = 0
+		for vertex in topology.isobarDecayVertices():
+			daughter = vertex.daughter1()
+			parent = vertex.parent()
+			mass = parent.lzVec.M()
+			phi = daughter.lzVec.Phi()
+			theta = daughter.lzVec.Theta()
+			hists[hist_i][0].Fill(phi)
+			hists[hist_i][1].Fill(pyRootPwa.ROOT.TMath.Cos(theta))
+			hists[hist_i][2].Fill(mass)
+			hist_i += 1
+
 		progressbar.update(i)
+
+	outputFile.Write()
+	outputFile.Close()
 
 	pyRootPwa.utils.printPrintingSummary(printingCounter)
