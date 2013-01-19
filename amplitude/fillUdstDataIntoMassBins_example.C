73452ece40956ed12e24879d01ea01a1c99678dd
Sebastian Uhl <sebastianuhl@users.sourceforge.net>
2013-01-19 21:15:59 +0000

    - correct calculation of tPrime, calculation of tMin was wrong

--- amplitude/fillUdstDataIntoMassBins_example.C
+++ amplitude/fillUdstDataIntoMassBins_example.C
@@ -323,7 +323,7 @@ fillUdstDataIntoMassBins_example(const string&      inFileNamePattern = "fillUds
 		const TLorentzVector X = piZeros[0] + piZeros[1] + *piMinus;
 		// calculate t'
 		const double t      = (*beam - X) * (*beam - X);
-		const double tPrime = fabs(t) - fabs((X.M() * X.M() - beam->M() * beam->M())
+		const double tPrime = fabs(t) - fabs((X.M() * X.M() - beam->M() * beam->M())*(X.M() * X.M() - beam->M() * beam->M())
 		                                     / (4 * (beam->Vect() * beam->Vect())));
 		// cut on t'
 		if ((tPrime < 0.1) or (tPrime > 1.0))
