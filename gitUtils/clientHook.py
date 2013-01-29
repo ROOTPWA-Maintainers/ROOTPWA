4b8177322f9ba4d58b143e34f74dd57343bf2209
Karl Bicker <kbicker@cern.ch>
2013-01-29 12:30:11 +0100

    gitUtils: Fix error messages.
    Change the error messages of the clientHook.py so that it refers to "commit"
    rather than "push".

--- gitUtils/clientHook.py
+++ gitUtils/clientHook.py
@@ -16,19 +16,19 @@ for line in prelimCommitMessage:
 	commitMessage.append(line.rstrip('\n'))
 
 if len(commitMessage) < 3:
-	sys.stderr.write("[POLICY] Commit message has less than 3 lines. Aborting push...\n")
+	sys.stderr.write("[POLICY] Commit message has less than 3 lines. Aborting commit...\n")
 	sys.exit(1)
 if len(commitMessage[0]) > 80 or len(commitMessage[0]) == 0:
-	sys.stderr.write("[POLICY] Commit message's first line is longer than 80 characters. Aborting push...\n")
+	sys.stderr.write("[POLICY] Commit message's first line is longer than 80 characters. Aborting commit...\n")
 	sys.exit(1)
 if len(commitMessage[1]) != 0:
-	sys.stderr.write("[POLICY] Commit message's second line is not empty. Aborting push...\n")
+	sys.stderr.write("[POLICY] Commit message's second line is not empty. Aborting commit...\n")
 	sys.exit(1)
 if len(commitMessage[2]) == 0:
-	sys.stderr.write("[POLICY] Commit message's third line must not be empty. Aborting push...\n")
+	sys.stderr.write("[POLICY] Commit message's third line must not be empty. Aborting commit...\n")
 	sys.exit(1)
 for line in commitMessage[2:]:
 	if len(line) > 80:
-		sys.stderr.write("[POLICY] Commit message has a line longer than 80 characters. Aborting push...\n")
+		sys.stderr.write("[POLICY] Commit message has a line longer than 80 characters. Aborting commit...\n")
 		sys.exit(1)
 
