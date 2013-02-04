1e7a2fa9b4702001ee700ea756cd4803421257ce
Karl Bicker <kbicker@cern.ch>
2013-02-04 17:03:23 +0100

    gitUtils: Save commit message in clientHook.py
    Make the clientHook.py save the commit message to a file in case it let's the
    commit fail so that the commit message is not lost.
    
    Fix for bug #12.

--- gitUtils/clientHook.py
+++ gitUtils/clientHook.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import os
 
 
 if __name__ == "__main__":
@@ -8,7 +9,6 @@ if __name__ == "__main__":
 	msgFileName = sys.argv[1]
 
 	with open(msgFileName, 'r') as msgFile:
-
 		prelimCommitMessage = msgFile.readlines()
 
 	commitMessage = []
@@ -17,19 +17,31 @@ if __name__ == "__main__":
 			continue
 		commitMessage.append(line.rstrip('\n'))
 
+	error = False
 	if len(commitMessage) < 3:
 		sys.stderr.write("[POLICY] Commit message has less than 3 lines. Aborting commit...\n")
-		sys.exit(1)
-	if len(commitMessage[0]) > 80 or len(commitMessage[0]) == 0:
+		error = True
+	if not error and (len(commitMessage[0]) > 80 or len(commitMessage[0]) == 0):
 		sys.stderr.write("[POLICY] Commit message's first line is longer than 80 characters. Aborting commit...\n")
-		sys.exit(1)
-	if len(commitMessage[1]) != 0:
+		error = True
+	if not error and (len(commitMessage[1]) != 0):
 		sys.stderr.write("[POLICY] Commit message's second line is not empty. Aborting commit...\n")
-		sys.exit(1)
-	if len(commitMessage[2]) == 0:
+		error = True
+	if not error and (len(commitMessage[2]) == 0):
 		sys.stderr.write("[POLICY] Commit message's third line must not be empty. Aborting commit...\n")
+		error = True
+	if not error:
+		for line in commitMessage[2:]:
+			if len(line) > 80:
+				sys.stderr.write("[POLICY] Commit message has a line longer than 80 characters. Aborting commit...\n")
+				error = True
+
+	if error:
+		i = 1
+		commitMessageFileName = "git-commit.tmp"
+		while os.path.isfile(commitMessageFileName):
+			commitMessageFileName = "git-commit_" + str(i) + ".tmp"
+			i += 1
+		os.system("cp " + msgFileName + " " + commitMessageFileName)
+		print("Could not commit, your commit message has been saved to '" + commitMessageFileName + "'")
 		sys.exit(1)
-	for line in commitMessage[2:]:
-		if len(line) > 80:
-			sys.stderr.write("[POLICY] Commit message has a line longer than 80 characters. Aborting commit...\n")
-			sys.exit(1)
