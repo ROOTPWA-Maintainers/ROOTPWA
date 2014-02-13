3f9496ec342a28867ecc33da82bf683e36116397
Karl Bicker <kbicker@cern.ch>
2014-02-13 17:37:13 +0100

    pyInterface: First version of genPseudoData.
    Add all the code necessary to make genPseudoData do what it is supposed to do.
    The script is now generating data, calculating the decay amplitudes on the fly,
    and producing an "data-like" file (i.e. same data structure complete with the
    weight) which can be used with hit and miss Monte Carlo to produce "artificial
    data". It has been tested against calcAmplitudes.py/evtweight and produces the
    same weights.
    
    In this version, a lot of debug output is printed. This will be removed
    in the next commit.

--- pyInterface/genPseudoData.py
+++ pyInterface/genPseudoData.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python
 
 import argparse
+import math
 import multiprocessing
+import numpy
 import os.path
 import sys
 
 import pyRootPwa
+
+def norm(c):
+	return (c.real*c.real + c.imag*c.imag)
+
+def getBestFitResult(massBinCenter, fitResultTree):
+	fitResult = pyRootPwa.core.fitResult().getAsRootObject()
+	fitResultTree.SetBranchAddress(config.fitResultBranchName, fitResult)
+	massBinCenterBest = 0.
+	bestIndex = 0
+	bestLikeli = 0.
+	for i in range(fitResultTree.GetEntries()):
+		fitResultTree.GetEntry(i)
+		mass = fitResult.massBinCenter()
+		logLike = fitResult.logLikelihood()
+		if i == 0 or abs(massBinCenter - mass) < abs(massBinCenter - massBinCenterBest):
+			bestIndex = i
+			bestLikeli = logLike
+			massBinCenterBest = mass
+		elif abs(massBinCenter - mass) == abs(massBinCenter - massBinCenterBest):
+			if logLike < bestLike:
+				bestIndex = i
+				massBinCenterBest = mass
+				logLikeBest = logLike
+	fitResultTree.GetEntry(bestIndex)
+	return pyRootPwa.core.fitResult(fitResult)
+
 
 if __name__ == "__main__":
 
@@ -14,17 +42,16 @@ if __name__ == "__main__":
 	                                )
 
 	parser.add_argument("reactionFile", type=str, metavar="reactionFile", help="reaction config file")
+	parser.add_argument("fitResult", type=str, metavar="fitResult", help="fitResult to get the production amplitudes")
+	parser.add_argument("integralFile", type=str, metavar="integralFile", help="integral file")
+	parser.add_argument("outputFile", type=str, metavar="outputFile", help="output root file")
+	parser.add_argument("-c", type=str, metavar="config-file", default="rootpwa.config", dest="configFileName", help="path to config file (default: ./rootpwa.config)")
 	parser.add_argument("-n", type=int, metavar="#", dest="nEvents", default=100, help="(max) number of events to generate (default: 100)")
-	parser.add_argument("-a", type=int, metavar="#", dest="maxAttempts", default=0, help="(max) number of attempts to do (default: infinity)")
-	parser.add_argument("-o", type=str, metavar="<outputFile>", dest="outputFileName", default="", help="ASCII output file (if not specified, generated automatically)")
 	parser.add_argument("-p", type=str, metavar="<particleDataTaple>", dest="particleDataTableFileName", default="./particleDataTable.txt", help="path to particle data table file (default: ./particleDataTable.txt)")
-	parser.add_argument("-c", action="store_true", dest="comgeantOutput", help="if present, a comgeant eventfile (.fort.26) is written with same naming as the root file")
 	parser.add_argument("-s", type=int, metavar="#", dest="seed", default=123456, help="random number generator seed (default: 123456)")
 	parser.add_argument("-M", type=float, metavar="#", dest="massLowerBinBoundary", help="lower boundary of mass range in MeV (overwrites values from reaction file)")
 	parser.add_argument("-B", type=float, metavar="#", dest="massBinWidth", help="width of mass bin in MeV")
-	parser.add_argument("--beamfile", type=str, metavar="<beamFile>", dest="beamFileName", help="path to beam file (overrides values from config file)")
-	parser.add_argument("--noRandomBeam", action="store_true", dest="noRandomBeam", help="read the events from the beamfile sequentially")
-	parser.add_argument("--randomBlockBeam", action="store_true", dest="randomBlockBeam", help="like --noRandomBeam but with random starting position")
+	parser.add_argument("-k", "--keyfiles", type=str, metavar="keyfiles", dest="keyfiles", nargs="*", help="keyfiles to calculate amplitude for (overrides settings from the config file)")
 
 	args = parser.parse_args()
 
@@ -50,9 +77,15 @@ if __name__ == "__main__":
 	printInfo = pyRootPwa.utils.printInfo
 	printDebug = pyRootPwa.utils.printDebug
 
-	if args.maxAttempts and args.maxAttempts < args.nEvents:
-		printWarn("Maximum attempts is smaller than the number of events. Setting it to infinity.")
-		args.maxAttempts = 0
+	pyRootPwa.core.particleDataTable.instance.readFile(args.particleDataTableFileName)
+
+	config = pyRootPwa.rootPwaConfig(args.configFileName)
+
+	integral = pyRootPwa.core.ampIntegralMatrix()
+	if not integral.readAscii(args.integralFile):
+		printErr("Cannot read normalization integral from file '" + args.integralFile + "'. Aborting...")
+		sys.exit(1)
+	nmbNormEvents = integral.nmbEvents()
 
 	overrideMass = (args.massLowerBinBoundary is not None) or (args.massBinWidth is not None)
 
@@ -63,11 +96,7 @@ if __name__ == "__main__":
 	printInfo("Setting random seed to " + str(args.seed))
 	pyRootPwa.core.randomNumberGenerator.instance.setSeed(args.seed)
 
-	pyRootPwa.core.particleDataTable.instance.readFile(args.particleDataTableFileName)
-
 	generatorManager = pyRootPwa.core.generatorManager()
-	if args.beamFileName is not None:
-		generatorManager.overrideBeamFile(args.beamFileName)
 
 	if not generatorManager.readReactionFile(args.reactionFile):
 		printErr("could not read reaction file. Aborting...")
@@ -75,64 +104,156 @@ if __name__ == "__main__":
 
 	if overrideMass:
 		generatorManager.overrideMassRange(args.massLowerBinBoundary / 1000., (args.massLowerBinBoundary + args.massBinWidth) / 1000.)
-	if args.noRandomBeam:
-		generatorManager.readBeamfileSequentially()
-	if args.randomBlockBeam:
-		generatorManager.readBeamfileSequentially
-		generatorManager.randomizeBeamfileStartingPosition()
 
 	if not generatorManager.initializeGenerator():
 		printErr("could not initialize generator. Aborting...")
 		sys.exit(1)
 
-	if args.outputFileName == "":
-		if overrideMass:
-			args.outputFileName = str(args.massLowerBinBoundary) + "." + str(args.massLowerBinBoundary + args.massBinWidth) + ".phaseSpace.evt"
-		else:
-			index = 1
-			filename = "1.phaseSpace.evt"
-			while os.path.exists(filename):
-				index += 1
-				filename = str(index) + ".phaseSpace.evt"
-			args.outputFileName = filename
+	keyfiles = []
+	if args.keyfiles is not None:
+		keyfiles =  args.keyfiles
+		for keyfile in keyfiles:
+			if not (os.path.isfile(keyfile) and keyfile.endswith(".key")):
+				pyRootPwa.utils.printErr("Keyfile '" + keyfile + "' not valid. Aborting...")
+				sys.exit(1)
+	else:
+		keyfiles = pyRootPwa.utils.getListOfKeyfiles(pyRootPwa.config.keyfilePattern)
+		if len(keyfiles) == 0:
+			pyRootPwa.utils.printErr("No keyfiles found with valid file extension. Aborting...")
+			sys.exit(1)
 
-	outputEvtFile = open(args.outputFileName, 'w')
-	printInfo("opened output event file: " + args.outputFileName)
-	outputComgeantFile = None
-	if args.comgeantOutput:
-		outputComgeantFile = open(args.outputFileName.replace(".evt", ".fort.26"), 'w')
-		printInfo("opened output comgeant file: " + args.outputFileName)
+	fitResultFile = pyRootPwa.ROOT.TFile.Open(args.fitResult, "READ")
+	if not fitResultFile:
+		printErr("Could not open fit result file. Aborting...")
+		sys.exit(1)
+	fitResultTree = fitResultFile.Get(config.fitResultTreeName)
+	if not fitResultTree:
+		printErr("Could not find fit result tree '" + config.fitResultTreeName +
+		         "' in file '" + args.fitResult + "'. Aborting...")
+		sys.exit(1)
+	massBinCenterBest = 0
+	massRange = generatorManager.getGenerator().getTPrimeAndMassPicker().massRange()
+	massBinCenter = (massRange[0] + massRange[1]) / 2.
+	fitResult = getBestFitResult(massBinCenter, fitResultTree)
+
+	waveDescriptions = []
+	amplitudes = []
+	reflectivities = []
+	prodAmps = []
+	waveNames = []
+	print(len(keyfiles))
+	for keyfile in keyfiles:
+		waveName = keyfile[keyfile.rfind('/')+1:].replace(".key", ".amp")
+		waveNames.append(waveName)
+		reflectivities.append(1 if waveName[6] == '+' else -1)
+		waveIndex = fitResult.waveIndex(waveName)
+		if waveIndex < 0:
+			printErr("Keyfile '" + keyfile + "' not found in fit result file '" + args.fitResult +"'. Aborting...")
+			sys.exit(1)
+		prodAmps.append(fitResult.prodAmp(waveIndex))
+		waveDescription = pyRootPwa.core.waveDescription()
+		waveDescription.parseKeyFile(keyfile)
+		(result, amplitude) = waveDescription.constructAmplitude()
+		if not result:
+			pyRootPwa.utils.printErr('Could not construct amplitude for keyfile "' + keyfile + '".')
+			sys.exit(1)
+		amplitude.init()
+		print(amplitude)
+		waveDescriptions.append(waveDescription)
+		amplitudes.append(amplitude)
+
+	outputFile = pyRootPwa.ROOT.TFile.Open(args.outputFile, "NEW")
+	if not outputFile:
+		printErr("could not open output file. Aborting...")
+		sys.exit(1)
+
+	printInfo("opened output root file: " + args.outputFile)
 	try:
 		print(generatorManager)
 		progressBar = pyRootPwa.utils.progressBar(0, args.nEvents-1, sys.stdout)
 		progressBar.start()
 		attempts = 0
-		eventsGenerated = 0
+		decayKin = None
+		prodKin = None
+		first = True
+		outTree = None
+		weight = numpy.zeros(1, dtype = float)
 
-		while eventsGenerated < args.nEvents:
+		for eventsGenerated in range(args.nEvents):
 
 				attempts += generatorManager.event()
 				generator = generatorManager.getGenerator()
 				beam = generator.getGeneratedBeam()
 				finalState = generator.getGeneratedFinalState()
-				outputEvtFile.write(generator.convertEventToAscii(beam, finalState))
-				if args.comgeantOutput:
-					recoil = generator.getGeneratedRecoil()
-					vertex = generator.getGeneratedVertex()
-					outputComgeantFile.write(generator.convertEventToComgeant(beam, recoil, vertex, finalState, False))
-				if args.maxAttempts and attempts > args.maxAttempts:
-					printWarn("reached maximum attempts. Aborting...")
-					break
-				eventsGenerated += 1
+
+				if first:
+					nDecayParticles = len(finalState)
+					decayKinNames = pyRootPwa.ROOT.TClonesArray("TObjString", len(finalState))
+					for i in range(len(finalState)):
+						decayKinNames[i] = pyRootPwa.ROOT.TObjString(finalState[i].name)
+					prodKinNames = pyRootPwa.ROOT.TClonesArray("TObjString", 1)
+					prodKinNames[0] = pyRootPwa.ROOT.TObjString(beam.name)
+					for amplitude in amplitudes:
+						topo = amplitude.decayTopology()
+						if not topo.initKinematicsData(prodKinNames, decayKinNames):
+							pyRootPwa.utils.printErr('Could not initialize kinematics Data. Aborting...')
+							sys.exit(1)
+					decayKin = pyRootPwa.ROOT.TClonesArray("TVector3", len(finalState))
+					prodKin = pyRootPwa.ROOT.TClonesArray("TVector3", 1)
+					outTree = pyRootPwa.ROOT.TTree(config.inTreeName, config.inTreeName)
+					outTree.Branch(config.prodKinMomentaLeafName, "TClonesArray", prodKin, 256000, 99)
+					outTree.Branch(config.decayKinMomentaLeafName, "TClonesArray", decayKin, 256000, 99)
+					outTree.Branch("weight", weight, "weight/D")
+					prodKinNames.Write(config.prodKinPartNamesObjName, pyRootPwa.ROOT.TObject.kSingleKey)
+					decayKinNames.Write(config.decayKinPartNamesObjName, pyRootPwa.ROOT.TObject.kSingleKey)
+					first = False
+
+				for i in range(len(finalState)):
+					decayKin[i] = finalState[i].lzVec.Vect()
+				prodKin[0] = beam.lzVec.Vect()
+
+				posReflAmpSum = 0
+				negReflAmpSum = 0
+
+				print("############## event " + str(eventsGenerated) + "##############")
+				for i in range(len(amplitudes)):
+					amplitude = amplitudes[i]
+					topo = amplitude.decayTopology()
+					if not topo.readKinematicsData(prodKin, decayKin):
+						progressBar.cancel()
+						pyRootPwa.utils.printErr('Could not read kinematics data. Aborting...')
+						sys.exit(1)
+
+					print("waveName = " + waveNames[i])
+					decayAmp = amplitude()
+					amp = (decayAmp * prodAmps[i]) / math.sqrt(integral.element(waveNames[i], waveNames[i]).real * nmbNormEvents)
+					print(str(amp) + " = " + str(decayAmp) + " * " + str(prodAmps[i]) + " / sqrt(" + str(integral.element(waveNames[i], waveNames[i]).real) + " * " + str(nmbNormEvents) + ")")
+					if reflectivities[i] > 0:
+						posReflAmpSum += amp
+					else:
+						negReflAmpSum += amp
+
+				weight[0] = norm(posReflAmpSum) + norm(negReflAmpSum)
+				print("-------------")
+				print("posReflAmpSum=" + str(posReflAmpSum))
+				print("negReflAmpSum=" + str(negReflAmpSum))
+				print("-------------")
+				print(str(weight) + " = " + str(norm(posReflAmpSum)) + " + " + str(norm(negReflAmpSum)) + " + 0")
+				print("weight = " + str(weight))
+				print("########## end event " + str(eventsGenerated) + "##############")
+				outTree.Fill()
+
 				progressBar.update(eventsGenerated)
-
+	except:
+		raise
+	else:
+		outTree.Write()
 	finally:
-		outputEvtFile.close()
-		if outputComgeantFile is not None:
-			outputComgeantFile.close()
+		outputFile.Close()
 
+	eventsGenerated += 1
 	printSucc("generated " + str(eventsGenerated) + " events.")
 	printInfo("attempts: " + str(attempts))
-	printInfo("efficiency: " + str(100. * (float(eventsGenerated) / float(attempts))))
+	printInfo("efficiency: " + str(100. * (float(eventsGenerated) / float(attempts))) + "%")
 
 	pyRootPwa.utils.printPrintingSummary(printingCounter)
