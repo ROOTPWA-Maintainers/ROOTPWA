b5d168fcbad1a2ec4c5a158053e4b6a350957e4b
Karl Bicker <kbicker@cern.ch>
2014-07-01 20:29:48 +0200

    keyfiles: Remove gamp keyfiles.
    Remove all the old gamp keyfiles which have become obsolete after the removal
    of the pwa2000 package.

--- keyfiles/key5pi/fixMode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in *.key ;
-  do cp $i $i.asc;
-     sed 's/ascii/binary/g' $i.asc > $i;
-done ;
--- keyfiles/keyKpipi/scripts/setup_Kpipi_workspace.sh
+++
@@ -1,4 +0,0 @@
-#source setup_new_workspace.sh /mnt/disk2/analysis/Kp_Kppipi/PWA2/SETX/ 110 800 3000 /mnt/disk2/analysis/Kp_Kppipi/PWA_output_test/hist.root events/tree_events 4
-#source setup_new_workspace.sh /mnt/disk2/analysis/Kp_Kppipi/PWA2/SETX_40MeV/ 55 800 3000 /mnt/disk2/analysis/Kp_Kppipi/PWA_output_test/hist.root events/tree_events 4
-#source setup_new_workspace.sh /mnt/disk2/analysis/Kp_Kppipi/PWA2/SET_WA03/ 110 800 3000 /mnt/disk2/analysis/Kp_Kppipi/PWA_output_test/hist.root events/tree_events 4
-source setup_new_workspace.sh /mnt/disk2/analysis/Kp_Kppipi/PWA2/SET_test/ 110 800 3000 /mnt/disk2/analysis/Kp_Kppipi/PWA_output_test/hist.root events/tree_events 4
