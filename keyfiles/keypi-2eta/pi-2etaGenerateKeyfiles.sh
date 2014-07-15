9a42cc360c24b49ce5c0b9dd5e24fb13c3750952
Sebastian Uhl <suhl@cern.ch>
2014-07-15 14:29:58 +0200

    keyfiles: add keyfiles for pi- eta eta
    Add template keyfiles for the pi- eta eta channel. The script and README
    is a closecopy of what is done for the pi- pi0 pi0 channel.

---
+++ keyfiles/keypi-2eta/pi-2etaGenerateKeyfiles.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+[[ -z ${DESTINATION_DIR} ]] && DESTINATION_DIR="keyfiles"
+[[ -z ${PARTICLE_DATA_TABLE} ]] && PARTICLE_DATA_TABLE="../../amplitude/particleDataTable.txt"
+[[ -z ${WAVESET_FILES} ]] && WAVESET_FILES=""
+
+TEMPLATE_KEY_FILES="pi-2eta.etaeta.template.key pi-2eta.pi-eta.template.key"
+
+# if WAVESET_FILES is not empty, only keep those keyfiles actually used in one
+# of the wavesets.
+
+# check and create directory to contain the keyfiles
+if [[ ! -d ${DESTINATION_DIR} ]]
+then
+	mkdir -p ${DESTINATION_DIR}
+fi
+
+# generate the keyfiles
+for TEMPLATE_KEY_FILE in ${TEMPLATE_KEY_FILES}
+do
+	generateWaveSet -p ${PARTICLE_DATA_TABLE} -o ${DESTINATION_DIR} -k ${TEMPLATE_KEY_FILE}
+done
+
+if [[ ! -z "${WAVESET_FILES}" ]]
+then
+	# copy wavesets to destination dir
+	ALL_WAVESET_FILES=
+	for WAVESET_FILE in ${WAVESET_FILES}
+	do
+		if [[ ! -e ${WAVESET_FILE} ]]
+		then
+			echo "Waveset file '${WAVESET_FILE}' does not exist."
+		else
+			cp ${WAVESET_FILE} ${DESTINATION_DIR}
+			ALL_WAVESET_FILES="${ALL_WAVESET_FILES} ${DESTINATION_DIR}/${WAVESET_FILE}"
+		fi
+	done
+	# create list of all waves in wavesets (removing the thresholds)
+	awk '{print $1}' ${ALL_WAVESET_FILES} | sed -e 's/\.amp$//' | awk '{print $0".key"}' | sort -u > temp.waves.keep
+	# create list of all keyfiles just created
+	for i in `ls -1 ${DESTINATION_DIR}/*.key` ; do basename $i ; done | sort -u > temp.waves.all
+	if [[ `diff temp.waves.all temp.waves.keep | grep "^>" | wc -l` -gt 0 ]]
+	then
+		echo "The wavesets contain waves for which no keyfiles were created."
+		diff temp.waves.all temp.waves.keep | grep "^>" | awk '{print $2}'
+		echo "No keyfiles will be removed!"
+	else
+		rm -rf `diff temp.waves.all temp.waves.keep | grep "^<" | awk "{print \"${DESTINATION_DIR}/\"\\\$2}"`
+	fi
+	rm -rf temp.waves.all temp.waves.keep
+fi
