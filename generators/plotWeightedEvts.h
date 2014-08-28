90e1b299b493188fc2a831e43b13d0e98d4b0378
Karl Bicker <kbicker@cern.ch>
2014-08-28 14:48:37 +0200

    generators: Remove header for root script.
    Remove a header file which was used to compile one of the plotting root
    scripts. With the removal of the gui some time ago, these scripts are not
    compiled anymore so this files was superfluous.

--- generators/plotWeightedEvts.h
+++
@@ -1,9 +0,0 @@
-#include "TTree.h"
-#include "TString.h"
-
-#ifndef PLOTWEIGHTEDEVTS_H
-
-void plotWeightedEvts(TTree* mctr, TTree* datatr, TString outfilename="kineplots.root", TString mass="000");
-
-#define PLOTWEIGHTEDEVTS_H
-#endif
