fa776f4a3aa54674596b805b469d5cd99ab3cff3
Karl Bicker <kbicker@cern.ch>
2014-10-13 14:39:28 +0200

    pyInterface: start cleaning up pyInterface/package.
    Start cleaning up the rootpwa python package by moving quite a number of files
    dealing with amplitude calculation to a newly created legacy script directory.
    There is no replacement at the moment, but this will be added over the course
    of the next few commits.

--- pyInterface/package/__init__.py
+++ pyInterface/package/__init__.py
@@ -1,12 +1,9 @@
 
 import ROOT
 
-from _calcAmplitudes import calcAmplitudes
 from _config import rootPwaConfig
 from _exception import rootPwaException
 
-del libRootPwaPy
-del _calcAmplitudes
 del _config
 del _exception
 
