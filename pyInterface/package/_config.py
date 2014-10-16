81085316586b10c0c291c279c9d57bd98a382f95
Karl Bicker <kbicker@cern.ch>
2014-10-16 13:25:31 +0200

    pyInterface: clean up config file.
    Delete a number of entries from the rootpwa config file which will not be
    needed for the next incarnation of the user interface scripts.

--- pyInterface/package/_config.py
+++ pyInterface/package/_config.py
@@ -14,9 +14,6 @@ class rootPwaConfig:
 	pdgFileName                            = ""
 	massBinDirectoryNamePattern            = ""
 	dataDirectory                          = ""
-	dataFileExtensionQualifier             = ""
-	phaseSpaceEventFileExtensionQualifier  = ""
-	accCorrPSEventFileExtensionQualifier   = ""
 	phaseSpaceWeightFileExtensionQualifier = ""
 	accCorrPSWeightFileExtensionQualifier  = ""
 
@@ -25,15 +22,7 @@ class rootPwaConfig:
 	dataAmplitudeDirectoryName             = ""
 	phaseSpaceAmpDirectoryName             = ""
 	accCorrPSAmpDirectoryName              = ""
-	amplitudeLeafName                      = ""
-	inTreeName                             = ""
-	prodKinPartNamesObjName                = ""
-	prodKinMomentaLeafName                 = ""
-	decayKinPartNamesObjName               = ""
-	decayKinMomentaLeafName                = ""
 	weightTreeName                         = ""
-	outputFileFormat                       = ""
-	outputCacheSize                        = 0
 
 	# fit section
 	fitResultTreeName                      = ""
@@ -55,7 +44,7 @@ class rootPwaConfig:
 			sys.exit(1)
 
 		try:
-			self.pdgFileName                 = os.path.expanduser(os.path.expandvars(self.config.get('general', 'particleDataTable')))
+			self.pdgFileName = os.path.expanduser(os.path.expandvars(self.config.get('general', 'particleDataTable')))
 
 			if self.config.has_option('general', 'dataDirectory'):
 				self.dataDirectory = os.path.expanduser(os.path.expandvars(self.config.get('general', 'dataDirectory')))
@@ -64,13 +53,6 @@ class rootPwaConfig:
 			else:
 				self.dataDirectory = os.path.abspath(os.path.dirname(configFileName))
 
-			if not self.config.has_option('general', 'dataFileExtensionQualifier'):
-				self.dataFileExtensionQualifier = ""
-			else:
-				self.dataFileExtensionQualifier = self.config.get('general', 'dataFileExtensionQualifier')
-
-			self.phaseSpaceEventFileExtensionQualifier  = self.config.get('general', 'phaseSpaceEventFileExtensionQualifier')
-			self.accCorrPSEventFileExtensionQualifier   = self.config.get('general', 'accCorrPSEventFileExtensionQualifier')
 			self.phaseSpaceWeightFileExtensionQualifier = self.config.get('general', 'phaseSpaceWeightFileExtensionQualifier')
 			self.accCorrPSWeightFileExtensionQualifier  = self.config.get('general', 'accCorrPSWeightFileExtensionQualifier')
 			self.massBinDirectoryNamePattern            = self.config.get('general', 'massBinDirectoryNamePattern')
@@ -79,22 +61,8 @@ class rootPwaConfig:
 			self.dataAmplitudeDirectoryName             = self.config.get('amplitudes', 'dataAmplitudeDirectoryName')
 			self.phaseSpaceAmpDirectoryName             = self.config.get('amplitudes', 'phaseSpaceAmpDirectoryName')
 			self.accCorrPSAmpDirectoryName              = self.config.get('amplitudes', 'accCorrPSAmpDirectoryName')
-			self.amplitudeLeafName                      = self.config.get('amplitudes', 'amplitudeLeafName')
-			self.inTreeName                             = self.config.get('amplitudes', 'inTreeName')
-
-			self.prodKinPartNamesObjName                = self.config.get('amplitudes', 'prodKinPartNamesObjName')
-			self.prodKinMomentaLeafName                 = self.config.get('amplitudes', 'prodKinMomentaLeafName')
-			self.decayKinPartNamesObjName               = self.config.get('amplitudes', 'decayKinPartNamesObjName')
-			self.decayKinMomentaLeafName                = self.config.get('amplitudes', 'decayKinMomentaLeafName')
 
 			self.weightTreeName                         = self.config.get('amplitudes', 'weightTreeName')
-
-			self.outputCacheSize                        = self.config.get('amplitudes', 'outputCacheSize')
-
-			self.outputFileFormat                       = self.config.get('amplitudes', 'outputFileFormat').lower()
-			if not self.outputFileFormat in ['binary', 'ascii', 'root']:
-				pyRootPwa.utils.printErr('"outputFileFormat" option of the "amplitude" section has to be either "binary" or "ascii" (found "' + self.outputFileFormat + '"). Aborting...')
-				sys.exit(1)
 
 			rawKeyfilePattern = rawKeyfilePattern.replace('\n', '').replace(' ', '')
 			if ';' in rawKeyfilePattern:
@@ -114,8 +82,3 @@ class rootPwaConfig:
 		if not os.path.isdir(self.dataDirectory):
 			pyRootPwa.utils.printErr("Data directory invalid. Aborting...")
 			sys.exit(1)
-
-		if self.phaseSpaceEventFileExtensionQualifier == "":
-			pyRootPwa.utils.printWarn("File extension qualifier for the phase space events is empty, no phase space events will be calculated...")
-		if self.accCorrPSEventFileExtensionQualifier == "":
-			pyRootPwa.utils.printWarn("File extension qualifier for the acceptance corrected phase space events is empty, no acc. cor. phase space events will be calculated...")
