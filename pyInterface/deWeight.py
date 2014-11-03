d2f7ae61a7ebb12779b5922b756a04f359a717ed
odrotleff <oliver.drotleff@gmail.com>
2014-11-03 12:46:50 +0100

    pyInterface: some deWeight.py bugfixes
    changed outputFileName to a mandatory command line argument, kept the weights
    in the deweighted files (were deleted before) and did some minor
    changes/improvements.

--- pyInterface/deWeight.py
+++ pyInterface/deWeight.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(
 	                                 description="Deweights ROOTPWA .root file."
 	                                 )
-	parser.add_argument("inputFilename", type=str, metavar="<inputFile>", help="input root file")
-	parser.add_argument("-o", "--outputFile", type=str, metavar="<outputFile>", default="out.root", help="deweighted output root file (if not specified, generated automatically)")
-	parser.add_argument("-f", "--weightFactor", type=float, default=1., metavar="#", dest="weightFactor", help="weight factor (default = 1")
-	parser.add_argument("-s", "--seed", type=int, metavar="#", dest="seed", default=2451083, help="random number seed (default = 2451083")
+	parser.add_argument("inputFileName", type=str, metavar="<inputFile>", help="input RootPwa file")
+	parser.add_argument("outputFileName", type=str, metavar="<outputFile>", help="deweighted output RootPwa file")
+	parser.add_argument("-f", "--weightFactor", type=float, default=1., metavar="#", help="weight factor (default = 1)")
+	parser.add_argument("-s", "--seed", type=int, metavar="#", dest="seed", default=2451083, help="random number seed (default = 2451083)")
 
 	args = parser.parse_args()
 
@@ -23,9 +23,9 @@ if __name__ == "__main__":
 	printSucc = pyRootPwa.utils.printSucc
 	printInfo = pyRootPwa.utils.printInfo
 
-	inputFile = pyRootPwa.ROOT.TFile.Open(args.inputFilename, "READ")
+	inputFile = pyRootPwa.ROOT.TFile.Open(args.inputFileName, "READ")
 	if not inputFile:
-		printErr("error opening input file. Aborting...")
+		printErr("error opening input file '" + args.inputFileName + "'. Aborting...")
 		sys.exit(1)
 
 	metaData = pyRootPwa.core.eventMetadata.readEventFile(inputFile)
@@ -35,46 +35,43 @@ if __name__ == "__main__":
 	maxWeight = 0.
 
 	for event in inputTree:
-		if event.weight>maxWeight:
-			maxWeight=event.weight
+		if event.weight > maxWeight:
+			maxWeight = event.weight
 
 	printInfo("maxWeight: " + str(maxWeight))
 	maxWeight *= args.weightFactor
 	printInfo("adjusted maxWeight to " + str(maxWeight))
 
-	outputFile = pyRootPwa.ROOT.TFile.Open(args.outputFile, "NEW")
+	outputFile = pyRootPwa.ROOT.TFile.Open(args.outputFileName, "NEW")
 	if not outputFile:
-		printErr("output file already exists. Aborting...")
+		printErr("output file '" + args.outputFileName + "' already exists. Aborting...")
 		sys.exit(1)
 
 	fileWriter = pyRootPwa.core.eventFileWriter()
-	if fileWriter.initialize(
-				                                outputFile,
-				                                metaData.userString(),
-				                                pyRootPwa.core.eventMetadata.GENERATED,
-				                                metaData.productionKinematicsParticleNames(),
-				                                metaData.decayKinematicsParticleNames(),
-# TODO: FILL THESE
-				                                {},
-				                                []
-				                                ):
-		pyRootPwa.ROOT.gRandom.SetSeed(args.seed)
-		acceptedEntries = 0
-		overallEntries = 0
-
-		for event in inputTree:
-			normWeight = event.weight / maxWeight
-			cut = pyRootPwa.ROOT.gRandom.Rndm()
-			if normWeight > cut:
-				fileWriter.addEvent(event.prodKinMomenta, event.decayKinMomenta)
-				acceptedEntries += 1
-			overallEntries += 1
-
-		printSucc("efficiency = %.2f" % (acceptedEntries*100./overallEntries) + "%")
-		printSucc("# accepted events = " + str(acceptedEntries))
-
-		fileWriter.finalize()
-	else:
+	if not fileWriter.initialize(outputFile,
+	                             metaData.userString(),
+	                             pyRootPwa.core.eventMetadata.GENERATED,
+	                             metaData.productionKinematicsParticleNames(),
+	                             metaData.decayKinematicsParticleNames(),
+	                             metaData.binningMap(),
+	                             ["weight"]):
 		printErr("could not initialize fileWriter. Aborting...")
+		inputFile.Close()
+		sys.exit(1)
+	pyRootPwa.ROOT.gRandom.SetSeed(args.seed)
+	acceptedEntries = 0
+	overallEntries = 0
+
+	for event in inputTree:
+		normWeight = event.weight / maxWeight
+		cut = pyRootPwa.ROOT.gRandom.Rndm()
+		if normWeight > cut:
+			fileWriter.addEvent(event.prodKinMomenta, event.decayKinMomenta, [event.weight])
+			acceptedEntries += 1
+		overallEntries += 1
+	fileWriter.finalize()
 	inputFile.Close()
-	outputFile.Close()
+
+	printSucc("efficiency = %.2f" % (acceptedEntries*100./overallEntries) + "%")
+	printSucc("# accepted events = " + str(acceptedEntries))
+	printSucc("successfully deweighted input file '" + args.inputFileName + "' and saved as '" + args.outputFileName + "'.")
