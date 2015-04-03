c3822629efa9b4ea8c863eea953b5d931bebd6e8
Sebastian Uhl <suhl@cern.ch>
2015-04-03 18:32:12 +0200

    partialWaveFit: improve 'eigenvectorLikelihoodSlices'
    * reduce number of command line arguments and extract the required
      values from the input fit result:
      - no need to have upper and lower mass limits
      - get wavelist rank from fit result
      - remove one argument not needed at all
      - in principle it should also be possible to feed the integrals from
        the fit result into the likelihood, then also '-n', '-a', '-A'
        could be removed
    * increase similarity between 'eigenvectorLikelihoodSlices.cc' and
      'pwafit.cc' in terms of variable names, and general structure of the
      code

--- partialWaveFit/eigenvectorLikelihoodSlices.cc
+++ partialWaveFit/eigenvectorLikelihoodSlices.cc
@@ -60,7 +60,7 @@
 #include "conversionUtils.hpp"
 #include "pwaLikelihood.h"
 #include "fitResult.h"
-
+#include "partialWaveFitHelper.h"
 #include "amplitudeTreeLeaf.h"
 
 
@@ -78,48 +78,56 @@ usage(const string& progName,
 	     << endl
 	     << "usage:" << endl
 	     << progName
-	     << " -l # -u # -w wavelist -f fitResultFile -o outputFile [-d amplitude directory -R -N -n normfile"
-	     << " [-a normfile] -r rank [-t # -q -h]" << endl
+	     << " [-d amplitude directory -R] -f fitResultFile [-o outfile -N -n normfile"
+	     << " -a normfile -A # normalisation events -C -q -h]" << endl
 	     << "    where:" << endl
-	     << "        -l #       lower edge of mass bin [MeV/c^2]" << endl
-	     << "        -u #       upper edge of mass bin [MeV/c^2]" << endl
-	     << "        -w file    path to wavelist file" << endl
-	     << "        -f file    path to fit result file with parameters" << endl
-	     << "        -o file    path to output file" << endl
-	     << "        -C         use half-Cauchy priors" << endl
 	     << "        -d dir     path to directory with decay amplitude files (default: '.')" << endl
 	     << "        -R         use .root amplitude files (default: false)" << endl
+	     << "        -f file    path to fit result file with parameters" << endl
+	     << "        -o file    path to output file (default: 'eigenSlices.root')" << endl
 	     << "        -N         use normalization of decay amplitudes (default: false)" << endl
 	     << "        -n file    path to normalization integral file (default: 'norm.int')" << endl
 	     << "        -a file    path to acceptance integral file (default: 'norm.int')" << endl
-	     << "        -A #       number of input events to normalize acceptance to" << endl
-	     << "        -r #       rank of spin density matrix (default: 1)" << endl
-	     << "        -t #       minimizer tolerance (default: 0.001)" << endl
+	     << "        -A #       number of input events to normalize acceptance to (default: use number of events from acceptance integral file)" << endl
+	     << "        -C         use half-Cauchy priors" << endl
 	     << "        -q         run quietly (default: false)" << endl
 	     << "        -h         print help" << endl
 	     << endl;
 	exit(errCode);
 }
 
-std::vector<double> getEigenvector(const TMatrixT<double>& eigenvectors, const unsigned int& col) {
+
+std::vector<double>
+getEigenvector(const TMatrixT<double>& eigenvectors,
+               const unsigned int& col)
+{
 	std::vector<double> eigenvector(eigenvectors.GetNrows());
-	for(int r = 0; r < eigenvectors.GetNrows(); r++) {
+	for(int r = 0; r < eigenvectors.GetNrows(); ++r) {
 		eigenvector[r] = eigenvectors[r][col];
 	}
 	return eigenvector;
 }
 
-std::vector<double> getParsForPoint(const std::vector<double>& eigenvector, const std::vector<double>& maximum, const double& eigenvalue, const int& p) {
+
+std::vector<double>
+getParsForPoint(const std::vector<double>& eigenvector,
+                const std::vector<double>& maximum,
+                const double& eigenvalue,
+                const int& p)
+{
 	std::vector<double> newPoint(maximum.size(), 0.);
-	for(unsigned int r = 0; r < maximum.size(); r++) {
+	for(unsigned int r = 0; r < maximum.size(); ++r) {
 		newPoint[r] = maximum[r] + eigenvector[r] * p / 50 * std::sqrt(eigenvalue);
 	}
 	return newPoint;
 }
 
-bool checkNorm(const std::vector<double>& eigenvector) {
+
+bool
+checkNorm(const std::vector<double>& eigenvector)
+{
 	double norm = 0.;
-	for(unsigned int r = 0; r < eigenvector.size(); r++) {
+	for(unsigned int r = 0; r < eigenvector.size(); ++r) {
 		norm += eigenvector[r] * eigenvector[r];
 	}
 	if(std::fabs(norm - 1.) < 1e-14) {
@@ -129,15 +137,19 @@ bool checkNorm(const std::vector<double>& eigenvector) {
 	}
 }
 
-void normalizeVector(std::vector<double>& eigenvector) {
+
+void
+normalizeVector(std::vector<double>& eigenvector)
+{
 	double norm = 0.;
-	for (unsigned int r = 0; r < eigenvector.size(); r++) {
+	for (unsigned int r = 0; r < eigenvector.size(); ++r) {
 		norm += eigenvector[r] * eigenvector[r];
 	}
-	for (unsigned int r = 0; r < eigenvector.size(); r++) {
+	for (unsigned int r = 0; r < eigenvector.size(); ++r) {
 		eigenvector[r] /= std::sqrt(norm);
 	}
 }
+
 
 int
 main(int    argc,
@@ -162,50 +174,33 @@ main(int    argc,
 	// ---------------------------------------------------------------------------
 	// parse command line options
 	const string progName            = argv[0];
-	double       massBinMin          = 0;                      // [MeV/c^2]
-	double       massBinMax          = 0;                      // [MeV/c^2]
-	string       waveListFileName    = "";                     // wavelist filename
-	string       fitResultFileName   = "";                     // fit result filename
-	string       outputFileName      = "eigenSlices.root";     // output filename
-	bool         cauchyPriors        = false;
 	string       ampDirName          = ".";                    // decay amplitude directory name
 	bool         useRootAmps         = false;                  // if true .root amplitude files are read
 	//bool         useRootAmps         = true;                   // if true .root amplitude files are read
+	string       inFileName          = "";                     // input filename
+	string       outFileName         = "eigenSlices.root";     // output filename
 	bool         useNormalizedAmps   = false;                  // if true normalized amplitudes are used
 	string       normIntFileName     = "";                     // file with normalization integrals
 	string       accIntFileName      = "";                     // file with acceptance integrals
 	unsigned int numbAccEvents       = 0;                      // number of events used for acceptance integrals
-	unsigned int rank                = 1;                      // rank of fit
-	double       minimizerTolerance  = 0.001;                   // minimizer tolerance
+	bool         cauchyPriors        = false;
 	bool         quiet               = false;
 	extern char* optarg;
 	// extern int optind;
 	int c;
-	while ((c = getopt(argc, argv, "l:u:w:f:o:Cd:RNn:a:A:r:t:qh")) != -1)
+	while ((c = getopt(argc, argv, "d:Rf:o:Nn:a:A:Cqh")) != -1)
 		switch (c) {
-		case 'l':
-			massBinMin = atof(optarg);
-			break;
-		case 'u':
-			massBinMax = atof(optarg);
-			break;
-		case 'w':
-			waveListFileName = optarg;
-			break;
-		case 'f':
-			fitResultFileName = optarg;
-			break;
-		case 'o':
-			outputFileName = optarg;
-			break;
-		case 'C':
-			cauchyPriors = true;
-			break;
 		case 'd':
 			ampDirName = optarg;
 			break;
 		case 'R':
 			useRootAmps = true;
+			break;
+		case 'f':
+			inFileName = optarg;
+			break;
+		case 'o':
+			outFileName = optarg;
 			break;
 		case 'N':
 			useNormalizedAmps = true;
@@ -219,11 +214,8 @@ main(int    argc,
 		case 'A':
 			numbAccEvents = atoi(optarg);
 			break;
-		case 'r':
-			rank = atoi(optarg);
-			break;
-		case 't':
-			minimizerTolerance = atof(optarg);
+		case 'C':
+			cauchyPriors = true;
 			break;
 		case 'q':
 			quiet = true;
@@ -241,23 +233,57 @@ main(int    argc,
 		printWarn << "using default acceptance normalization integral file "
 		          << "'" << accIntFileName << "'" << endl;
 	}
-	if (waveListFileName.length() <= 1) {
-		printErr << "no wavelist file specified. aborting." << endl;
+	if (inFileName.length() <= 1) {
+		printErr << "no input file name specified. aborting." << endl;
 		usage(progName, 1);
 	}
 	// report parameters
 	printInfo << "running " << progName << " with the following parameters:" << endl;
-	cout << "    mass bin [" <<  massBinMin << ", " <<  massBinMax << "] MeV/c^2" << endl
-	     << "    path to wave list file ......................... '" << waveListFileName << "'" << endl
-	     << "    path to amplitude directory .................... '" << ampDirName       << "'" << endl
-	     << "    use .root amplitude files ...................... "  << yesNo(useRootAmps)      << endl
+	cout << "    path to amplitude directory .................... '" << ampDirName               << "'" << endl
+	     << "    use .root amplitude files ...................... "  << yesNo(useRootAmps)       << endl
+	     << "    path to input file ............................. '" << inFileName               << "'" << endl
+	     << "    path to output file ............................ '" << outFileName              << "'" << endl
 	     << "    use normalization .............................. "  << yesNo(useNormalizedAmps) << endl
-	     << "    path to file with normalization integral ... '" << normIntFileName  << "'" << endl
-	     << "    path to file with acceptance integral ...... '" << accIntFileName   << "'" << endl
-	     << "    number of acceptance norm. events .......... "  << numbAccEvents    << endl
-	     << "    rank of spin density matrix .................... "  << rank                    << endl
-	     << "    minimizer tolerance ............................ "  << minimizerTolerance << endl
-	     << "    quiet .......................................... "  << yesNo(quiet) << endl;
+	     << "        path to file with normalization integral ... '" << normIntFileName          << "'" << endl
+	     << "        path to file with acceptance integral ...... '" << accIntFileName           << "'" << endl
+	     << "        number of acceptance norm. events .......... "  << numbAccEvents            << endl
+	     << "    use half-Cauchy priors ......................... '" << yesNo(cauchyPriors)      << "'" << endl
+	     << "    quiet .......................................... '" << yesNo(quiet)             << "'" << endl;
+
+	TFile* inFile = TFile::Open(inFileName.c_str(), "READ");
+	if(not inFile || inFile->IsZombie()) {
+		printErr << "could not open input file '" << inFileName << "'. aborting." << endl;
+		return 1;
+	}
+
+	TTree* inTree = 0;
+	inFile->GetObject(valTreeName.c_str(), inTree);
+	if(not inTree) {
+		printErr << "could not find result tree '" << valTreeName << "' in input file '" << inFileName << "'. aborting." << endl;
+		return 1;
+	}
+
+	fitResult* result = 0;
+	inTree->SetBranchAddress(valBranchName.c_str(), &result);
+
+	if(inTree->GetEntries() != 1) {
+		printErr << "result tree '" << valTreeName << "' has more than one entry, NOT IMPLEMENTED." << endl;
+		return 1;
+	}
+	inTree->GetEntry(0);
+
+	// create temporary file to store the wavelist
+	char tempFileName[] = "XXXXXX";
+	close(mkstemp(tempFileName));
+	const string waveListFileName(tempFileName);
+	ofstream waveListFile(waveListFileName.c_str());
+	rpwa::partialWaveFitHelper::extractWaveList(*result, waveListFile);
+	waveListFile.close();
+
+	printInfo << "parameters extracted from input fit result" << endl;
+	cout << "    mass bin centered at ........................... "  << result->massBinCenter() << " MeV/c^2" << endl
+	     << "    path to temporary wave list file ............... '" << waveListFileName        << "'" << endl
+	     << "    rank of spin density matrix .................... "  << result->rank()          << endl;
 
 	// ---------------------------------------------------------------------------
 	// setup likelihood function
@@ -266,111 +292,97 @@ main(int    argc,
 	if (quiet)
 		L.setQuiet();
 	L.useNormalizedAmps(useNormalizedAmps);
-	L.init(rank, waveListFileName, normIntFileName, accIntFileName,
+	if (cauchyPriors)
+		L.setPriorType(L.HALF_CAUCHY);
+	L.init(result->rank(), waveListFileName, normIntFileName, accIntFileName,
 	       ampDirName, numbAccEvents, useRootAmps);
+	remove(waveListFileName.c_str());
 	if (not quiet)
 		cout << L << endl;
-	const unsigned int nmbPar  = L.NDim();
+	const unsigned int nmbPar = L.NDim();
 
-	if(cauchyPriors) {
-		L.setPriorType(L.HALF_CAUCHY);
+	unsigned int maxParNameLength = 0;  // maximum length of parameter names
+	for(unsigned int i = 0; i < nmbPar; ++i) {
+		const string& parName = L.parName(i);
+		if (parName.length() > maxParNameLength)
+			maxParNameLength = parName.length();
 	}
-
-	printInfo << "using prior: ";
-	switch(L.priorType())
-	{
-		case pwaLikelihood<complex<double> >::FLAT:
-			cout << "flat" << endl;
-			break;
-		case pwaLikelihood<complex<double> >::HALF_CAUCHY:
-			cout << "half-cauchy" << endl;
-			break;
-	}
-
-	TFile* fitResultFile = TFile::Open(fitResultFileName.c_str(), "READ");
-	if(not fitResultFile) {
-		printErr << "could not open fit result file '" << fitResultFileName << "'. Aborting..." << endl;
-		return 1;
-	}
-
-	TTree* fitResultTree = 0;
-	fitResultFile->GetObject("pwa", fitResultTree);
-	if(not fitResultTree) {
-		printErr << "could not find result tree in fit result file '" << fitResultFileName << "'. Aborting..." << endl;
-		return 1;
-	}
-
-	fitResult* result = 0;
-	fitResultTree->SetBranchAddress("fitResult_v2", &result);
-
-	if(fitResultTree->GetEntries() != 1) {
-		printErr << "result tree has more than one entry, NOT IMPLEMENTED" << endl;
-		return 1;
-	}
-	fitResultTree->GetEntry(0);
-
 	std::vector<double> pars(nmbPar);
 	for(unsigned int i = 0; i < nmbPar; ++i) {
-		const string parName = L.parName(i);
+		const string& parName = L.parName(i);
 		pars[i] = result->fitParameter(parName);
-		printInfo << "setting parameter[" << i << "] (name: '" << parName << "') = " << pars[i] << endl;
+		printInfo << "setting parameter [" << setw(3) << i << "] " << setw(maxParNameLength) << parName << " = " << maxPrecisionAlign(pars[i]) << endl;
 	}
 
-	TMatrixT<double> covMatrixMinuit = result->fitParCovMatrix();
-	TMatrixT<double> covMatrixAna = L.CovarianceMatrixAnalytically(pars.data());
-	TVectorT<double> eigenvaluesMinuit;
-	TMatrixT<double> eigenvectorsMinuit = covMatrixMinuit.EigenVectors(eigenvaluesMinuit);
-	TVectorT<double> eigenvaluesAna;
-	TMatrixT<double> eigenvectorsAna = covMatrixAna.EigenVectors(eigenvaluesAna);
-	double maximum = L.DoEval(pars.data());
+	const double maxLogLikelihood = L.DoEval(pars.data());
+	printInfo << "likelihood at maximum is " << maxPrecisionAlign(maxLogLikelihood) << "." << endl;
 
-	const unsigned int nmbGraphs = nmbPar;
-	TFile* outFile = new TFile(outputFileName.c_str(), "RECREATE");
-	for(unsigned int par = 0; par < nmbGraphs; par++) {
-		std::stringstream canvasName;
-		canvasName << "eigenvectorSlice" << par;
-		TCanvas cnvs(canvasName.str().c_str());
-		cnvs.cd();
-		TGraph graphLikeli;
-		std::vector<double> eigenvector = getEigenvector(eigenvectorsMinuit, par);
-		if (not checkNorm(eigenvector)) {
-			printWarn << "eigenvector " << par << " is not normalized. Normalizing..." << endl;
-			normalizeVector(eigenvector);
+	const TMatrixT<double> covMatrixMinuit = result->fitParCovMatrix();
+	TVectorT<double> eigenvaluesMinuit;
+	const TMatrixT<double> eigenvectorsMinuit = covMatrixMinuit.EigenVectors(eigenvaluesMinuit);
+
+	const TMatrixT<double> covMatrixAna = L.CovarianceMatrixAnalytically(pars.data());
+	TVectorT<double> eigenvaluesAna;
+	const TMatrixT<double> eigenvectorsAna = covMatrixAna.EigenVectors(eigenvaluesAna);
+
+	TFile* outFile = new TFile(outFileName.c_str(), "RECREATE");
+	for(unsigned int par = 0; par < nmbPar; ++par) {
+		std::vector<double> eigenvectorMinuit = getEigenvector(eigenvectorsMinuit, par);
+		if (not checkNorm(eigenvectorMinuit)) {
+			printWarn << "Minuit eigenvector " << par << " is not normalized. normalizing." << endl;
+			normalizeVector(eigenvectorMinuit);
 			// check again
-			if (not checkNorm(eigenvector)) {
-				printErr << "could not normalize eigenvector! Aborting..." << endl;
+			if (not checkNorm(eigenvectorMinuit)) {
+				printErr << "could not normalize Minuit eigenvector! aborting." << endl;
 				return 1;
 			}
 		}
-		printInfo << "Eigenvector [" << par << "]: " << eigenvector << endl;
-		printInfo << "Eigenvalue  [" << par << "]:" << eigenvaluesMinuit[par] << endl;
+		printInfo << "eigenvector          [" << setw(3) << par << "]: " << eigenvectorMinuit << endl;
+		printInfo << "eigenvalue           [" << setw(3) << par << "]: " << eigenvaluesMinuit[par] << endl;
 
-		for(int p = -50; p < 50; p++) {
-			std::vector<double> newPars = getParsForPoint(eigenvector, pars, eigenvaluesMinuit[par], p);
-			const double likeli = L.DoEval(newPars.data()) - maximum;
+		std::vector<double> eigenvectorAna = getEigenvector(eigenvectorsAna, par);
+		if (not checkNorm(eigenvectorAna)) {
+			printWarn << "analytic eigenvector " << par << " is not normalized. normalizing." << endl;
+			normalizeVector(eigenvectorAna);
+			// check again
+			if (not checkNorm(eigenvectorAna)) {
+				printErr << "could not normalize analytic eigenvector! aborting." << endl;
+				return 1;
+			}
+		}
+		printInfo << "analytic eigenvector [" << setw(3) << par << "]: " << eigenvectorAna << endl;
+		printInfo << "analytic eigenvalue  [" << setw(3) << par << "]: " << eigenvaluesAna[par] << endl;
+
+		TGraph graphLikeli;
+		for(int p = -50; p <= 50; ++p) {
+			std::vector<double> newPars = getParsForPoint(eigenvectorMinuit, pars, eigenvaluesMinuit[par], p);
+			const double likeli = L.DoEval(newPars.data()) - maxLogLikelihood;
 			graphLikeli.SetPoint(p + 50, p / 50. * std::sqrt(eigenvaluesMinuit[par]), likeli);
 		}
 
 		std::stringstream minuitGaussFormula;
-		minuitGaussFormula << "-1*TMath::Gaus(x, 0, TMath::Sqrt(" << (eigenvaluesMinuit[par]) << "))+" << (0+1);
+		minuitGaussFormula << "1-TMath::Gaus(x, 0, TMath::Sqrt(" << (eigenvaluesMinuit[par]) << "))";
 		TF1 gaussMinuit("minuitGauss", minuitGaussFormula.str().c_str(), -100, 100);
-		cout << minuitGaussFormula.str() << endl;
 
 		std::stringstream anaGaussFormula;
-		anaGaussFormula << "-1*TMath::Gaus(x, 0, TMath::Sqrt(" << (eigenvaluesAna[par]) << "))+" << (0+1);
+		anaGaussFormula << "1-TMath::Gaus(x, 0, TMath::Sqrt(" << (eigenvaluesAna[par]) << "))";
 		TF1 gaussAna("analyticGauss", anaGaussFormula.str().c_str(), -100, 100);
-		cout << anaGaussFormula.str() << endl;
 
+		std::stringstream canvasName;
+		canvasName << "eigenvectorSlice" << par;
+		TCanvas cnvs(canvasName.str().c_str());
+		cnvs.cd();
 		graphLikeli.Draw("A*");
 		gaussMinuit.Draw("Lsame");
 		gaussMinuit.SetLineColor(kBlue);
 		gaussAna.Draw("Lsame");
+		gaussAna.SetLineColor(kRed);
 		cnvs.Write();
 	}
-	printInfo << "Setting Minuit line color to blue, analytical solution to red." << endl;
-	printInfo << "Likelihood at maximum is " << setprecision(10) << L.DoEval(pars.data()) << endl;
 	outFile->Close();
-	printSucc << "Slices successfully written to file '" << outputFileName <<"'." << endl;
+	printSucc << "slices successfully written to file '" << outFileName <<"'." << endl;
+
+	printInfo << "setting Minuit line color to blue, analytical solution to red." << endl;
 
 	return 0;
 }
