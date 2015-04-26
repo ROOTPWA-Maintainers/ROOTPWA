14a542c9cdaeaa8d4eacc6d811cd89f7b2fd0b3a
Karl Bicker <kbicker@cern.ch>
2015-04-26 14:17:43 +0200

    src: Allow for multiple fit results in the plotGui.
    Add the possibilities to plot more than one fit result in the plotGui script.
    (cherry picked from commit 101f0d3beb43ac919d850c5d1e7b8067c6b82b5c)

--- plotScripts/plotGui.C
+++ plotScripts/plotGui.C
@@ -12,6 +12,7 @@
 #include <TGButton.h>
 #include <TGListBox.h>
 #include <TList.h>
+#include <TMultiGraph.h>
 #include <TFile.h>
 #include <TTree.h>
 #include <TAxis.h>
@@ -70,16 +71,17 @@ class plotGuiMainFrame : public TGMainFrame {
 		TGListBox*          _listBox2;
 		TGCheckButton*      _checkDrawNewCanvas;
 		std::vector<std::string> _waveNames;
-		std::vector<const rpwa::fitResult*> _fitResults;
+		std::vector<std::vector<const rpwa::fitResult*> > _fitResults;
 		const double _binWidth;
 		unsigned int _canvasCounter;
 		TCanvas* _currentCanvas;
+		vector<int> _usefulColors;
 
 	public:
 		plotGuiMainFrame(const TGWindow *p,
 		            UInt_t w,
 		            UInt_t h,
-		            TTree* tree,
+		            vector<TTree*> trees,
 		            const double& binWidth,
 		            const double& intensityThreshold,
 		            const string& waveListFileName);
@@ -123,35 +125,38 @@ void plotGuiMainFrame::DoExit()
 plotGuiMainFrame::plotGuiMainFrame(const TGWindow *p,
                          UInt_t w,
                          UInt_t h,
-                         TTree* tree,
+                         vector<TTree*> trees,
                          const double& binWidth,
                          const double& intensityThreshold,
                          const string& waveListFileName) :
 	TGMainFrame(p, w, h),
+	_fitResults(trees.size(), std::vector<const rpwa::fitResult*>()),
 	_binWidth(binWidth),
 	_canvasCounter(0),
 	_currentCanvas(0)
 {
 
+	for(unsigned int treeIndex = 0; treeIndex < trees.size(); ++treeIndex)
 	{
+		printInfo << "reading tree " << treeIndex << " of " << trees.size() << "..." << endl;
 		fitResult* res = 0;//new fitResult();
 		string branchname = "fitResult_v2";
-		if(tree->FindBranch(branchname.c_str()) == 0){
+		if(trees[treeIndex]->FindBranch(branchname.c_str()) == 0){
 			printErr << "invalid branch '" << branchname << "'. Aborting..." << endl;
 			throw;
 		}
-		tree->SetBranchAddress(branchname.c_str(),&res);
-		long entries = tree->GetEntries();
+		trees[treeIndex]->SetBranchAddress(branchname.c_str(),&res);
+		long entries = trees[treeIndex]->GetEntries();
 		if(entries < 1) {
 			printErr << "tree has no entries. Aborting..." << endl;
 			throw;
 		}
 		printInfo << "Mass bins: " << entries << endl;
-		_fitResults.resize(entries, 0);
+		_fitResults[treeIndex].resize(entries, 0);
 		for(long i = 0; i < entries; ++i) {
 			printInfo << "loading result " << i << " of " << entries << "... " << std::flush;
-			tree->GetEntry(i);
-			_fitResults[i] = new rpwa::fitResult(*res);
+			trees[treeIndex]->GetEntry(i);
+			_fitResults[treeIndex][i] = new rpwa::fitResult(*res);
 			cout << "done!" << endl;
 		}
 	}
@@ -165,9 +170,11 @@ plotGuiMainFrame::plotGuiMainFrame(const TGWindow *p,
 			printInfo << "found " << whitelistedWaves.size() << " white-listed waves." << endl;
 		}
 		set<string> waveNameCollector;
-		for(unsigned int i = 0; i < _fitResults.size(); ++i) {
-			const vector<string>& waveNames = _fitResults[i]->waveNames();
-			waveNameCollector.insert(waveNames.begin(), waveNames.end());
+		for(unsigned int treeIndex = 0; treeIndex < _fitResults.size(); ++treeIndex) {
+			for(unsigned int i = 0; i < _fitResults[treeIndex].size(); ++i) {
+				const vector<string>& waveNames = _fitResults[treeIndex][i]->waveNames();
+				waveNameCollector.insert(waveNames.begin(), waveNames.end());
+			}
 		}
 		vector<string> tentativeWaveNames(waveNameCollector.size());
 		std::copy(waveNameCollector.begin(), waveNameCollector.end(), tentativeWaveNames.begin());
@@ -179,11 +186,13 @@ plotGuiMainFrame::plotGuiMainFrame(const TGWindow *p,
 				if(_debug) {
 					printDebug << "checking if intensity is above threshold for wave '" << waveName << "'." << endl;
 				}
-				for(unsigned int bin_i = 0; bin_i < _fitResults.size(); ++bin_i) {
-					const double intensity = _fitResults[bin_i]->intensity(waveName.c_str());
-					if(intensity >= intensityThreshold) {
-						whitelistedWaves.insert(waveName);
-						break;
+				for(unsigned int treeIndex = 0; treeIndex < _fitResults.size(); ++treeIndex) {
+					for(unsigned int bin_i = 0; bin_i < _fitResults[treeIndex].size(); ++bin_i) {
+						const double intensity = _fitResults[treeIndex][bin_i]->intensity(waveName.c_str());
+						if(intensity >= intensityThreshold) {
+							whitelistedWaves.insert(waveName);
+							break;
+						}
 					}
 				}
 			}
@@ -254,6 +263,16 @@ plotGuiMainFrame::plotGuiMainFrame(const TGWindow *p,
 	MapWindow();
 	_listBox1->Select(0);
 	_listBox2->Select(0);
+
+	vector<int> badColors;
+	int badColorsArray[12] = {5, 10, 18, 19, 21, 22, 23, 25, 26, 34, 37, 31};
+	badColors.assign(badColorsArray, badColorsArray+12);
+	for(unsigned int i = 1; i < 41; ++i) {
+		if(std::find(badColors.begin(), badColors.end(), i)==badColors.end()) {
+			_usefulColors.push_back(i);
+		}
+	}
+
 }
 
 
@@ -318,150 +337,199 @@ void plotGuiMainFrame::PrintSelected(const int sel1, const int sel2)
 		}
 	}
 
-	// Produce plots
-	unsigned int nFitResults = _fitResults.size();
+	stringstream sstr;
+	sstr << "g" << w1;
+	const string intensity1GraphName = sstr.str();
+	sstr.str("");
+	sstr << "g" << w2;
+	const string intensity2GraphName = sstr.str();
+	sstr.str("");
+	sstr << "PHI"<< w1 << "---" << "PHI" << w2;
+	const string phaseGraphName = sstr.str();
+	sstr.str("");
+	sstr << "RE_" << w1 << "---" << w2;
+	const string reGraphName = sstr.str();
+	sstr.str("");
+	sstr << "IM_" << w1 << "---" << w2;
+	const string imGraphName = sstr.str();
 
-	Int_t colour=1;
+	TMultiGraph* intensity1Graph = new TMultiGraph();
+	TMultiGraph* intensity2Graph = new TMultiGraph();
+	TMultiGraph* phaseGraph = new TMultiGraph();
+	TMultiGraph* reGraph = new TMultiGraph();
+	TMultiGraph* imGraph = new TMultiGraph();
 
-	TGraphErrors* gph = new TGraphErrors(nFitResults);
-	stringstream graphName;
-	graphName << "PHI"<<w1<<"---"<<"PHI"<<w2;
-	if(_debug) {
-		printDebug << "creating graph   " << graphName.str() << endl;
+	intensity1Graph->SetName(intensity1GraphName.c_str());
+	intensity1Graph->SetTitle(intensity1GraphName.c_str());
+	intensity2Graph->SetName(intensity2GraphName.c_str());
+	intensity2Graph->SetTitle(intensity2GraphName.c_str());
+	phaseGraph->SetName(phaseGraphName.c_str());
+	phaseGraph->SetTitle(phaseGraphName.c_str());
+	reGraph->SetName(reGraphName.c_str());
+	reGraph->SetTitle(reGraphName.c_str());
+	imGraph->SetName(imGraphName.c_str());
+	imGraph->SetTitle(imGraphName.c_str());
+
+	for(unsigned int treeIndex = 0; treeIndex < _fitResults.size(); ++treeIndex) {
+
+		const unsigned int& nPoints = _fitResults[treeIndex].size();
+
+		TGraphErrors* gph = new TGraphErrors(nPoints);
+		stringstream graphName;
+		graphName << phaseGraphName << "_" << treeIndex;
+		if(_debug) {
+			printDebug << "creating graph   " << graphName.str() << endl;
+		}
+		gph->SetName (graphName.str().c_str());
+		gph->SetTitle(graphName.str().c_str());
+		gph->SetMarkerStyle(21);
+		gph->SetMarkerSize(0.5);
+		gph->SetMarkerColor(_usefulColors[treeIndex % _usefulColors.size()]);
+		gph->SetLineColor(_usefulColors[treeIndex % _usefulColors.size()]);
+
+		TGraphErrors* gphP1 = (TGraphErrors*)gph->Clone("gph+1");
+		TGraphErrors* gphM1 = (TGraphErrors*)gph->Clone("gph-1");
+		if(_fitResults.size() == 1) {
+			gphP1->SetMarkerColor(2);
+			gphP1->SetLineColor(2);
+			gphM1->SetMarkerColor(3);
+			gphM1->SetLineColor(3);
+		} else {
+			gphP1->SetMarkerColor(_usefulColors[treeIndex % _usefulColors.size()]);
+			gphP1->SetLineColor(_usefulColors[treeIndex % _usefulColors.size()]);
+			gphM1->SetMarkerColor(_usefulColors[treeIndex % _usefulColors.size()]);
+			gphM1->SetLineColor(_usefulColors[treeIndex % _usefulColors.size()]);
+		}
+
+		TGraphErrors* gRe = new TGraphErrors(nPoints);
+		graphName.str("");
+		graphName << reGraphName << "_" << treeIndex;
+		if(_debug) {
+			printDebug << "creating graph   " << graphName.str() << endl;
+		}
+		gRe->SetName (graphName.str().c_str());
+		gRe->SetTitle(graphName.str().c_str());
+		gRe->SetMarkerStyle(21);
+		gRe->SetMarkerSize(0.5);
+		gRe->SetMarkerColor(_usefulColors[treeIndex % _usefulColors.size()]);
+		gRe->SetLineColor(_usefulColors[treeIndex % _usefulColors.size()]);
+
+		TGraphErrors* gIm = new TGraphErrors(nPoints);
+		graphName.str("");
+		graphName << imGraphName << "_" << treeIndex;
+		if(_debug) {
+			printDebug << "creating graph   " << graphName.str() << endl;
+		}
+		gIm->SetName (graphName.str().c_str());
+		gIm->SetTitle(graphName.str().c_str());
+		gIm->SetMarkerStyle(21);
+		gIm->SetMarkerSize(0.5);
+		gIm->SetMarkerColor(_usefulColors[treeIndex % _usefulColors.size()]);
+		gIm->SetLineColor(_usefulColors[treeIndex % _usefulColors.size()]);
+
+		TGraphErrors* g1 = new TGraphErrors(nPoints);
+		graphName.str("");
+		graphName << intensity1GraphName << "_" << treeIndex;
+		g1->SetName (graphName.str().c_str());
+		g1->SetTitle(graphName.str().c_str());
+		g1->SetMarkerStyle(21);
+		g1->SetMarkerSize(0.5);
+		g1->SetMarkerColor(_usefulColors[treeIndex % _usefulColors.size()]);
+		g1->SetLineColor(_usefulColors[treeIndex % _usefulColors.size()]);
+		TGraphErrors* g2 = new TGraphErrors(nPoints);
+		graphName.str("");
+		graphName << intensity1GraphName << "_" << treeIndex;
+		g2->SetName (graphName.str().c_str());
+		g2->SetTitle(graphName.str().c_str());
+		g2->SetMarkerStyle(21);
+		g2->SetMarkerSize(0.5);
+		g2->SetMarkerColor(_usefulColors[treeIndex % _usefulColors.size()]);
+		g2->SetLineColor(_usefulColors[treeIndex % _usefulColors.size()]);
+
+		for(unsigned int i = 0; i < nPoints; ++i){
+			const fitResult* result = _fitResults[treeIndex][i];
+
+			if(not result->converged()) {
+				continue;
+			}
+
+			int wi1 = result->waveIndex(w1);
+			int wi2 = result->waveIndex(w2);
+			double intensity1 = 0.;
+			double intensity2 = 0.;
+			double intensityErr1 = 0.;
+			double intensityErr2 = 0.;
+			double ph = 0.;
+			double phErr = 0.;
+			complex<double> rho(0., 0.);
+			TMatrixT<double> rhoCov(2, 2);
+			if(wi1 >= 0) {
+				intensity1 = result->intensity(w1.c_str());
+				intensityErr1 = result->intensityErr(w1.c_str());
+			}
+			if(wi2 >= 0) {
+				intensity2 = result->intensity(w2.c_str());
+				intensityErr2 = result->intensityErr(w2.c_str());
+			}
+			if(wi1 >= 0 and wi2 >= 0) {
+				ph = result->phase(w1.c_str(), w2.c_str());
+				phErr = result->phaseErr(w1.c_str(), w2.c_str());
+				rho = result->spinDensityMatrixElem(wi1, wi2);
+				rhoCov = result->spinDensityMatrixElemCov(wi1, wi2);
+			}
+
+			if((numeric_limits<double>::has_infinity and intensity1 == numeric_limits<double>::infinity()) or intensity1!=intensity1) {
+				continue;
+			}
+
+			g1->SetPoint(i, result->massBinCenter()*0.001, intensity1);
+			g1->SetPointError(i, _binWidth*0.5, intensityErr1);
+
+			if((numeric_limits<double>::has_infinity and intensity2 == numeric_limits<double>::infinity()) or intensity2!=intensity2) {
+				continue;
+			}
+
+			g2->SetPoint(i, result->massBinCenter()*0.001, intensity2);
+			g2->SetPointError(i, _binWidth*0.5, intensityErr2);
+
+			// check if we should make a transformation by 2pi
+			// this is needed because of cyclical variable phi
+			if(i>11){
+				double mpre;
+				double phpre;
+				gph->GetPoint(i-1,mpre,phpre);
+				double diff1=fabs(ph-phpre);
+				double diff2=fabs(ph+360-phpre);
+				double diff3=fabs(ph-360-phpre);
+				if(diff2<diff1 && diff2<diff3)ph+=360;
+				else if(diff3<diff1 && diff3<diff2)ph-=360;
+			}
+
+			gph->SetPoint(i, result->massBinCenter()*0.001, ph);
+			gph->SetPointError(i, _binWidth*0.5, phErr);
+
+			// add point +- 360 degree
+			gphP1->SetPoint(i, result->massBinCenter()*0.001, ph+360);
+			gphP1->SetPointError(i, _binWidth*0.5, phErr);
+
+			gphM1->SetPoint(i, result->massBinCenter()*0.001, ph-360);
+			gphM1->SetPointError(i, _binWidth*0.5, phErr);
+
+			gRe->SetPoint(i, result->massBinCenter()*0.001, rho.real());
+			gRe->SetPointError(i, _binWidth*0.5, sqrt(rhoCov[0][0]));
+			gIm->SetPoint(i, result->massBinCenter()*0.001, rho.imag());
+			gIm->SetPointError(i, _binWidth*0.5, sqrt(rhoCov[1][1]));
+		}// end loop over bins
+
+		intensity1Graph->Add(g1);
+		intensity2Graph->Add(g2);
+		phaseGraph->Add(gph);
+		phaseGraph->Add(gphP1);
+		phaseGraph->Add(gphM1);
+		reGraph->Add(gRe);
+		imGraph->Add(gIm);
+
 	}
-	gph->SetName (graphName.str().c_str());
-	gph->SetTitle(graphName.str().c_str());
-	gph->SetMarkerStyle(21);
-	gph->SetMarkerSize(0.5);
-	gph->SetMarkerColor(colour);
-	gph->SetLineColor(colour);
-
-	TGraphErrors* gphP1 = (TGraphErrors*)gph->Clone("gph+1");
-	TGraphErrors* gphM1 = (TGraphErrors*)gph->Clone("gph-1");
-	gphP1->SetMarkerColor(2);
-	gphP1->SetLineColor(2);
-	gphM1->SetMarkerColor(3);
-	gphM1->SetLineColor(3);
-
-	TGraphErrors* gRe = new TGraphErrors(nFitResults);
-	graphName.str("");
-	graphName.clear();
-	graphName << "RE_"<<w1<<"---"<<""<<w2;
-	if(_debug) {
-		printDebug << "creating graph   " << graphName.str() << endl;
-	}
-	gRe->SetName (graphName.str().c_str());
-	gRe->SetTitle(graphName.str().c_str());
-	gRe->SetMarkerStyle(21);
-	gRe->SetMarkerSize(0.5);
-	gRe->SetMarkerColor(colour);
-	gRe->SetLineColor(colour);
-
-	TGraphErrors* gIm = new TGraphErrors(nFitResults);
-	graphName.str("");
-	graphName.clear();
-	graphName << "IM_"<<w1<<"---"<<""<<w2;
-	if(_debug) {
-		printDebug << "creating graph   " << graphName.str() << endl;
-	}
-	gIm->SetName (graphName.str().c_str());
-	gIm->SetTitle(graphName.str().c_str());
-	gIm->SetMarkerStyle(21);
-	gIm->SetMarkerSize(0.5);
-	gIm->SetMarkerColor(colour);
-	gIm->SetLineColor(colour);
-
-	TGraphErrors* g1 = new TGraphErrors(nFitResults);
-	graphName.str("");
-	graphName.clear();
-	graphName<<"g"<<w1;
-	g1->SetName (graphName.str().c_str());
-	g1->SetTitle(graphName.str().c_str());
-	g1->SetMarkerStyle(21);
-	g1->SetMarkerSize(0.5);
-	TGraphErrors* g2 = new TGraphErrors(nFitResults);
-	graphName.str("");
-	graphName.clear();
-	graphName<<"g"<<w2;
-	g2->SetName (graphName.str().c_str());
-	g2->SetTitle(graphName.str().c_str());
-	g2->SetMarkerStyle(21);
-	g2->SetMarkerSize(0.5);
-
-	for(unsigned int i = 0; i < nFitResults; ++i){
-		const fitResult* result = _fitResults[i];
-
-		if(not result->converged()) {
-			continue;
-		}
-
-		int wi1 = result->waveIndex(w1);
-		int wi2 = result->waveIndex(w2);
-		double intensity1 = 0.;
-		double intensity2 = 0.;
-		double intensityErr1 = 0.;
-		double intensityErr2 = 0.;
-		double ph = 0.;
-		double phErr = 0.;
-		complex<double> rho(0., 0.);
-		TMatrixT<double> rhoCov(2, 2);
-		if(wi1 >= 0) {
-			intensity1 = result->intensity(w1.c_str());
-			intensityErr1 = result->intensityErr(w1.c_str());
-		}
-		if(wi2 >= 0) {
-			intensity2 = result->intensity(w2.c_str());
-			intensityErr2 = result->intensityErr(w2.c_str());
-		}
-		if(wi1 >= 0 and wi2 >= 0) {
-			ph = result->phase(w1.c_str(), w2.c_str());
-			phErr = result->phaseErr(w1.c_str(), w2.c_str());
-			rho = result->spinDensityMatrixElem(wi1, wi2);
-			rhoCov = result->spinDensityMatrixElemCov(wi1, wi2);
-		}
-
-		if((numeric_limits<double>::has_infinity and intensity1 == numeric_limits<double>::infinity()) or intensity1!=intensity1) {
-			continue;
-		}
-
-		g1->SetPoint(i, result->massBinCenter()*0.001, intensity1);
-		g1->SetPointError(i, _binWidth*0.5, intensityErr1);
-
-		if((numeric_limits<double>::has_infinity and intensity2 == numeric_limits<double>::infinity()) or intensity2!=intensity2) {
-			continue;
-		}
-
-		g2->SetPoint(i, result->massBinCenter()*0.001, intensity2);
-		g2->SetPointError(i, _binWidth*0.5, intensityErr2);
-
-		// check if we should make a transformation by 2pi
-		// this is needed because of cyclical variable phi
-		if(i>11){
-			double mpre;
-			double phpre;
-			gph->GetPoint(i-1,mpre,phpre);
-			double diff1=fabs(ph-phpre);
-			double diff2=fabs(ph+360-phpre);
-			double diff3=fabs(ph-360-phpre);
-			if(diff2<diff1 && diff2<diff3)ph+=360;
-			else if(diff3<diff1 && diff3<diff2)ph-=360;
-		}
-
-		gph->SetPoint(i, result->massBinCenter()*0.001, ph);
-		gph->SetPointError(i, _binWidth*0.5, phErr);
-
-		// add point +- 360 degree
-		gphP1->SetPoint(i, result->massBinCenter()*0.001, ph+360);
-		gphP1->SetPointError(i, _binWidth*0.5, phErr);
-
-		gphM1->SetPoint(i, result->massBinCenter()*0.001, ph-360);
-		gphM1->SetPointError(i, _binWidth*0.5, phErr);
-
-		gRe->SetPoint(i, result->massBinCenter()*0.001, rho.real());
-		gRe->SetPointError(i, _binWidth*0.5, sqrt(rhoCov[0][0]));
-		gIm->SetPoint(i, result->massBinCenter()*0.001, rho.imag());
-		gIm->SetPointError(i, _binWidth*0.5, sqrt(rhoCov[1][1]));
-	}// end loop over bins
 
 	// plot graphs
 	if(not _currentCanvas or _checkDrawNewCanvas->IsOn()) {
@@ -476,29 +544,52 @@ void plotGuiMainFrame::PrintSelected(const int sel1, const int sel2)
 		_currentCanvas->Clear();
 	}
 	_currentCanvas->Divide(2,2);
+
 	_currentCanvas->cd(3);
-
-	gph->Draw("AP");
-	gph->GetXaxis()->SetTitle("5#pi mass (GeV/c^2)");
-	gph->GetYaxis()->SetTitle("Phase difference");
-	gph->GetYaxis()->SetRangeUser(-270,270);
-
-	gphP1->Draw("PSAME");
-	gphM1->Draw("PSAME");
+	phaseGraph->Draw("AP");
+	phaseGraph->GetXaxis()->SetTitle("5#pi mass (GeV/c^2)");
+	phaseGraph->GetYaxis()->SetTitle("Phase difference");
+	phaseGraph->GetYaxis()->SetRangeUser(-270,270);
 
 	_currentCanvas->cd(1);
-	g1->Draw("AP");
-	g1->GetXaxis()->SetTitle("5#pi mass (GeV/c^2)");
-	g1->GetYaxis()->SetTitle("Intensity");
+	intensity1Graph->Draw("AP");
+	intensity1Graph->GetXaxis()->SetTitle("5#pi mass (GeV/c^2)");
+	intensity1Graph->GetYaxis()->SetTitle("Intensity");
 
 	_currentCanvas->cd(2);
-	g2->Draw("AP");
-	g2->GetXaxis()->SetTitle("5#pi mass (GeV/c^2)");
-	g2->GetYaxis()->SetTitle("Intensity");
+	intensity2Graph->Draw("AP");
+	intensity2Graph->GetXaxis()->SetTitle("5#pi mass (GeV/c^2)");
+	intensity2Graph->GetYaxis()->SetTitle("Intensity");
 
 	_currentCanvas->Draw();
 	_currentCanvas->Update();
 
+}
+
+void plotGui(const vector<std::string>& inFileNames,
+             const double& binWidth = 0.03,
+             const double& intensityThreshold = -1.,
+             const string& whiteListFileName = "")
+{
+
+	printInfo << "opening file '" << inFileNames << "'." << endl;
+	vector<TTree*> trees(inFileNames.size(), 0);
+	for(unsigned int i = 0; i < inFileNames.size(); ++i) {
+		TFile* infile=TFile::Open(inFileNames[i].c_str(),"READ");
+		if(not infile){
+			printErr << "file '" << inFileNames << " not found. Aborting..."<< endl;
+			return;
+		}
+		TTree* pwa=(TTree*)infile->FindObjectAny("pwa");
+		if(not pwa){
+			printErr << "tree not found in input file '" << inFileNames << "'. Aborting..."<< endl;
+			return;
+		}
+		trees[i] = pwa;
+	}
+
+	// Popup the GUI...
+	new plotGuiMainFrame(gClient->GetRoot(), 20, 20, trees, binWidth, intensityThreshold, whiteListFileName);
 }
 
 void plotGui(const std::string& inFileName,
@@ -506,19 +597,6 @@ void plotGui(const std::string& inFileName,
              const double& intensityThreshold = -1.,
              const string& whiteListFileName = "")
 {
-
-	printInfo << "opening file '" << inFileName << "'." << endl;
-	TFile* infile=TFile::Open(inFileName.c_str(),"READ");
-	if(infile==NULL){
-		printErr << "file '" << inFileName << " not found. Aborting..."<< endl;
-		return;
-	}
-	TTree* pwa=(TTree*)infile->FindObjectAny("pwa");
-	if(pwa==NULL){
-		printErr << "tree not found in input file '" << inFileName << "'. Aborting..."<< endl;
-		return;
-	}
-
-	// Popup the GUI...
-	new plotGuiMainFrame(gClient->GetRoot(), 20, 20, pwa, binWidth, intensityThreshold, whiteListFileName);
+	const std::vector<string> fileNames(1, inFileName);
+	plotGui(fileNames, binWidth, intensityThreshold, whiteListFileName);
 }
