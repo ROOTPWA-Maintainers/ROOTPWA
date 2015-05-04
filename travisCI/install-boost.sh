173e567284a4015af14c98e9b0bf31efc93d7ac3
Karl Bicker <kbicker@cern.ch>
2015-05-04 15:00:09 +0200

    buildSystem: Add Travis CI files.
    Add the necessary files to have Travis CI test ROOTPWA. All these files were
    perpared by Florian Kaspar, a big thank you to him!
    
    (cherry picked from commit 44ec4fed544d18371adbb802e72a73e6da210da2)

---
+++ travisCI/install-boost.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -exv
+wget 'http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz'
+tar -xzf boost_1_58_0.tar.gz
---
+++ travisCI/install-cmake.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -exv
+wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
+tar -xzf cmake-3.2.2-Linux-x86_64.tar.gz
+mv ./cmake-3.2.2-Linux-x86_64 ./cmake
