1d56879befc89328e84125d9530e3321efefe99f
Karl Bicker <kbicker@cern.ch>
2015-05-04 11:52:29 +0200

    src: Fix arguments for pwaNloptFit and pwafit.
    Remove the arguments with optional value which have a very peculiar and
    non-intuitive behavior.

--- src/pwafit.cc
+++ src/pwafit.cc
@@ -77,7 +77,7 @@ usage(const string& progName,
 	     << endl
 	     << "usage:" << endl
 	     << progName
-	     << " -l # -u # -w wavelist [-d amplitude directory -R -o outfile -S start value file -s seed -x [start value] -N -n normfile"
+	     << " -l # -u # -w wavelist [-d amplitude directory -R -o outfile -S start value file -s seed -x -V startValue -N -n normfile"
 	     << " -a normfile -A # normalisation events -r rank -M minimizer -m algorithm -g strategy -t # -e -c -H -q -h]" << endl
 	     << "    where:" << endl
 	     << "        -l #       lower edge of mass bin [MeV/c^2]" << endl
@@ -92,8 +92,8 @@ usage(const string& progName,
 	     << "        -o file    path to output file (default: 'fitresult.root')" << endl
 	     << "        -S file    path to file with start values (default: none; highest priority)" << endl
 	     << "        -s #       seed for random start values (default: 1234567)" << endl
-	     << "        -x #       use fixed instead of random start values (default: 0.01)" << endl
-
+	     << "        -x         use fixed instead of random start values" << endl
+	     << "        -V #       start values to use (only in combination with -x) (default: 0.01)" << endl
 	     << "        -N         use normalization of decay amplitudes (default: false)" << endl
 	     << "        -n file    path to normalization integral file (default: 'norm.int')" << endl
 	     << "        -a file    path to acceptance integral file (default: 'norm.int')" << endl
@@ -209,10 +209,11 @@ main(int    argc,
 	bool         cudaEnabled         = false;                  // if true CUDA kernels are activated
 	bool         checkHessian        = false;                  // if true checks analytical Hessian eigenvalues
 	bool         quiet               = false;
+
 	extern char* optarg;
 	// extern int optind;
 	int c;
-	while ((c = getopt(argc, argv, "l:u:w:d:Ro:S:s:x::Nn:a:A:r:M:m:g:t:ecHqh")) != -1)
+	while ((c = getopt(argc, argv, "l:u:w:d:Ro:S:s:xV:Nn:a:A:r:M:m:g:t:ecHqh")) != -1)
 		switch (c) {
 		case 'l':
 			massBinMin = atof(optarg);
@@ -241,9 +242,10 @@ main(int    argc,
 			startValSeed = atoi(optarg);
 			break;
 		case 'x':
-			if (optarg)
-				defaultStartValue = atof(optarg);
 			useFixedStartValues = true;
+			break;
+		case 'V':
+			defaultStartValue = atof(optarg);
 			break;
 		case 'N':
 			useNormalizedAmps = true;
