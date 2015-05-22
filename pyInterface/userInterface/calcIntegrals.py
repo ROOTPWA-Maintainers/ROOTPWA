c7048328609dcb63bf21966c772d70da3fd8c068
Oliver Drotleff <oliver.drotleff@gmail.com>
2015-05-22 12:46:11 +0200

    pyInterface: Allow calculating only one event type with calcIntegrals.py
    Make it possible so that you can calculate integrals for only one event type
    (e.g. generated data) instead of always having to calculate integrals for all
    event types.

--- pyInterface/userInterface/calcIntegrals.py
+++ pyInterface/userInterface/calcIntegrals.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
 	parser.add_argument("-c", type=str, metavar="configFileName", dest="configFileName", default="./rootpwa.config", help="path to config file (default: './rootpwa.config')")
 	parser.add_argument("-n", type=int, metavar="#", dest="nEvents", default=0, help="maximum number of events to process (default: all)")
 	parser.add_argument("-b", type=int, metavar="massBin", default=-1, dest="massBin", help="mass bin to be calculated (default: all)")
+	parser.add_argument("-e", type=str, metavar="eventsType", default="all", dest="eventsType", help="events type to be calculated ('real', 'generated' or 'accepted', default: all)")
 	parser.add_argument("-w", type=str, metavar="path", dest="weightsFileName", default="", help="path to MC weight file for de-weighting (default: none)")
 	args = parser.parse_args()
 
@@ -41,8 +42,20 @@ if __name__ == "__main__":
 	if (not args.massBin == -1):
 		binIDList = [args.massBin]
 
+	eventsTypes = []
+	if (args.eventsType == "generated"):
+		eventsTypes = [ pyRootPwa.core.eventMetadata.GENERATED ]
+	elif (args.eventsType == "accepted"):
+		eventsTypes = [ pyRootPwa.core.eventMetadata.ACCEPTED ]
+	elif (args.eventsType == "all"):
+		eventsTypes = [ pyRootPwa.core.eventMetadata.GENERATED,
+		                pyRootPwa.core.eventMetadata.ACCEPTED ]
+	else:
+		pyRootPwa.utils.printErr("Invalid events type given ('" + args.eventsType + "'). Aborting...")
+		sys.exit(1)
+
 	for binID in binIDList:
-		for eventsType in [ pyRootPwa.core.eventMetadata.GENERATED, pyRootPwa.core.eventMetadata.ACCEPTED ]:
+		for eventsType in eventsTypes:
 			outputFileName = fileManager.getIntegralFilePath(binID, eventsType)
 			outputFile = pyRootPwa.ROOT.TFile.Open(outputFileName, "NEW")
 			if not outputFile:
