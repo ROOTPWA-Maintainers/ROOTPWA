3147e225ed86ce0c60f90c2998a8b2177d12a947
Karl Bicker <kbicker@cern.ch>
2015-09-14 14:51:10 +0200

    partialWaveFit: Enable multiple attempts at once.
    Make it possible to make several fit attempts at once in both the pwaFit and
    pwaNloptFit python scripts.

--- pyInterface/package/fit.py
+++ pyInterface/package/fit.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import pyRootPwa
 import pyRootPwa.core
@@ -53,7 +54,8 @@ def pwaFit(ampFileList,
            checkHessian=False,
            saveSpace=False,
            rank=1,
-           verbose=False
+           verbose=False,
+           attempts=1
           ):
 
 	waveDescThres = readWaveList(waveListFileName, keyFiles)
@@ -70,25 +72,25 @@ def pwaFit(ampFileList,
 	                        rank,
 	                        massBinCenter)):
 		printErr("could not initialize likelihood. Aborting...")
-		return False
+		return [ ]
 
 	normIntFile = ROOT.TFile.Open(normIntegralFileName, "READ")
 	if len(normIntFile.GetListOfKeys()) != 1:
 		pyRootPwa.utils.printWarn("'" + normIntegralFileName + "' does not contain exactly one TKey.")
-		return False
+		return [ ]
 	normIntMatrix = normIntFile.Get(pyRootPwa.core.ampIntegralMatrix.integralObjectName)
 	if (not likelihood.addNormIntegral(normIntMatrix)):
 		pyRootPwa.utils.printErr("could not add normalization integral. Aborting...")
-		return False
+		return [ ]
 	normIntFile.Close()
 	accIntFile = ROOT.TFile.Open(accIntegralFileName, "READ")
 	if len(accIntFile.GetListOfKeys()) != 1:
 		pyRootPwa.utils.printWarn("'" + normIntegralFileName + "' does not contain exactly one TKey.")
-		return False
+		return [ ]
 	accIntMatrix = accIntFile.Get(pyRootPwa.core.ampIntegralMatrix.integralObjectName)
 	if (not likelihood.addAccIntegral(accIntMatrix, accEventsOverride)):
 		pyRootPwa.utils.printErr("could not add acceptance integral. Aborting...")
-		return False
+		return [ ]
 	accIntFile.Close()
 
 	for wave in waveDescThres:
@@ -97,28 +99,46 @@ def pwaFit(ampFileList,
 		ampFile = ROOT.TFile.Open(ampFileName, "READ")
 		if not ampFile:
 			pyRootPwa.utils.printErr("could not open amplitude file '" + ampFileName + "'.")
-			return False
+			return [ ]
 		meta = pyRootPwa.core.amplitudeMetadata.readAmplitudeFile(ampFile, waveName)
 		if not meta:
 			pyRootPwa.utils.printErr("could not get metadata for waveName '" + waveName + "'.")
-			return False
+			return [ ]
 		if (not likelihood.addAmplitude(meta)):
 			pyRootPwa.utils.printErr("could not add amplitude '" + waveName + "'. Aborting...")
-			return False
+			return [ ]
 	if (not likelihood.finishInit()):
 		pyRootPwa.utils.printErr("could not finish initialization of likelihood. Aborting...")
-		return False
+		return [ ]
 	lowerBound = binningMap[binningMap.keys()[0]][0]
 	upperBound = binningMap[binningMap.keys()[0]][1]
-	fitResult = pyRootPwa.core.pwaFit(likelihood       = likelihood,
-	                                  massBinMin       = lowerBound,
-	                                  massBinMax       = upperBound,
-	                                  seed             = seed,
-	                                  startValFileName = startValFileName,
-	                                  checkHessian     = checkHessian,
-	                                  saveSpace        = saveSpace,
-	                                  verbose          = verbose)
-	return fitResult
+
+	if attempts == 1:
+		seeds = [ seed ]
+	elif seed == 0:
+		seeds = [ 0 for _ in xrange(attempts) ]
+	else:
+		random.seed(seed)
+		seeds = [ ]
+		for _ in xrange(attempts):
+			while True:
+				randVal = random.randint(1000, 2**32-1)
+				if randVal not in seeds:
+					break
+			seeds.append(randVal)
+
+	fitResults = [ ]
+	for fitSeed in seeds:
+		fitResult = pyRootPwa.core.pwaFit(likelihood       = likelihood,
+		                                  massBinMin       = lowerBound,
+		                                  massBinMax       = upperBound,
+		                                  seed             = fitSeed,
+		                                  startValFileName = startValFileName,
+		                                  checkHessian     = checkHessian,
+		                                  saveSpace        = saveSpace,
+		                                  verbose          = verbose)
+		fitResults.append(fitResult)
+	return fitResults
 
 
 def pwaNloptFit(ampFileList,
@@ -135,7 +155,8 @@ def pwaNloptFit(ampFileList,
                 checkHessian=False,
                 saveSpace=False,
                 rank=1,
-                verbose=False
+                verbose=False,
+                attempts=1
                ):
 
 	waveDescThres = readWaveList(waveListFileName, keyFiles)
@@ -152,25 +173,25 @@ def pwaNloptFit(ampFileList,
 	                        rank,
 	                        massBinCenter)):
 		printErr("could not initialize likelihood. Aborting...")
-		return False
+		return [ ]
 
 	normIntFile = ROOT.TFile.Open(normIntegralFileName, "READ")
 	if len(normIntFile.GetListOfKeys()) != 1:
 		pyRootPwa.utils.printWarn("'" + normIntegralFileName + "' does not contain exactly one TKey.")
-		return False
+		return [ ]
 	normIntMatrix = normIntFile.Get(pyRootPwa.core.ampIntegralMatrix.integralObjectName)
 	if (not likelihood.addNormIntegral(normIntMatrix)):
 		pyRootPwa.utils.printErr("could not add normalization integral. Aborting...")
-		return False
+		return [ ]
 	normIntFile.Close()
 	accIntFile = ROOT.TFile.Open(accIntegralFileName, "READ")
 	if len(accIntFile.GetListOfKeys()) != 1:
 		pyRootPwa.utils.printWarn("'" + normIntegralFileName + "' does not contain exactly one TKey.")
-		return False
+		return [ ]
 	accIntMatrix = accIntFile.Get(pyRootPwa.core.ampIntegralMatrix.integralObjectName)
 	if (not likelihood.addAccIntegral(accIntMatrix, accEventsOverride)):
 		pyRootPwa.utils.printErr("could not add acceptance integral. Aborting...")
-		return False
+		return [ ]
 	accIntFile.Close()
 
 	for wave in waveDescThres:
@@ -179,25 +200,43 @@ def pwaNloptFit(ampFileList,
 		ampFile = ROOT.TFile.Open(ampFileName, "READ")
 		if not ampFile:
 			pyRootPwa.utils.printErr("could not open amplitude file '" + ampFileName + "'.")
-			return False
+			return [ ]
 		meta = pyRootPwa.core.amplitudeMetadata.readAmplitudeFile(ampFile, waveName)
 		if not meta:
 			pyRootPwa.utils.printErr("could not get metadata for waveName '" + waveName + "'.")
-			return False
+			return [ ]
 		if (not likelihood.addAmplitude(meta)):
 			pyRootPwa.utils.printErr("could not add amplitude '" + waveName + "'. Aborting...")
-			return False
+			return [ ]
 	if (not likelihood.finishInit()):
 		pyRootPwa.utils.printErr("could not finish initialization of likelihood. Aborting...")
-		return False
+		return [ ]
 	lowerBound = binningMap[binningMap.keys()[0]][0]
 	upperBound = binningMap[binningMap.keys()[0]][1]
-	fitResult = pyRootPwa.core.pwaNloptFit(likelihood       = likelihood,
-	                                       massBinMin       = lowerBound,
-	                                       massBinMax       = upperBound,
-	                                       seed             = seed,
-	                                       startValFileName = startValFileName,
-	                                       checkHessian     = checkHessian,
-	                                       saveSpace        = saveSpace,
-	                                       verbose          = verbose)
-	return fitResult
+
+	if attempts == 1:
+		seeds = [ seed ]
+	elif seed == 0:
+		seeds = [ 0 for _ in xrange(attempts) ]
+	else:
+		random.seed(seed)
+		seeds = [ ]
+		for _ in xrange(attempts):
+			while True:
+				randVal = random.randint(1000, 2**32-1)
+				if randVal not in seeds:
+					break
+			seeds.append(randVal)
+
+	fitResults = [ ]
+	for fitSeed in seeds:
+		fitResult = pyRootPwa.core.pwaNloptFit(likelihood       = likelihood,
+		                                       massBinMin       = lowerBound,
+		                                       massBinMax       = upperBound,
+		                                       seed             = fitSeed,
+		                                       startValFileName = startValFileName,
+		                                       checkHessian     = checkHessian,
+		                                       saveSpace        = saveSpace,
+		                                       verbose          = verbose)
+		fitResults.append(fitResult)
+	return fitResults
