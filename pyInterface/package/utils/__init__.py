12df376939eef16b3d235d2680d402215fda7ff9
Karl Bicker <kbicker@cern.ch>
2015-09-17 16:12:31 +0200

    pyInterface: Add some utility functions.
    Add two functions to the utilities which should make it easier to handle large
    numbers of fit results in TTrees.

--- pyInterface/package/utils/__init__.py
+++ pyInterface/package/utils/__init__.py
@@ -3,8 +3,10 @@ from _printingUtils import *
 from _progressBar import progressBar
 from _silencer import silencer
 from _binningMapFromArgList import binningMapFromArgList
+from _fitTreeUtils import getFitResultDict, getBestFitResults
 
 del _printingUtils
 del _progressBar
 del _silencer
 del _binningMapFromArgList
+del _fitTreeUtils
