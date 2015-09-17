196677278b1caebee2f04b9bc165ba889d77fe1c
Sebastian Uhl <suhl@cern.ch>
2015-09-17 16:09:37 +0200

    storageFormats: print amplitude metadata from Python script
    Modify the Python version of 'printMetadata' to be able to also print
    amplitude metadata and not only event metadata. A new option '-a' has
    been added allowing to the the base name of the amplitude object
    (typically the wave name). If this option is not present, try to
    guess the name from filenames following the naming convention of the
    file manager.
    
    Also removed the initialisation of the printing stuff which since
    some time is taken care of during 'import pyRootPwa'.

--- storageFormats/printMetadata.py
+++ storageFormats/printMetadata.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
-import multiprocessing
+import os
 import sys
 
 import pyRootPwa
@@ -13,39 +13,54 @@ if __name__ == "__main__":
 	pyRootPwa.core.printLibraryInfo()
 	pyRootPwa.core.printGitHash()
 
-	#initialize the printing functors
-	printingCounter = multiprocessing.Array('i', [0]*5)
-	pyRootPwa.utils.printErr = pyRootPwa.utils.printErrClass(printingCounter)
-	pyRootPwa.utils.printWarn = pyRootPwa.utils.printWarnClass(printingCounter)
-	pyRootPwa.utils.printSucc = pyRootPwa.utils.printSuccClass(printingCounter)
-	pyRootPwa.utils.printInfo = pyRootPwa.utils.printInfoClass(printingCounter)
-	pyRootPwa.utils.printDebug = pyRootPwa.utils.printDebugClass(printingCounter)
-
-
 	parser = argparse.ArgumentParser(description="print event metadata")
 	parser.add_argument("inputFile", type=str, metavar="inputFile", help="input file in ROOTPWA format")
-	parser.add_argument("-r", action="store_true", dest="recalculateHash", help="recalculate hash and compare it with the stored one (default: false)")
+	parser.add_argument("-a", "--amplitudeObjectBaseName", type=str, metavar="objectBaseName", help="amplitude object base name (default: try to guess from input file name)")
+	parser.add_argument("-r", action="store_true", dest="recalculateHash", help="recalculate hash and compare it with the stored one (default: %(default)s)")
 	args = parser.parse_args()
 
 	inputFile = ROOT.TFile.Open(args.inputFile, "READ")
 	if not inputFile:
 		pyRootPwa.utils.printErr("could not open input file '" + args.inputFile + "'.")
-
-	eventMeta = pyRootPwa.core.eventMetadata.readEventFile(inputFile, True)
-	if not eventMeta:
-		pyRootPwa.utils.printErr("could not read event metadata")
 		sys.exit(1)
 
-	if args.recalculateHash:
-		pyRootPwa.utils.printInfo("recalculating hash...")
-		calcHash = eventMeta.recalculateHash(True)
-		if calcHash != eventMeta.contentHash():
-			pyRootPwa.utils.printErr("hash verification failed, hash from metadata '" +
-			                         eventMeta.contentHash() +"' does not match with " +
-			                         "calculated hash '" + calcHash + "'.")
-		else:
-			print("")
-			pyRootPwa.utils.printSucc("recalculated hash matches with hash from metadata.")
-			print("")
+	# try to read event metadata
+	eventMeta = pyRootPwa.core.eventMetadata.readEventFile(inputFile, True)
+	if eventMeta:
+		if args.recalculateHash:
+			pyRootPwa.utils.printInfo("recalculating hash...")
+			calcHash = eventMeta.recalculateHash(True)
+			if calcHash != eventMeta.contentHash():
+				pyRootPwa.utils.printErr("hash verification failed, hash from metadata '" +
+				                         eventMeta.contentHash() +"' does not match with " +
+				                         "calculated hash '" + calcHash + "'.")
+			else:
+				print("")
+				pyRootPwa.utils.printSucc("recalculated hash matches with hash from metadata.")
+				print("")
 
-	print(eventMeta)
+		print(eventMeta)
+
+	# try to read amplitude metadata
+	amplitudeObjectBaseName = args.amplitudeObjectBaseName
+	if not amplitudeObjectBaseName:
+		amplitudeObjectBaseName = os.path.basename(args.inputFile).split("_binID")[0]
+	amplitudeMeta = pyRootPwa.core.amplitudeMetadata.readAmplitudeFile(inputFile, amplitudeObjectBaseName, True)
+	if amplitudeMeta:
+		if args.recalculateHash:
+			pyRootPwa.utils.printInfo("recalculating hash...")
+			calcHash = amplitudeMeta.recalculateHash(True)
+			if calcHash != amplitudeMeta.contentHash():
+				pyRootPwa.utils.printErr("hash verification failed, hash from metadata '" +
+				                         amplitudeMeta.contentHash() +"' does not match with " +
+				                         "calculated hash '" + calcHash + "'.")
+			else:
+				print("")
+				pyRootPwa.utils.printSucc("recalculated hash matches with hash from metadata.")
+				print("")
+
+		print(amplitudeMeta)
+
+	if not eventMeta and not amplitudeMeta:
+		pyRootPwa.utils.printErr("could not read any metadata")
+		sys.exit(1)
