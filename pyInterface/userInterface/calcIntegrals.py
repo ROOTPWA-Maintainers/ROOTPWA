1121f295a2dbb55f0da82dda4ea63bf912b619fb
Sebastian Uhl <suhl@cern.ch>
2015-10-08 16:25:05 +0200

    pyInterface: cosmetics
    Correct some help texts:
    * calculating integrals is only supported for 'generated' and 'accepted'
      events
    * number of events for the normalization of the acceptance integrals is
      actually read from the normalization integrals
    
    Correct a message so that he fit scripts write fit results to the output
    file, and not integrals.

--- pyInterface/userInterface/calcIntegrals.py
+++ pyInterface/userInterface/calcIntegrals.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 	parser.add_argument("-c", type=str, metavar="configFileName", dest="configFileName", default="./rootpwa.config", help="path to config file (default: './rootpwa.config')")
 	parser.add_argument("-n", type=int, metavar="#", dest="nEvents", default=0, help="maximum number of events to process (default: all)")
 	parser.add_argument("-b", type=int, metavar="massBin", default=-1, dest="massBin", help="mass bin to be calculated (default: all)")
-	parser.add_argument("-e", type=str, metavar="eventsType", default="all", dest="eventsType", help="events type to be calculated ('real', 'generated' or 'accepted', default: all)")
+	parser.add_argument("-e", type=str, metavar="eventsType", default="all", dest="eventsType", help="events type to be calculated ('generated' or 'accepted', default: both)")
 	parser.add_argument("-w", type=str, metavar="path", dest="weightsFileName", default="", help="path to MC weight file for de-weighting (default: none)")
 	args = parser.parse_args()
 
