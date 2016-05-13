e78a666b400d3083d1a7841bd3f47011ff253b24
Karl Bicker <kbicker@cern.ch>
2016-05-13 14:54:56 +0200

    pyInterface: Add binning to config file.
    Add the possibility to specify a binning for the integral files in the
    configuration file. This is done in preparation for the implementation of the
    binning logic in the file manager.

--- pyInterface/package/_config.py
+++ pyInterface/package/_config.py
@@ -1,4 +1,5 @@
 
+import ast
 import ConfigParser
 import os
 
@@ -17,6 +18,8 @@ class rootPwaConfig(object):
 	ampDirectory                           = ""
 	intDirectory                           = ""
 	limitFilesPerDir                       = -1
+
+	integralBinning                        = []
 
 	# amplitude section
 	phaseSpaceIntegralDirectory            = ""
@@ -72,6 +75,11 @@ class rootPwaConfig(object):
 			else:
 				self.limitFilesPerDir                   = -1
 
+			self.integralBinning = _readBinning(self.config.get("general", "integralBinning"))
+			if not self.integralBinning:
+				pyRootPwa.utils.printErr("could not read integral binning string.")
+				return False
+
 			self.phaseSpaceIntegralDirectory = self.config.get("amplitude", "phaseSpaceIntegralDirectory")
 			self.phaseSpaceUpperMassBound    = float(self.config.get("amplitude", "phaseSpaceUpperMassBound"))
 
@@ -104,3 +112,101 @@ class rootPwaConfig(object):
 			os.mkdir(self.intDirectory)
 			pyRootPwa.utils.printInfo("created integral directory '" + self.intDirectory + "'.")
 		return True
+
+
+def _readBinning(binningString):
+	# Three cases:
+	#
+	# 1. list of bins [ { "name": (lowBound, highBound)} ]
+	# 2. grid coordinates for axes { "name": [ binBoundaries ] }
+	# 3. edges of rectangular grid { "name": (lowBound, highBound, nBins) }
+	#
+	# Case 3. should be converted to case 2. and case 2. to case 1.
+	#
+	# returns list of bins as in 1. or empty list in case of errors.
+	try:
+		inputVal = ast.literal_eval(binningString)
+	except (SyntaxError, ValueError):
+		pyRootPwa.utils.printWarn("error when converting binning string to python.")
+		return []
+	retval = []
+	if isinstance(inputVal, list): # case 1.
+		for binningItem in inputVal:
+			try:
+				retval.append(pyRootPwa.utils.multiBin(binningItem))
+			except (TypeError, ValueError):
+				pyRootPwa.utils.printWarn("could not convert entry in binning list to multiBin.")
+				return []
+	elif isinstance(inputVal, dict): # case 2. + 3.
+		try:
+			inputVal = _binningInputHandlingForCaseTwoAndThree(inputVal)
+		except (ValueError, TypeError):
+			return []
+		keys = inputVal.keys()
+		indices = [1] * len(keys)
+		limits = [ len(inputVal[key]) for key in keys ]
+		while indices[len(indices)-1] != limits[len(limits)-1]:
+			boundaries = {}
+			for i, key in enumerate(keys):
+				boundaries[key] = (inputVal[key][indices[i]-1], inputVal[key][indices[i]])
+			retval.append(pyRootPwa.utils.multiBin(boundaries))
+			indices[0] += 1
+			for metaIndex in xrange(0, len(indices)-1):
+				if indices[metaIndex] == limits[metaIndex]:
+					indices[metaIndex] = 1
+					indices[metaIndex+1] += 1
+	else:
+		pyRootPwa.utils.printWarn("binning string is neither a list nor a dict.")
+		return []
+	return retval
+
+
+def _binningInputHandlingForCaseTwoAndThree(inputVal):
+	# check the input dictionary for binning cases 2. and 3. (see above)
+	# return a dictionary which conforms to case 2..
+	for key in inputVal:
+		if not isinstance(key, str):
+			errMsg = "key in binning definition is not of type 'str'."
+			pyRootPwa.utils.printWarn(errMsg)
+			raise TypeError(errMsg)
+		axesPartition = inputVal[key]
+		if not (isinstance(axesPartition, list) or isinstance(axesPartition, tuple)):
+			errMsg = "axes partition for variable '" + key + "' is not of type 'tuple' or 'list'."
+			pyRootPwa.utils.printWarn(errMsg)
+			raise TypeError(errMsg)
+		for i in axesPartition:
+			if not (isinstance(i, float) or isinstance(i, int)):
+				errMsg = "axes parition for variable '" + key + "' is not a number."
+				pyRootPwa.utils.printWarn(errMsg)
+				raise TypeError(errMsg)
+		if isinstance(axesPartition, tuple):
+			if len(axesPartition) != 3:
+				errMsg = "axes partition for variable '" + key + "' does not have length 3."
+				pyRootPwa.utils.printWarn(errMsg)
+				raise ValueError(errMsg)
+			expandedBoundaryList = []
+			for i in xrange(axesPartition[2]+1):
+				expandedBoundaryList.append(float(axesPartition[0]) + (float(axesPartition[1] - axesPartition[0]) / float(axesPartition[2])) * i)
+			inputVal[key] = expandedBoundaryList
+	return inputVal
+
+
+def _testReadBinning():
+	testCases = []
+	testString = "["
+	for i in xrange(4):
+		for j in xrange(4):
+			testString += "{ \"mass\": (" + str(1.0+0.1*i) + ", " + str(1.0+0.1*(i+1)) + "), \"tPrime\": (" + str(1.0+0.1*j) + ", " + str(1.0+0.1*(j+1)) + ") }, "
+	testString = testString[:-2]
+	testString += "]"
+	testCases.append(testString)
+	testCases.append("{ \"mass\": [ 1.0, 1.5, 1.9, 1.95, 2.0 ], \"tPrime\": [ 1.0, 1.05, 1.1, 2.0 ] }")
+	testCases.append("{ \"mass\": ( 1.0, 1.4, 4 ), \"tPrime\": ( 2.0, 2.4, 4 ) }")
+	testCases.append("{ \"mass\": ( 1.0, 1.4, 4 ), \"tPrime\": [ 1.0, 1.05, 1.1, 2.0 ] }")
+	testCases.append("{ \"mass\": [ 1.0, 1.5, 1.9, 1.95, 2.0 ], \"tPrime\": ( 2.0, 2.4, 4 ) }")
+	for testCase in testCases:
+		binning = _readBinning(testCase)
+		pyRootPwa.utils.printDebug(testCase + "\n")
+		for mBin in binning:
+			pyRootPwa.utils.printDebug(str(mBin))
+		pyRootPwa.utils.printDebug("##################################################")
