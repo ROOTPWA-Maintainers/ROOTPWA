ROOTPWA-Maintainers/ROOTPWA

commit 71a39d39057ddc47868cbba46cb12868c9806cb5

pyInterface: Fix issues found by pylint in 'pyRootPwa'
+ Layouting
+ Import loops
+ Errors: Mostly in error messages

pyInterface/package/utils/_waveDescThresUtils.py
@@ -1,6 +1,6 @@
 
 import pyRootPwa.core
-import pyRootPwa.utils
+from _printingUtils import printInfo, printWarn
 
 def getWaveDescThresFromFitResult(fitResult, keyFiles):
 	waveDescThres = []
@@ -14,38 +14,38 @@ def getWaveDescThresFromFitResult(fitResult, keyFiles):
 		for prodAmpIndex in xrange(fitResult.nmbProdAmps()):
 			if fitResult.waveNameForProdAmp(prodAmpIndex) == waveName:
 				if fitResult.prodAmp(prodAmpIndex) != 0.:
-					tresholded = False
+					thresholded = False
 
 		threshold = 0.
 		if thresholded:
-			treshold = 1.1 * fitResult.massBinCenter()
+			threshold= 1.1 * fitResult.massBinCenter()
 
 		waveDescThres.append( (waveName, waveDesc, threshold) )
 	return waveDescThres
 
 
 def getWaveDescThresFromWaveList(waveListFileName, keyFiles):
-	pyRootPwa.utils.printInfo("reading amplitude names and thresholds from wave list file "
+	printInfo("reading amplitude names and thresholds from wave list file "
 	          + "'" + waveListFileName + "'.")
 	waveDescThres = []
 	with open(waveListFileName, 'r') as waveListFile:
 		lineNmb = 0
 		for line in waveListFile:
-			if (line[0] == '#'):  # comments start with #
+			if line[0] == '#':  # comments start with #
 				continue
 			line = line.replace('\n', '')
 			lineArray = line.split(" ")
-			if(len(lineArray) >= 1 and len(lineArray) <= 2):
+			if lineArray in [1, 2]:
 				waveName = lineArray[0]
-				if(len(lineArray) == 1):
+				if len(lineArray) == 1:
 					threshold = 0
 				else:
 					threshold = lineArray[1]
 				waveDesc = pyRootPwa.core.waveDescription.parseKeyFile(keyFiles[waveName][0])[keyFiles[waveName][1]]
 				waveDescThres.append( (waveName, waveDesc, float(threshold)) )
 			else:
-				pyRootPwa.utils.printWarn("cannot parse line '" + line + "' in wave list file "
+				printWarn("cannot parse line '" + line + "' in wave list file "
 				          + "'" + waveListFileName + "'.")
 			lineNmb += 1
-		pyRootPwa.utils.printInfo("read " + str(lineNmb) + " lines from wave list file " + "'" + waveListFileName + "'")
+		printInfo("read " + str(lineNmb) + " lines from wave list file " + "'" + waveListFileName + "'")
 	return waveDescThres

pyInterface/package/__init__.py
@@ -1,6 +1,4 @@
 
-import ROOT
-
 from _amplitude import calcAmplitude
 from _config import rootPwaConfig
 from _fileManager import fileManager
@@ -11,6 +9,10 @@ from _fit import pwaNloptFit
 from _integrals import calcIntegrals
 from _likelihood import initLikelihood
 
+import utils
+ROOT = utils.ROOT
+
+# pylint: disable=E0602
 del _amplitude
 del _config
 del _fileManager

pyInterface/package/_amplitude.py
@@ -1,9 +1,9 @@
 
 import os as _os
 
-import pyRootPwa
 import pyRootPwa.core
-ROOT = pyRootPwa.ROOT
+import pyRootPwa.utils
+ROOT = pyRootPwa.utils.ROOT
 
 def calcAmplitude(inputFileName,
                   keyFileName,
@@ -12,14 +12,12 @@ def calcAmplitude(inputFileName,
                   maxNumberOfEvents = -1,
                   printProgress = True):
 
-	printDebug = pyRootPwa.utils.printDebug
 	printInfo = pyRootPwa.utils.printInfo
 	printSucc = pyRootPwa.utils.printSucc
 	printWarn = pyRootPwa.utils.printWarn
-	printErr = pyRootPwa.utils.printErr
 
 	printInfo("Calculating amplitude for key file '" + keyFileName + "' (index " + str(waveDescriptionID) +
-	                          ") with input file '" + inputFileName + "', output file '" + outputFileName + "'.")
+	          ") with input file '" + inputFileName + "', output file '" + outputFileName + "'.")
 
 	if 'ROOTPWA' not in _os.environ:
 		printWarn("$ROOTPWA not set.")

pyInterface/package/_config.py
@@ -4,7 +4,7 @@ import os
 
 import pyRootPwa.utils
 
-class rootPwaConfig:
+class rootPwaConfig(object):
 
 	config = None
 	configFileName = ""
@@ -15,6 +15,7 @@ class rootPwaConfig:
 	dataDirectory                          = ""
 	keyDirectory                           = ""
 	ampDirectory                           = ""
+	intDirectory                           = ""
 	limitFilesPerDir                       = -1
 
 	# fit section

pyInterface/package/_fileManager.py
@@ -1,11 +1,11 @@
+#pylint: skip-file
 
 import glob
 import os
 import cPickle as pickle
 
-import pyRootPwa
-import pyRootPwa.core
-ROOT = pyRootPwa.ROOT
+import pyRootPwa.utils
+ROOT = pyRootPwa.utils.ROOT
 
 
 def saveFileManager(fileManagerObject, path):
@@ -66,7 +66,7 @@ class fileManager:
 	dataFiles       = {}
 	keyFiles        = {}
 	amplitudeFiles  = {}
-	intergalFiles   = {}
+	integralFiles   = {}
 	globalAxes = {}
 	binList = []
 
@@ -161,7 +161,7 @@ class fileManager:
 	def getAmplitudeFilePaths(self, binID, eventsType):
 		ampFileList = {}
 		for key in sorted(self.amplitudeFiles):
-			if (key[0] == binID and key[2] == eventsType):
+			if key[0] == binID and key[2] == eventsType:
 				ampFileList[key[1]] = self.amplitudeDirectory + "/" + self.amplitudeFiles[key]
 		return ampFileList
 
@@ -224,7 +224,7 @@ class fileManager:
 			binAxes[binningVariable] = sorted(binAxes[binningVariable], key=lambda t: t[0])
 			bins = binAxes[binningVariable]
 			if not bins:
-				pyRootPwa.util.printErr("no bins found for variable '" + binningVariable + "'.")
+				pyRootPwa.utils.printErr("no bins found for variable '" + binningVariable + "'.")
 				return {}
 			bound = bins[0]
 			for currentBin in bins[1:]:
@@ -254,7 +254,7 @@ class fileManager:
 						globalBinList.append(currentBin)
 			globalBinList = sorted(globalBinList, key=lambda t: t[0])
 			if not globalBinList:
-				pyRootPwa.util.printErr("global currentBin list empty for '" + binningVariable + "'.")
+				pyRootPwa.utils.printErr("global currentBin list empty for '" + binningVariable + "'.")
 				return {}
 			bound = globalBinList[0]
 			for currentBin in globalBinList[1:]:

pyInterface/package/_fit.py
@@ -1,9 +1,8 @@
-import os
 import random
 
-import pyRootPwa
 import pyRootPwa.core
-ROOT = pyRootPwa.ROOT
+import pyRootPwa.utils
+ROOT = pyRootPwa.utils.ROOT
 
 def pwaFit(ampFileList,
            normIntegralFileName,

pyInterface/package/_integrals.py
@@ -1,8 +1,6 @@
-import os
-
-import pyRootPwa
 import pyRootPwa.core
-ROOT = pyRootPwa.ROOT
+import pyRootPwa.utils
+ROOT = pyRootPwa.utils.ROOT
 
 def calcIntegrals(ampFileList, maxNmbEvents=0, weightFileName=""):
 	integralMatrix = pyRootPwa.core.ampIntegralMatrix()

pyInterface/package/_likelihood.py
@@ -1,5 +1,5 @@
-import pyRootPwa
-ROOT = pyRootPwa.ROOT
+import pyRootPwa.utils
+ROOT = pyRootPwa.utils.ROOT
 
 def initLikelihood(waveDescThres,
                    massBinCenter,
@@ -14,7 +14,7 @@ def initLikelihood(waveDescThres,
                   ):
 	likelihood = pyRootPwa.core.pwaLikelihood()
 	likelihood.useNormalizedAmps(True)
-	if (not verbose):
+	if not verbose:
 		likelihood.setQuiet()
 	if cauchy:
 		likelihood.setPriorType(pyRootPwa.core.HALF_CAUCHY)
@@ -22,7 +22,7 @@ def initLikelihood(waveDescThres,
 	if (not likelihood.init(waveDescThres,
 	                        rank,
 	                        massBinCenter)):
-		printErr("could not initialize likelihood. Aborting...")
+		pyRootPwa.utils.printErr("could not initialize likelihood. Aborting...")
 		return None
 
 	normIntFile = ROOT.TFile.Open(normIntegralFileName, "READ")
@@ -30,7 +30,7 @@ def initLikelihood(waveDescThres,
 		pyRootPwa.utils.printWarn("'" + normIntegralFileName + "' does not contain exactly one TKey.")
 		return None
 	normIntMatrix = normIntFile.Get(pyRootPwa.core.ampIntegralMatrix.integralObjectName)
-	if (not likelihood.addNormIntegral(normIntMatrix)):
+	if not likelihood.addNormIntegral(normIntMatrix):
 		pyRootPwa.utils.printErr("could not add normalization integral. Aborting...")
 		return None
 	normIntFile.Close()
@@ -39,7 +39,7 @@ def initLikelihood(waveDescThres,
 		pyRootPwa.utils.printWarn("'" + accIntegralFileName + "' does not contain exactly one TKey.")
 		return None
 	accIntMatrix = accIntFile.Get(pyRootPwa.core.ampIntegralMatrix.integralObjectName)
-	if (not likelihood.addAccIntegral(accIntMatrix, accEventsOverride)):
+	if not likelihood.addAccIntegral(accIntMatrix, accEventsOverride):
 		pyRootPwa.utils.printErr("could not add acceptance integral. Aborting...")
 		return None
 	accIntFile.Close()
@@ -55,10 +55,10 @@ def initLikelihood(waveDescThres,
 		if not meta:
 			pyRootPwa.utils.printErr("could not get metadata for waveName '" + waveName + "'.")
 			return None
-		if (not likelihood.addAmplitude([meta])):
+		if not likelihood.addAmplitude([meta]):
 			pyRootPwa.utils.printErr("could not add amplitude '" + waveName + "'. Aborting...")
 			return None
-	if (not likelihood.finishInit()):
+	if not likelihood.finishInit():
 		pyRootPwa.utils.printErr("could not finish initialization of likelihood. Aborting...")
 		return None
 

commit b0c0ce3560619be97d9da50b071a64cd8b02ef4c

pyInterface: fix 'getWaveDescThresFromWaveList'
The python function 'getWaveDescThresFromWaveList' should check whether
a line read from the wavelist can be split into either one or two parts,
not whether the split array is either '1' or '2'. This was broken by commit
71a39d39057ddc47868cbba46cb12868c9806cb5,

pyInterface/package/utils/_waveDescThresUtils.py
@@ -35,7 +35,7 @@ def getWaveDescThresFromWaveList(waveListFileName, keyFiles):
 				continue
 			line = line.replace('\n', '')
 			lineArray = line.split(" ")
-			if lineArray in [1, 2]:
+			if len(lineArray) in [1, 2]:
 				waveName = lineArray[0]
 				if len(lineArray) == 1:
 					threshold = 0