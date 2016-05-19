11296a533067acf1aad084802a37fec097ed997c
fkaspar <florian.kaspar@tum.de>
2016-05-19 13:07:45 +0200

    Fix some further bugs.
    Remove old script and check exit codes in a better way.

--- pyInterface/testMC.sh
+++ pyInterface/testMC.sh
@@ -80,9 +80,7 @@ cp "${ROOTPWA}/pyInterface/mcTest/reference_fit/bin65_c2pap_bestfits_converged_M
 
 # generate phase space data
 echo "Generating phase space ..."
-${ROOTPWA}/build/bin/genpw.py -n $NMB_PS_EVENTS -p "${PARTICLE_DATA_TABLE}" -M $MASS -B $BINWIDTH "./generator_noBeamSimulation.conf" -o "./data/phase_space_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PS_EVENTS}.root"
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/genpw.py -n $NMB_PS_EVENTS -p "${PARTICLE_DATA_TABLE}" -M $MASS -B $BINWIDTH "./generator_noBeamSimulation.conf" -o "./data/phase_space_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PS_EVENTS}.root"; then
 	echo "Generation of phase space was not successful! Aborting."
 	exit 1
 fi
@@ -93,9 +91,7 @@ DESTINATION_DIR="${TESTDIR}/keyfiles"
 export PARTICLE_DATA_TABLE
 export DESTINATION_DIR
 export WAVESET_FILES="wavelist.compass.2008.88waves"
-${ROOTPWA}/userAnalysisWorkspace/3pi.--+/keyfiles/GenerateKeyfiles.sh
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/userAnalysisWorkspace/3pi.--+/keyfiles/GenerateKeyfiles.sh; then
 	echo "Generation of keyfiles was not successful! Aborting."
 	exit 1
 fi
@@ -106,45 +102,35 @@ rm ${TESTDIR}/keyfiles/*[f0_980_0=[*.key
 
 # create first file manager
 echo "Create first file manager ..."
-${ROOTPWA}/build/bin/createFileManager.py
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/createFileManager.py; then
 	echo "Creation of first file manager was not successful! Aborting."
 	exit 1
 fi
 
 # calculate amplitudes for generated data
 echo "Calculate amplitudes for generated data ..."
-${ROOTPWA}/build/bin/calcAmplitudes.py -e generated
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/calcAmplitudes.py -e generated; then
 	echo "Calculation of amplitudes for generated data was not successful! Aborting."
 	exit 1
 fi
 
 # calculate integrals for generated data
 echo "Calculate integrals for generated data ..."
-${ROOTPWA}/build/bin/calcIntegrals.py -e generated
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/calcIntegrals.py -e generated; then
 	echo "Calculation of integrals for generated was not successful! Aborting."
 	exit 1
 fi
 
 # generate weighted MC pseudo data
 echo "Generate weighted MC pseudo data ..."
-${ROOTPWA}/build/bin/genPseudoData.py "./generator_noBeamSimulation.conf" "${TESTDIR}/reference_fit/bin65_c2pap_bestfits_converged_MASS_1800_1820_N45340.root" "./ints/integral_binID-0_2.root" "./weighted_mc_data/weighted_pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root" -s 654321 -n ${NMB_PSEUDO_EVENTS} -M ${MASS} -B ${BINWIDTH} 
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/genPseudoData.py "./generator_noBeamSimulation.conf" "${TESTDIR}/reference_fit/bin65_c2pap_bestfits_converged_MASS_1800_1820_N45340.root" "./ints/integral_binID-0_2.root" "./weighted_mc_data/weighted_pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root" -s 654321 -n ${NMB_PSEUDO_EVENTS} -M ${MASS} -B ${BINWIDTH}; then 
 	echo "Generation of weighted MC pseudo data was not successful! Aborting."
 	exit 1
 fi
 
 # deweight the pseudo data
 echo "Deweight pseudo data ..."
-${ROOTPWA}/build/bin/deWeight.py "./weighted_mc_data/weighted_pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root" "./data/pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root"
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/deWeight.py "./weighted_mc_data/weighted_pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root" "./data/pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root"; then
 	echo "Deweighting of weighted MC pseudo data was not successful! Aborting."
 	exit 1
 fi
@@ -155,18 +141,14 @@ mv ./fileManager.pkl ./fileManager.pkl.old
 
 # ... create a new file manager
 echo "Create second file manager ..."
-${ROOTPWA}/build/bin/createFileManager.py
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/createFileManager.py; then
 	echo "Creation of second file manager was not successful! Aborting."
 	exit 1
 fi
 
 # calculate amplitudes for 'real' (= MC) data
 echo "Calculate amplitudes for 'real' (= MC) data ..."
-${ROOTPWA}/build/bin/calcAmplitudes.py -e real
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/calcAmplitudes.py -e real; then
 	echo "Calculation of amplitudes for 'real' (= MC) data  was not successful! Aborting."
 	exit 1
 fi
@@ -180,33 +162,25 @@ ln -s "${TESTDIR}/ints/integral_binID-0_2.root" "${TESTDIR}/ints/integral_binID-
 ### BEGIN FIT TEST ###
 
 echo "Test pwaFit.py without prior ..."
-${ROOTPWA}/build/bin/pwaFit.py "./fits/pwaTest_NONLOPT_NOPRIOR.root" -z -w wavelist.compass.2008.88waves
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/pwaFit.py "./fits/pwaTest_NONLOPT_NOPRIOR.root" -z -w wavelist.compass.2008.88waves; then
 	echo "pwaFit was not successful! Aborting."
 	exit 1
 fi
 
 echo "Test pwaFit.py with prior ..."
-${ROOTPWA}/build/bin/pwaFit.py "./fits/pwaTest_NONLOPT_CAUCHY_PRIOR_WIDTH_0.5.root" -z -w wavelist.compass.2008.88waves -C -P 0.5
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/pwaFit.py "./fits/pwaTest_NONLOPT_CAUCHY_PRIOR_WIDTH_0.5.root" -z -w wavelist.compass.2008.88waves -C -P 0.5; then
 	echo "pwaFit with prior was not successful! Aborting."
 	exit 1
 fi
 
 echo "Test pwaNloptFit.py without prior ..."
-${ROOTPWA}/build/bin/pwaNloptFit.py "./fits/pwaTest_NLOPT_NOPRIOR.root" -z -w wavelist.compass.2008.88waves
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/pwaNloptFit.py "./fits/pwaTest_NLOPT_NOPRIOR.root" -z -w wavelist.compass.2008.88waves; then
 	echo "pwaNloptFit was not successful! Aborting."
 	exit 1
 fi
 
 echo "Test pwaNloptFit.py with prior ..."
-${ROOTPWA}/build/bin/pwaNloptFit.py "./fits/pwaTest_NLOPT_CAUCHY_PRIOR_WIDTH_0.5.root" -z -w wavelist.compass.2008.88waves -C -P 0.5
-
-if [ $? -ne 0 ]; then
+if ! ${ROOTPWA}/build/bin/pwaNloptFit.py "./fits/pwaTest_NLOPT_CAUCHY_PRIOR_WIDTH_0.5.root" -z -w wavelist.compass.2008.88waves -C -P 0.5; then
 	echo "pwaNloptFit with prior was not successful! Aborting."
 	exit 1
 fi
