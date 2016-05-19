0cb8bd57dfa87c0675b717d5c9cdee83c32c55b8
fkaspar <florian.kaspar@tum.de>
2016-05-19 14:36:45 +0200

    Fix testMC.sh.
    Fix some error messages and do not copy template.key anymore.

--- pyInterface/testMC.sh
+++ pyInterface/testMC.sh
@@ -34,17 +34,17 @@
 ### BEGIN PREPARATION ###
 
 if [ -d "${ROOTPWA}" ]; then
-	echo "RootPwa installation found in: ${ROOTPWA}"
+	echo "Installation of RootPwa found in: ${ROOTPWA}"
 	echo "Starting MC test ..."
 else
-	echo "Error no installation of RootPwa found! Aborting."
+	echo "No installation of RootPwa found. Aborting..."
 	exit 1
 fi
 
 TESTDIR=${PWD}/ROOTPWA_MC_TEST
 
 if [ -d "${TESTDIR}" ]; then
-	echo "Test directory ${TESTDIR} already exists! Aborting."
+	echo "Test directory ${TESTDIR} already exists. Aborting..."
 	exit 1
 fi
 
@@ -69,7 +69,6 @@ exec 2>&1
 echo "Copying necessary files to test directory ..."
 
 cp "${ROOTPWA}/userAnalysisWorkspace/3pi.--+/generator_noBeamSimulation.conf" "./"
-cp "${ROOTPWA}/userAnalysisWorkspace/3pi.--+/keyfiles/template.key" "./"
 cp "${ROOTPWA}/userAnalysisWorkspace/3pi.--+/keyfiles/wavelist.compass.2008.88waves" "./"
 cp "${ROOTPWA}/pyInterface/rootpwa.config" "./"
 cp "${ROOTPWA}/pyInterface/mcTest/reference_fit/bin65_c2pap_bestfits_converged_MASS_1800_1820_N45340.root.example" "./reference_fit/bin65_c2pap_bestfits_converged_MASS_1800_1820_N45340.root"
@@ -81,7 +80,7 @@ cp "${ROOTPWA}/pyInterface/mcTest/reference_fit/bin65_c2pap_bestfits_converged_M
 # generate phase space data
 echo "Generating phase space ..."
 if ! ${ROOTPWA}/build/bin/genpw.py -n $NMB_PS_EVENTS -p "${PARTICLE_DATA_TABLE}" -M $MASS -B $BINWIDTH "./generator_noBeamSimulation.conf" -o "./data/phase_space_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PS_EVENTS}.root"; then
-	echo "Generation of phase space was not successful! Aborting."
+	echo "Generation of phase space was not successful. Aborting..."
 	exit 1
 fi
 
@@ -91,8 +90,9 @@ DESTINATION_DIR="${TESTDIR}/keyfiles"
 export PARTICLE_DATA_TABLE
 export DESTINATION_DIR
 export WAVESET_FILES="wavelist.compass.2008.88waves"
+export TEMPLATE_KEY_FILES="${ROOTPWA}/userAnalysisWorkspace/3pi.--+/keyfiles/template.key"
 if ! ${ROOTPWA}/userAnalysisWorkspace/3pi.--+/keyfiles/GenerateKeyfiles.sh; then
-	echo "Generation of keyfiles was not successful! Aborting."
+	echo "Generation of keyfiles was not successful. Aborting..."
 	exit 1
 fi
 
@@ -103,35 +103,35 @@ rm ${TESTDIR}/keyfiles/*[f0_980_0=[*.key
 # create first file manager
 echo "Create first file manager ..."
 if ! ${ROOTPWA}/build/bin/createFileManager.py; then
-	echo "Creation of first file manager was not successful! Aborting."
+	echo "Creation of first file manager was not successful. Aborting..."
 	exit 1
 fi
 
 # calculate amplitudes for generated data
 echo "Calculate amplitudes for generated data ..."
 if ! ${ROOTPWA}/build/bin/calcAmplitudes.py -e generated; then
-	echo "Calculation of amplitudes for generated data was not successful! Aborting."
+	echo "Calculation of amplitudes for generated data was not successful. Aborting..."
 	exit 1
 fi
 
 # calculate integrals for generated data
 echo "Calculate integrals for generated data ..."
 if ! ${ROOTPWA}/build/bin/calcIntegrals.py -e generated; then
-	echo "Calculation of integrals for generated was not successful! Aborting."
+	echo "Calculation of integrals for generated was not successful. Aborting..."
 	exit 1
 fi
 
 # generate weighted MC pseudo data
 echo "Generate weighted MC pseudo data ..."
 if ! ${ROOTPWA}/build/bin/genPseudoData.py "./generator_noBeamSimulation.conf" "${TESTDIR}/reference_fit/bin65_c2pap_bestfits_converged_MASS_1800_1820_N45340.root" "./ints/integral_binID-0_2.root" "./weighted_mc_data/weighted_pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root" -s 654321 -n ${NMB_PSEUDO_EVENTS} -M ${MASS} -B ${BINWIDTH}; then 
-	echo "Generation of weighted MC pseudo data was not successful! Aborting."
+	echo "Generation of weighted MC pseudo data was not successful. Aborting..."
 	exit 1
 fi
 
 # deweight the pseudo data
 echo "Deweight pseudo data ..."
 if ! ${ROOTPWA}/build/bin/deWeight.py "./weighted_mc_data/weighted_pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root" "./data/pseudoData_MASS_${MASS}-$((MASS+BINWIDTH))_N_${NMB_PSEUDO_EVENTS}.root"; then
-	echo "Deweighting of weighted MC pseudo data was not successful! Aborting."
+	echo "Deweighting of weighted MC pseudo data was not successful. Aborting..."
 	exit 1
 fi
 
@@ -142,14 +142,14 @@ mv ./fileManager.pkl ./fileManager.pkl.old
 # ... create a new file manager
 echo "Create second file manager ..."
 if ! ${ROOTPWA}/build/bin/createFileManager.py; then
-	echo "Creation of second file manager was not successful! Aborting."
+	echo "Creation of second file manager was not successful. Aborting..."
 	exit 1
 fi
 
 # calculate amplitudes for 'real' (= MC) data
 echo "Calculate amplitudes for 'real' (= MC) data ..."
 if ! ${ROOTPWA}/build/bin/calcAmplitudes.py -e real; then
-	echo "Calculation of amplitudes for 'real' (= MC) data  was not successful! Aborting."
+	echo "Calculation of amplitudes for 'real' (= MC) data  was not successful. Aborting..."
 	exit 1
 fi
 
@@ -163,25 +163,25 @@ ln -s "${TESTDIR}/ints/integral_binID-0_2.root" "${TESTDIR}/ints/integral_binID-
 
 echo "Test pwaFit.py without prior ..."
 if ! ${ROOTPWA}/build/bin/pwaFit.py "./fits/pwaTest_NONLOPT_NOPRIOR.root" -z -w wavelist.compass.2008.88waves; then
-	echo "pwaFit was not successful! Aborting."
+	echo "pwaFit was not successful. Aborting..."
 	exit 1
 fi
 
 echo "Test pwaFit.py with prior ..."
 if ! ${ROOTPWA}/build/bin/pwaFit.py "./fits/pwaTest_NONLOPT_CAUCHY_PRIOR_WIDTH_0.5.root" -z -w wavelist.compass.2008.88waves -C -P 0.5; then
-	echo "pwaFit with prior was not successful! Aborting."
+	echo "pwaFit with prior was not successful. Aborting..."
 	exit 1
 fi
 
 echo "Test pwaNloptFit.py without prior ..."
 if ! ${ROOTPWA}/build/bin/pwaNloptFit.py "./fits/pwaTest_NLOPT_NOPRIOR.root" -z -w wavelist.compass.2008.88waves; then
-	echo "pwaNloptFit was not successful! Aborting."
+	echo "pwaNloptFit was not successful. Aborting..."
 	exit 1
 fi
 
 echo "Test pwaNloptFit.py with prior ..."
 if ! ${ROOTPWA}/build/bin/pwaNloptFit.py "./fits/pwaTest_NLOPT_CAUCHY_PRIOR_WIDTH_0.5.root" -z -w wavelist.compass.2008.88waves -C -P 0.5; then
-	echo "pwaNloptFit with prior was not successful! Aborting."
+	echo "pwaNloptFit with prior was not successful. Aborting..."
 	exit 1
 fi
 
