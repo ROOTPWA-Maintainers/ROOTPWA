5be058b4d45fc54574753a268f98f7492f558357
Boris Grube <bgrube@tum.de>
2016-07-22 16:06:26 +0200

    Test: improve reporting of script for test MC
    Fix misbehavior of sed line on BSD-like systems, as reported by
    Sebastian

--- test/testMC.sh
+++ test/testMC.sh
@@ -116,7 +116,7 @@ cp --verbose "${ROOTPWA}/rootpwa.config" "./"
 cp --verbose "${ROOTPWA}/test/mcTest/reference_fit/bin65_c2pap_bestfits_converged_MASS_1800_1820_N45340.root.example" "./reference_fit/bin65_c2pap_bestfits_converged_MASS_1800_1820_N45340.root"
 
 # make sure the integral binning in the config file is set correctly
-sed -i 's/^integralBinning.*$/integralBinning                        = [ { "mass": (1.8, 1.82) } ]/' rootpwa.config
+sed -i'' 's/^integralBinning.*$/integralBinning                        = [ { "mass": (1.8, 1.82) } ]/' rootpwa.config
 
 #-- END PREPARATION --#
 
