33bb89b8840f8121b428b7ff776b26d501467878
Sebastian Uhl <suhl@cern.ch>
2016-12-07 18:00:02 +0100

    buildSystem: rely on having 'thread_local'
    With C++11 it should be possible to rely on having the 'thread_local'
    keyword working. So no longer check this in CMake, and remove the one
    occasion where the corresponding define is used.

--- resonanceFit/massDepFitFunction.cc
+++ resonanceFit/massDepFitFunction.cc
@@ -502,7 +502,6 @@ rpwa::massDepFit::function::chiSquare(const std::vector<double>& par) const
 double
 rpwa::massDepFit::function::chiSquare(const double* par) const
 {
-#ifdef COMPILER_PROVIDES_THREAD_LOCAL
 	// in C++11 we can use a static variable per thread so that the
 	// parameters are kept over function calls and we can implement some
 	// caching
@@ -515,17 +514,6 @@ rpwa::massDepFit::function::chiSquare(const double* par) const
 	                                           _compset->getMaxChannelsInComponent(),
 	                                           _nrBins,
 	                                           _nrMassBins);
-#else
-	rpwa::massDepFit::parameters fitParameters(_compset->getNrComponents()+1,           // nr components + final-state mass-dependence
-	                                           _compset->getMaxChannelsInComponent(),
-	                                           _compset->getMaxParametersInComponent(),
-	                                           _nrBins);
-	rpwa::massDepFit::cache cache(_nrWaves,
-	                              _compset->getNrComponents()+1,           // nr components + final-state mass-dependence
-	                              _compset->getMaxChannelsInComponent(),
-	                              _nrBins,
-	                              _nrMassBins);
-#endif
 
 	// import parameters (couplings, branchings, resonance parameters, ...)
 	_compset->importParameters(par, fitParameters, cache);
