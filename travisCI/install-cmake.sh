be9563a0c69f8555f2d55784f55f3071278f19a1
Sebastian Uhl <suhl@cern.ch>
2017-06-19 19:06:06 +0200

    buildSystem: use more modern version of CMake in Travis
    Use CMake version 3.8.2 in Travis, the previous version of CMake spat
    out tons of useless warnings for the new (at the time of CMake release
    unknown) Boost version.

--- travisCI/install-cmake.sh
+++ travisCI/install-cmake.sh
@@ -3,12 +3,12 @@ set -exv
 
 cd ${TRAVIS_BUILD_DIR}/deps/
 
-if [ -d cmake-3.6.1-Linux-x86_64 ] ; then
-	echo "CMake installation found in 'cmake-3.6.1-Linux-x86_64', using that."
+if [ -d cmake-3.8.2-Linux-x86_64 ] ; then
+	echo "CMake installation found in 'cmake-3.8.2-Linux-x86_64', using that."
 else
 	echo "No CMake installation found, installing a fresh one."
 
-	wget https://cmake.org/files/v3.6/cmake-3.6.1-Linux-x86_64.tar.gz
-	tar -xzf cmake-3.6.1-Linux-x86_64.tar.gz
+	wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
+	tar -xzf cmake-3.8.2-Linux-x86_64.tar.gz
 fi
-ln -sfn cmake-3.6.1-Linux-x86_64 cmake
+ln -sfn cmake-3.8.2-Linux-x86_64 cmake
