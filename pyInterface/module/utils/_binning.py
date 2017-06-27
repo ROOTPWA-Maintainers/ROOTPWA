c3917fd6235c0d80601379babff181b27ae58195
Stefan Wallner <stefan@nuklid.com>
2017-06-27 11:52:28 +0200

    pyInterface: Additional functionality of the multiBin class
    Implement hash function for the multiBin class, such that
    it can be used as key in a dict.
    Additionally, the printing of the multiBin class is improved.
    Implement fetching of a sub-bin.
    Implement 'A in B' operator if bin A is contained in bin B.
    Remove restriction that a multiBin must have at least one variable
    (this restriction, if needed, should be implemented in the config file)
    Remove constrain that a the boundaries of a multi-bin cannot be equal,
    because this is needed if we have only the bin-center information
    available (e.g. old version of fitResult class).

--- pyInterface/module/utils/_binning.py
+++ pyInterface/module/utils/_binning.py
@@ -1,4 +1,5 @@
-from _printingUtils import printDebug
+import copy
+from _printingUtils import printDebug, printErr
 
 
 class multiBin(object):
@@ -61,15 +62,95 @@ class multiBin(object):
 
 
 	def __str__(self):
-		retval = ""
-		retval += "multiBin: { "
-		for key in sorted(self.boundaries.keys()):
-			retval += "\"" + key + "\": (" + str(self.boundaries[key][0]) + ", " + str(self.boundaries[key][1]) + "), "
-		retval = retval[:-2] + " }"
+		retval = "multiBin: " + self.__repr__()
 		return retval
 
+	def __repr__(self):
+		retVariables = []
+		for key in sorted(self.boundaries.keys()):
+			retVariables.append( "\"" + key + "\": (" + str(self.boundaries[key][0]) + ", " + str(self.boundaries[key][1]) + ")")
+		retval = "{ "
+		retval += ", ".join(retVariables)
+		retval += " }"
+		return retval
+
+	def __hash__(self):
+		keys = tuple(self.variables())
+		values = tuple([ self.boundaries[k] for k in keys])
+		return hash((keys, values))
+
+	def __contains__(self, other):
+		'''
+		The other multiBin is contained in this multiBin if the intervals of all variables
+		of this multiBin are larger than the corresponding intervals of the other multiBin.
+		If this multiBin is binned in a variable in which the other multiBin is not binned,
+		the other multiBin is not contained in this multiBin.
+		If the other multiBin is binned in a variable in which this multiBin is not binned,
+		the other multiBin is contained in this multiBin.
+		'''
+		if isinstance(other, dict):
+			other = multiBin(other)
+		if not isinstance(other, multiBin):
+			msg = "Cannot check whether object of type {0} is contained in a multiBin".format(type(other))
+			printErr(msg)
+			raise ValueError(msg)
+
+		for key in self.boundaries.keys():
+			if key in other.boundaries.keys():
+				if not self.boundaries[key][0] <= other.boundaries[key][0]:
+					return False
+				if not self.boundaries[key][1] >= other.boundaries[key][1]:
+					return False
+			else:
+				return False
+		return True
+
+	def variables(self):
+		'''
+		@return: sorted list of variables defined in the multibin
+		'''
+		return sorted(self.boundaries.keys())
+
+	def uniqueStr(self):
+		'''
+		@return: string, unique for the multibin (without whitespaces)
+		'''
+		out = []
+		for variable in self.variables():
+			variableStr = "{0}_{1!r}_{2!r}".format(variable, self.boundaries[variable][0], self.boundaries[variable][1])
+			out.append(variableStr)
+		return "__".join(out)
+
+	@classmethod
+	def fromUniqueStr(cls, strIn):
+		boundaries = {}
+		if strIn:
+			variables = strIn.split('__')
+			for variable in variables:
+				if variable.count('_') == 2:
+					name, lower, upper = variable.split("_")
+					boundaries[name] = (float(lower), float(upper))
+				else:
+					printErr("Cannot get multiBin form string '{0}'".format(strIn))
+		return multiBin(boundaries)
 
 	def overlap(self, other, strict=True):
+		'''
+		@return: True if this and the other multiBin overlap in all variables, False otherwise.
+		'''
+		keys = self.variables()
+		if keys != other.variables():
+			return False
+		for key in keys:
+			if not self.overlapInVariable(other, key, strict):
+				return False
+		return True
+
+
+	def overlapInVariable(self, other, key, strict=True):
+		'''
+		@return: True if this and the other multiBin overlap in variable key, False otherwise.
+		'''
 		def comparator(left, right, direction):
 			if direction == ">":
 				right, left = left, right
@@ -78,14 +159,10 @@ class multiBin(object):
 			else:
 				return left <= right
 
-		keys = sorted(self.boundaries.keys())
-		if keys != sorted(other.boundaries.keys()):
+		if comparator(self.boundaries[key][1], other.boundaries[key][0], "<"):
 			return False
-		for key in keys:
-			if comparator(self.boundaries[key][1], other.boundaries[key][0], "<"):
-				return False
-			if comparator(self.boundaries[key][0], other.boundaries[key][1], ">"):
-				return False
+		if comparator(self.boundaries[key][0], other.boundaries[key][1], ">"):
+			return False
 		return True
 
 
@@ -101,6 +178,26 @@ class multiBin(object):
 			if value < self.boundaries[variableName][0] or value >= self.boundaries[variableName][1]:
 				return False
 		return True
+
+	def getSubMultiBin(self, exception=None):
+		'''
+		@param exception: Str or list of Str with the variables which should be removed from the returned multiBin object
+		@return: multiBin in all binning variables except those given in exception
+		@rtype: multiBin
+		'''
+		if exception is None:
+			return copy.deepcopy(self)
+
+		if isinstance(exception, str):
+			exception = [exception]
+		boundaries = {}
+		for k,values in self.boundaries.iteritems():
+			if k not in exception:
+				boundaries[k] = values
+		return multiBin(boundaries)
+
+	def getBinCenters(self):
+		return {k: 0.5*(v[0]+v[1]) for k,v in self.boundaries.iteritems()}
 
 
 def _testMultiBin():
