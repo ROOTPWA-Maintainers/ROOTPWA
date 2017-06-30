47ee208abe4604a6fd604eacd4293142c76002e4
Sebastian Uhl <suhl@cern.ch>
2017-06-30 16:07:15 +0200

    buildSystem: coding style in Travis
    Enable a check of the coding style in Travis. The result of this check
    is not taken into account when considering to fail a build.
    
    The format is checked as a new entry in the Travis build matrix.

--- travisCI/run.sh
+++ travisCI/run.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 set -exv
 
-cd ${TRAVIS_BUILD_DIR}/build
-
-# configure the build process
-cmake ${TRAVIS_BUILD_DIR}
-
-# run 'make'
-make
-
-# - if 'make' was successful, run 'make test'
-# - if 'make test' was not successful, print the log and fail the build
-make test || (cat Testing/Temporary/LastTest.log && false)
-
-# - if 'make test' was successful, run 'testMC.sh'
-cd ${TRAVIS_BUILD_DIR}
-# set some environment variables
-export ROOTPWA=${PWD}
-export LD_LIBRARY_PATH=${ROOTPWA}/build/lib:${LD_LIBRARY_PATH}
-export PATH=${ROOTPWA}/build/bin:${PATH}
-export PYTHONPATH=${ROOTPWA}/build/pyLib:${PYTHONPATH}
-# run the test itself
-cd ${TRAVIS_BUILD_DIR}/test
-./testMC.sh
+if [ ${TRAVIS_RPWA_RUN_FORMAT:-0} -eq 1 ] ; then
+	travisCI/run_format.sh
+fi
+if [ ${TRAVIS_RPWA_RUN_BUILD:-0} -eq 1 ] ; then
+	travisCI/run_build.sh
+fi
