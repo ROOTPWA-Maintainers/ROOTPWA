273b23336173d422a8a63aee77c28d6fde2ad57d
Sebastian Uhl <suhl@cern.ch>
2018-05-15 15:59:48 +0200

    buildSystem: move Travis build to script
    Move the build commands from the Travis configuration file 'travis.yml'
    to an independent script.

---
+++ travisCI/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -exv
+
+cd ${TRAVIS_BUILD_DIR}/build
+
+# configure the build process
+cmake ${TRAVIS_BUILD_DIR}
+
+# run 'make'
+make
+
+# - if 'make' was successful, run 'make test'
+# - if 'make test' was not successful, print the log and fail the build
+make test || (cat Testing/Temporary/LastTest.log && false)
+
+# - if 'make test' was successful, run 'testMC.sh'
+cd ${TRAVIS_BUILD_DIR}
+# set some environment variables
+export ROOTPWA=${PWD}
+export LD_LIBRARY_PATH=${ROOTPWA}/build/lib:${LD_LIBRARY_PATH}
+export PATH=${ROOTPWA}/build/bin:${PATH}
+export PYTHONPATH=${ROOTPWA}/build/pyLib:${PYTHONPATH}
+# run the test itself
+cd ${TRAVIS_BUILD_DIR}/test
+./testMC.sh
