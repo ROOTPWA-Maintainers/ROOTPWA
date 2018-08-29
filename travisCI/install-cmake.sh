8053cee9d4340200d334b635788d992c7b57269b
Sebastian Uhl <suhl@cern.ch>
2018-08-29 09:08:55 +0200

    buildSystem: use more recent Cmake version in Travis
    Update Cmake in Travis to version 3.12.1 for the previous commit
    concerning the Python version in Boost libraries.

--- travisCI/install-cmake.sh
+++ travisCI/install-cmake.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -exv
 
+CMAKE_VERSION=3.12.1
+CMAKE_VERSION_SHORT=$(echo "${CMAKE_VERSION}" | cut -d "." -f 1-2 -)
+
 cd ${TRAVIS_BUILD_DIR}/deps/
 
-if [ -d cmake-3.8.2-Linux-x86_64 ] ; then
-	echo "CMake installation found in 'cmake-3.8.2-Linux-x86_64', using that."
+if [ -d cmake-${CMAKE_VERSION}-Linux-x86_64 ] ; then
+	echo "CMake installation found in 'cmake-${CMAKE_VERSION}-Linux-x86_64', using that."
 else
 	echo "No CMake installation found, installing a fresh one."
 
-	wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
-	tar -xzf cmake-3.8.2-Linux-x86_64.tar.gz
-	rm -rf cmake-3.8.2-Linux-x86_64.tar.gz
+	wget https://cmake.org/files/v${CMAKE_VERSION_SHORT}/cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz
+	tar -xzf cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz
+        rm -rf cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz
 fi
-ln -sfn cmake-3.8.2-Linux-x86_64 cmake
+ln -sfn cmake-${CMAKE_VERSION}-Linux-x86_64 cmake
